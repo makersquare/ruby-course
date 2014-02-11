06a91f824c23445b1813d11ff47eb1d812ba31d0
Taaha Chaudhry <t44h413@gmail.com>
2014-02-11 17:21:12 +0000

    Can add menu items and retrieve them

--- bar.rb
+++ bar.rb
@@ -7,4 +7,17 @@ class Bar
     @name=name
     @menu_items=[]
   end
+
+  def add_menu_item(name,price)
+    @menu_items.push(Menu.new(name,price))
+  end
+end
+
+class Menu
+  attr_accessor :name
+  attr_accessor :price
+  def initialize(name,price)
+    @name=name
+    @price=price
+  end
 end
