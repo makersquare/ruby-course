4c81f15cba9b3e48b683f782224d9951695a55f8
Bethany Nagel <beth0851@gmail.com>
2014-02-11 09:54:43 -0600

    Add MenuItem class and add_menu_item method to Bar class

--- bar.rb
+++ bar.rb
@@ -2,7 +2,30 @@ require 'time' # you're gonna need it
 
 class Bar
 	attr_reader :name
+	attr_accessor :menu_items
+	
 	def initialize(name)
 		@name = name
+		@menu_items = []
+	end
+
+	def add_menu_item(item, price)
+		@menu_items << MenuItem.new(item, price)
 	end
 end
+
+class MenuItem
+	attr_accessor :name
+	attr_accessor :price
+
+	def initialize(name, price)
+		@name = name
+		@price = price
+	end
+
+end
+
+
+
+
+
