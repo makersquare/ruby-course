0009ea91bc22f215ffe18edfcc148031612e8fca
Micah Stewart <stewart.micah@gmail.com>
2014-02-11 10:32:43 -0600

    7/8 tests passed

--- bar.rb
+++ bar.rb
@@ -1,4 +1,31 @@
 require 'time' # you're gonna need it
 
 class Bar
+attr_reader :name
+attr_accessor :menu_items, :happy_discount
+
+  def initialize(name, menu_items = nil, happy_discount = 0)
+    @name = name
+    @menu_items = []
+    @happy_discount = happy_discount
+  end
+
+  def add_menu_item(name, price)
+    item = Drink.new(name, price)
+    @menu_items.push(item)
+  end
+
+
+end
+
+
+
+class Drink
+  attr_accessor :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
 end
