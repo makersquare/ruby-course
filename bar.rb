f30055847ec8256bfdc8555c543e5537d572ff2d
Ian White <ianwhite@utexas.edu>
2014-02-11 10:16:05 -0600

    create #happy_hour method as custom setter for @happy_hour to constrain it's value between 0 and 1

--- bar.rb
+++ bar.rb
@@ -1,8 +1,8 @@
 require 'time' # you're gonna need it
 
 class Bar
-  attr_reader :name
-  attr_accessor :menu_items, :happy_discount
+  attr_reader :name, :happy_discount
+  attr_accessor :menu_items
   def initialize(name)
     @name = name
     @menu_items = []
@@ -11,6 +11,16 @@ class Bar
 
   def add_menu_item(name, price)
     @menu_items << MenuItem.new(name, price)
+  end
+
+  def happy_discount=(discount)
+    if discount < 0
+      @happy_discount = 0
+    elsif discount > 1
+      @happy_discount = 1
+    else
+      @happy_discount = discount
+    end
   end
 end
 
