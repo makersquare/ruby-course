64f792db7fc51800f80c3670ffe3ab334a621576
Taaha Chaudhry <t44h413@gmail.com>
2014-02-11 17:45:08 +0000

    Default happy hour discount to 0, can set happy hour discount and constrains it to betweeen 0 and 1.

--- bar.rb
+++ bar.rb
@@ -6,10 +6,22 @@ class Bar
   def initialize(name)
     @name=name
     @menu_items=[]
+    @happy_discount=0
   end
 
   def add_menu_item(name,price)
     @menu_items.push(Menu.new(name,price))
+  end
+
+  attr_reader :happy_discount
+  def happy_discount=(discount)
+    if discount > 1
+      @happy_discount=1
+    elsif discount < 0
+      @happy_discount=0
+    else
+      @happy_discount=discount
+    end
   end
 end
 
