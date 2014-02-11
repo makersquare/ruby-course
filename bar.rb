cee78d8b3510cab776dd38a8b4d8e254e974aa2e
vagrant <vagrant@student-dev-box>
2014-02-11 16:16:10 +0000

    add new menu items

--- bar.rb
+++ bar.rb
@@ -2,11 +2,21 @@ require 'time' # you're gonna need it
 
 class Bar
   attr_reader :name
+  attr_accessor :menu_items
   def initialize(name)
     @name = name
+    @menu_items = []
   end
 
   def name
     @name
+  end
+
+  def menu_items
+    @menu_items
+  end
+
+  def add_menu_item(item,price)
+    @menu_items << [item,price]
   end
 end
