c8da517a4771946423837c5468c5789b1133b526
Ian White <ianwhite@utexas.edu>
2014-02-14 09:18:55 -0600

    add TaxiMeter#start and TaxiMeter#stop

--- taxi_meter.rb
+++ taxi_meter.rb
@@ -1,8 +1,19 @@
 class TaxiMeter
-  attr_accessor :amount_due, :miles_driven
+  attr_accessor :amount_due, :miles_driven, :start_time
+  attr_reader :stop_time
 
   def initialiaze
     @amount_due = 0
     @miles_driven = 0
+    @start_time = nil
+    @stop_time = nil
+  end
+
+  def start
+    @start_time = Time.now
+  end
+
+  def stop
+    @stop_time = Time.now
   end
 end
