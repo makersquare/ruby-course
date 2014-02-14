1c99b0aedb5666e46465f001de2736ba64c29b1f
Michael Young <michaeljy@gmail.com>
2014-02-14 11:56:05 -0600

    all tests green. code is ugly. will refactor if have time

--- spec/taxi_meter_spec.rb
+++ spec/taxi_meter_spec.rb
@@ -13,8 +13,8 @@ describe TaxiMeter do
     end
 
     it "starts at zero" do
-      @meter.amount_due = 0
-      @meter.miles_driven = 0
+      expect(@meter.amount_due).to eq(0)
+      expect(@meter.miles_driven).to eq(0)
     end
 
     it "can start and stop" do
@@ -41,6 +41,7 @@ describe TaxiMeter do
       expect(@meter.start_time).to eq(start_time)
     end
 
+    ### CHECK THIS TEST ###
     it "records the time it stopped" do
       stop_time = Time.now
       Time.stub(:now).and_return(stop_time)
@@ -60,7 +61,16 @@ describe TaxiMeter do
       @meter.start
     end
 
-    it "charges $2.50 for the first 1/6 mile (recorded in cents)"
+    ### CHECK THIS TEST ###
+    it "charges $2.50 for the first 1/6 mile (recorded in cents)" do
+      @meter.miles_driven = 0.1
+      expect(@meter.amount_due).to eq(2.50)
+
+      #charges 2.40 per mile prorated for each additional 1/6 mile
+      @meter.miles_driven = 1.0
+      expect(@meter.amount_due).to eq(4.50)
+      # (2.50+(2.40*(5.0/6.0))).ceil_to(2)
+    end
   end
 
 
@@ -74,7 +84,40 @@ describe TaxiMeter do
       @meter.start
     end
 
-    it "has a minimum fare of $13.10"
+    it "has a minimum fare of $13.10" do
+      @meter.miles_driven = 1.0
+      expect(@meter.amount_due).to eq(13.10)
+      @meter.miles_driven = 2.0
+      expect(@meter.amount_due).to eq(13.10)
+      @meter.miles_driven = 6.0
+      expect(@meter.amount_due).to eq(16.50)
+    end
+  end
+
+  context "The taxi meter starts between 9pm and 4am" do
+    before do
+      @meter = TaxiMeter.new
+
+      start_time = Time.parse("9:30 pm")
+      Time.stub(:now).and_return(start_time)
+      @meter.start
+
+      stop_time = Time.parse("10:30 pm")
+      Time.stub(:now).and_return(stop_time)
+      @meter.stop
+
+
+    end
+
+    it "charges an additional $1.00 if start between 9pm and 4am" do
+
+      @meter.miles_driven = 0.1
+      expect(@meter.amount_due).to eq(32.50)
+
+      @meter.miles_driven = 1.0
+      expect(@meter.amount_due).to eq(34.50)
+    end
+
   end
 
 end
--- taxi_meter.rb
+++ taxi_meter.rb
@@ -1,13 +1,15 @@
+require 'pry'
+
 class TaxiMeter
 
-  attr_accessor :amount_due, :miles_driven
-  attr_reader :start_time, :stop_time
+  attr_accessor :miles_driven, :start_time, :stop_time, :airport
 
-  def initialize
-    @amount_due = 0
+  def initialize(airport: false)
     @miles_driven = 0
     @start_time = nil
     @stop_time = nil
+    @wait_time = 0
+    @airport = airport
   end
 
   def start
@@ -18,4 +20,46 @@ class TaxiMeter
     @stop_time = Time.now
   end
 
+  def amount_due
+    # sprintf("%.2f",item[:price].to_f * item[:quantity])
+
+    charge_per_sixth = 2.40 / 6.0
+
+    if @miles_driven == 0
+      total_charge = 0.00
+    elsif @miles_driven <= (1.0/6.0)
+      total_charge = 2.50
+    else
+      if (@miles_driven - (1.0/6.0) / (1.0/6.0)) % 1.0 == 0
+        total_charge = (@miles_driven - (1.0/6.0)) / (1.0/6.0) * charge_per_sixth + 2.50
+      else
+        total_charge = (@miles_driven - (1.0/6.0)) / (1.0/6.0) * charge_per_sixth + charge_per_sixth + 2.50
+      end
+    end
+
+    if @start_time && @stop_time
+      @wait_time = @stop_time - @start_time
+      charge_per_wait_min = 29.00 / 60
+      wait_charge = (@wait_time / 60).ceil * charge_per_wait_min
+      total_charge += wait_charge
+
+      if @start_time.hour < 4 || @start_time.hour >= 21
+        total_charge += 1.00
+      end
+    end
+
+    if @airport && total_charge < 13.10
+      total_charge = 13.10
+    end
+
+    total_charge.ceil_to(2)
+  end
 end
+
+class Float
+  def ceil_to(x)
+    # Rounds up to the nearest x decimal place
+    (self * 10**x).ceil.to_f / 10**x
+  end
+end
+
