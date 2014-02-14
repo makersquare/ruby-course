f90e0418b8a965732e41675ac2e57ccbb0b24b66
zedwarth <zedwarth@gmail.com>
2014-02-14 10:13:55 -0600

    Rest to soft-tabs

--- spec/taxi_meter_spec.rb
+++ spec/taxi_meter_spec.rb
@@ -2,87 +2,87 @@ require './taxi_meter.rb'
 
 describe TaxiMeter do
 
-	def one_sixth
-		1.0 / 6.0
-	end
+  def one_sixth
+    1.0 / 6.0
+  end
 
-	describe "Basic functionality" do
+  describe "Basic functionality" do
 
-		before do
-			@meter = TaxiMeter.new
-		end
+    before do
+      @meter = TaxiMeter.new
+    end
 
-		it "starts at zero" do
-			@meter.amount_due = 0
-			@meter.miles_driven = 0
-		end
+    it "starts at zero" do
+      @meter.amount_due = 0
+      @meter.miles_driven = 0
+    end
 
-		it "can start and stop" do
-			@meter.start
-			expect(@meter.start_time).to_not be_nil
-			expect(@meter.stop_time).to be_nil
+    it "can start and stop" do
+      @meter.start
+      expect(@meter.start_time).to_not be_nil
+      expect(@meter.stop_time).to be_nil
 
-			@meter.stop
-			expect(@meter.stop_time).to_not be_nil
-		end
+      @meter.stop
+      expect(@meter.stop_time).to_not be_nil
+    end
 
-		it "records the time it started" do
-			# We want to freeze time to the point when the meter starts
-			start_time = Time.now
-			Time.stub(:now).and_return(start_time)
+    it "records the time it started" do
+      # We want to freeze time to the point when the meter starts
+      start_time = Time.now
+      Time.stub(:now).and_return(start_time)
 
-			# This should grab the current time
-			@meter.start
+      # This should grab the current time
+      @meter.start
 
-			# Re-stub Time to be 5 minutes into the future
-			Time.stub(:now).and_return(start_time + 5 * 60)
+      # Re-stub Time to be 5 minutes into the future
+      Time.stub(:now).and_return(start_time + 5 * 60)
 
-			# Once started, start_time shouldn't rely on the current time
-			expect(@meter.start_time).to eq(start_time)
-		end
+      # Once started, start_time shouldn't rely on the current time
+      expect(@meter.start_time).to eq(start_time)
+    end
 
-		it "records the time it stopped" do
-			# Stub the stopping time
-			stop_time = Time.now
-			Time.stub(:now).and_return(stop_time)
+    it "records the time it stopped" do
+      # Stub the stopping time
+      stop_time = Time.now
+      Time.stub(:now).and_return(stop_time)
 
-			# This should grab the current time
-			@meter.stop
+      # This should grab the current time
+      @meter.stop
 
-			# Re-stub Time to be 5 minutes into the future
-			Time.stub(:now).and_return(stop_time + 5 * 60)
+      # Re-stub Time to be 5 minutes into the future
+      Time.stub(:now).and_return(stop_time + 5 * 60)
 
-			# Once started, start_time shouldn't rely on the current time
-			expect(@meter.stop_time).to eq(stop_time)
-		end
-	end
+      # Once started, start_time shouldn't rely on the current time
+      expect(@meter.stop_time).to eq(stop_time)
+    end
+  end
 
-	context "The taxi meter starts" do
-		before do
-			# We want to freeze time to the point when the meter starts
-			@start_time = Time.now
-			Time.stub(:now).and_return(@start_time)
+  context "The taxi meter starts" do
+    before do
+      # We want to freeze time to the point when the meter starts
+      @start_time = Time.now
+      Time.stub(:now).and_return(@start_time)
 
-			@meter = TaxiMeter.new
-			@meter.start
-		end
+      @meter = TaxiMeter.new
+      @meter.start
+    end
 
-		xit "charges $2.50 for the first 1/6 mile (recorded in cents)" do
-		end
-	end
+    xit "charges $2.50 for the first 1/6 mile (recorded in cents)" do
+    end
+  end
 
 
-	context "The taxi meter starts from ABIA" do
-		before do
-			# We want to freeze time to the point when the meter starts
-			start_time = Time.now
-			Time.stub(:now).and_return(start_time)
+  context "The taxi meter starts from ABIA" do
+    before do
+      # We want to freeze time to the point when the meter starts
+      start_time = Time.now
+      Time.stub(:now).and_return(start_time)
 
-			@meter = TaxiMeter.new(airport: true)
-			@meter.start
-		end
+      @meter = TaxiMeter.new(airport: true)
+      @meter.start
+    end
 
-		xit "has a minimum fare of $13.10" do
-		end
-	end
+    xit "has a minimum fare of $13.10" do
+    end
+  end
 end
