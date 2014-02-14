45acb1f3f9144aa6e255c97035edd98681a4b426
Neil Mithipati <neil.mithipati@gmail.com>
2014-02-14 09:36:57 -0600

    Add passing methods: start_time and stop_time

--- taxi_meter.rb
+++ taxi_meter.rb
@@ -1,3 +1,17 @@
 class TaxiMeter
-  # TODO
+  attr_accessor :amount_due, :miles_driven, :start_time, :stop_time
+
+  def initialize
+    @amount_due = 0
+    @miles_driven = 0
+  end
+
+  def start
+    @start_time = Time.now
+  end
+
+  def stop
+    @stop_time = Time.now
+  end
+
 end
