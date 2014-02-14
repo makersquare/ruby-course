159c9161258896ed240444526abf325f7b271ec8
vagrant <vagrant@student-dev-box>
2014-02-14 15:51:58 +0000

    Make methods and fix tests to pass initialize and start/stop times

--- taxi_meter.rb
+++ taxi_meter.rb
@@ -1,3 +1,21 @@
 class TaxiMeter
-  # TODO
+ 
+  attr_reader :amount_due, :miles_driven, :start_time, :stop_time
+  
+  def initialize(airport:false)
+  	@amount_due = 0
+  	@miles_driven = 0
+  	@airport = false
+  	@start_time = nil
+  	@stop_time = nil
+  end
+
+  def start
+  	 @start_time = Time.now
+  end
+
+  def stop
+  	@stop_time = Time.now
+  end
+
 end
