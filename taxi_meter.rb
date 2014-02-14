6d58ee92011b22ab424c0fe631319e14403b32ca
Matt Denton <geositta@gmail.com>
2014-02-14 11:07:50 -0600

    first test passed

--- taxi_meter.rb
+++ taxi_meter.rb
@@ -1,3 +1,21 @@
 class TaxiMeter
-  # TODO
+
+	attr_accessor :meter, :amount_due, :miles_driven
+  
+  def initialize
+  	@meter=meter
+  	@amount_due=amount_due
+  	@miles_driven=miles_driven
+  end
+
+  def amount_due
+  	@amount_due=0
+  end
+
+  def miles_driven
+  	@miles_driven=0
+  end
+
+
+  	
 end
