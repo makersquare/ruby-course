1b977efb2edca715c53c6903a635e563f529195d
Michael Young <michaeljy@gmail.com>
2014-02-14 09:22:02 -0600

    Basic functionality tests are green

--- taxi_meter.rb
+++ taxi_meter.rb
@@ -1,3 +1,21 @@
 class TaxiMeter
-  # TODO
+
+  attr_accessor :amount_due, :miles_driven
+  attr_reader :start_time, :stop_time
+
+  def initialize
+    @amount_due = 0
+    @miles_driven = 0
+    @start_time = nil
+    @stop_time = nil
+  end
+
+  def start
+    @start_time = Time.now
+  end
+
+  def stop
+    @stop_time = Time.now
+  end
+
 end
