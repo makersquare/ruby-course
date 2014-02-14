83327e97dff81d47f7cfe8faba086483bbf567cc
Kathryn Beaty <kabeaty@gmail.com>
2014-02-14 15:38:55 -0600

    Add logic for if taxi originates from airport

--- taxi_meter.rb
+++ taxi_meter.rb
@@ -1,16 +1,19 @@
 class TaxiMeter
   attr_accessor :miles_driven, :start_time, :stop_time
+  attr_reader :airport
   attr_writer :amount_due
 
-  def initialize
+  def initialize(airport: false)
     # Amount due is recorded in cents
     @amount_due = 0
     @miles_driven = 0
     @start_time = nil
     @stop_time = nil
+    @airport = airport
   end
 
   def amount_due
+      # Find wait time in minutes between start time and current time.
       t = Time.now
       if t.hour > @start_time.hour
         wait_time = ((60 - @start_time.min) + t.min) + (60 * ((t.hour - @start_time.hour) - 1))
@@ -22,12 +25,24 @@ class TaxiMeter
         wait_time = (minutes - minutes2)
       end
 
+      # Calculate amount due with miles driven and wait time.
       if @miles_driven <= (1.0 / 6.0)
         @amount_due = (250 + ((2900 / 60.0) * wait_time)).round(0)
       elsif @miles_driven > (1.0 / 6.0)
         one_sixth = (@miles_driven * 6) - 1
         @amount_due = ((250 + (one_sixth * 40)) + ((2900 / 60.0) * wait_time)).round(0)
       end
+
+      if self.airport != true
+        @amount_due
+      else
+        if @amount_due < 1310
+          @amount_due = 1310
+        else
+          @amount_due
+        end
+      end
+
   end
 
   def start
