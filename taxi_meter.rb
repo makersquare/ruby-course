a02397b188ae1e383ace033d16f0eb2b8b2f31a6
Ian White <ianwhite@utexas.edu>
2014-02-14 16:19:30 -0600

    create #time_amout_due, #mileage_amount_due, #wait_time to clean up #amount_due

--- taxi_meter.rb
+++ taxi_meter.rb
@@ -2,7 +2,7 @@ class TaxiMeter
   attr_accessor :amount_due, :miles_driven, :start_time, :wait_time
   attr_reader :stop_time
 
-  def initialiaze
+  def initialize
     @amount_due = 0
     @miles_driven = 0
     @start_time = nil
@@ -18,8 +18,23 @@ class TaxiMeter
     @stop_time = Time.now
   end
 
-  def amount_due
+  def wait_time
+    return 0 if @stop_time.nil? || @start_time.nil?
 
-    (2.10 + 0.4 * (@miles_driven * 6.0)) * 100
+    (@stop_time - @start_time) / 60
+  end
+
+  def time_amount_due
+    wait_time * (2900 / 60.0)
+  end
+
+  def mileage_amount_due
+    return 250 if @miles_driven.zero?
+
+    (210 + 40 * (@miles_driven * 6.0))
+  end
+
+  def amount_due
+    mileage_amount_due + time_amount_due
   end
 end
