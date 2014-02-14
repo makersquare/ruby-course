61c3f1c84088400faf60ba6b714964911e44ee0f
Gilbert <gilbertbgarza@gmail.com>
2014-02-14 08:09:34 -0600

    Taxi meter project

---
+++ spec/taxi_meter_spec.rb
@@ -0,0 +1,74 @@
+require './taxi_meter.rb'
+
+describe TaxiMeter do
+
+  def one_sixth
+    1.0 / 6.0
+  end
+
+  describe "Basic functionality" do
+
+    before do
+      @meter = TaxiMeter.new
+    end
+
+    it "starts at zero" do
+      @meter.amount_due = 0
+      @meter.miles_driven = 0
+    end
+
+    it "can start and stop" do
+      @meter.start
+      expect(@meter.start_time).to_not be_nil
+      expect(@meter.stop_time).to be_nil
+
+      @meter.stop
+      expect(@meter.stop_time).to_not be_nil
+    end
+
+    it "records the time it started" do
+      # We want to freeze time to the point when the meter starts
+      start_time = Time.now
+      Time.stub(:now).and_return(start_time)
+
+      # This should grab the current time
+      @meter.start
+
+      # Re-stub Time to be 5 minutes into the future
+      Time.stub(:now).and_return(start_time + 5 * 60)
+
+      # Once started, start_time shouldn't rely on the current time
+      expect(@meter.start_time).to eq(start_time)
+    end
+
+    it "records the time it stopped"
+  end
+
+  context "The taxi meter starts" do
+    before do
+      # We want to freeze time to the point when the meter starts
+      @start_time = Time.now
+      Time.stub(:now).and_return(@start_time)
+
+      @meter = TaxiMeter.new
+      @meter.start
+    end
+
+    it "charges $2.50 for the first 1/6 mile (recorded in cents)"
+  end
+
+
+  context "The taxi meter starts from ABIA" do
+    before do
+      # We want to freeze time to the point when the meter starts
+      start_time = Time.now
+      Time.stub(:now).and_return(start_time)
+
+      @meter = TaxiMeter.new(airport: true)
+      @meter.start
+    end
+
+    it "has a minimum fare of $13.10"
+  end
+
+end
---
+++ taxi_meter.rb
@@ -0,0 +1,3 @@
+class TaxiMeter
+  # TODO
+end
