3e7cf98d32ebd3f0274f5bff9602de4b29d5028d
zedwarth <zedwarth@gmail.com>
2014-02-14 10:08:18 -0600

    Pass start and stop tests.

--- taxi_meter.rb
+++ taxi_meter.rb
@@ -1,5 +1,13 @@
 class TaxiMeter
 	attr_accessor :amount_due, :miles_driven
+	attr_reader :start_time, :stop_time
 
+	def start
+		@start_time = Time.now
+	end
+
+	def stop
+		@stop_time = Time.now
+	end
 
 end
