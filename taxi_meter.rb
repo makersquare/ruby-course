166871a0683d3da224137b43c263356f452d5c65
Joseph Chappell <jdchappe@gmail.com>
2014-02-14 09:39:32 -0600

    implement instance variables amount_due,miles_driven

--- taxi_meter.rb
+++ taxi_meter.rb
@@ -1,3 +1,16 @@
 class TaxiMeter
-  # TODO
+
+  attr_accessor :amount_due, :miles_driven
+
+  def initialize
+    @amount_due = 0
+    @miles_driven = 0
+  end
+
+  def one_sixth
+    1.0 / 6.0
+  end
+
+
 end
+
