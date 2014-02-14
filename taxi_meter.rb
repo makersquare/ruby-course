aee6d4d0a39e0dc7f300fd34ea814092dc7debe5
Kathryn Beaty <kabeaty@gmail.com>
2014-02-14 10:04:45 -0600

    Add custom getter method for amount_due

--- taxi_meter.rb
+++ taxi_meter.rb
@@ -1,9 +1,27 @@
 class TaxiMeter
-  attr_accessor :amount_due, :miles_driven
+  attr_accessor :miles_driven, :start_time, :stop_time
+  attr_writer :amount_due
 
   def initialize
+    # Amount due is recorded in cents
     @amount_due = 0
     @miles_driven = 0
+    @start_time = nil
+    @stop_time = nil
+  end
+
+  def amount_due
+    if self.miles_driven <= (1.0 / 6.0)
+      @amount_due = 250
+    end
+  end
+
+  def start
+    self.start_time = Time.now
+  end
+
+  def stop
+    self.stop_time = Time.now
   end
 
 end
