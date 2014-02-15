84dc5687aed75b6d3b8d011e10936d8fd6bfe25b
Gilad Salmon <Gilad.Salmon@gmail.com>
2014-02-15 18:53:11 +0000

    Clean up some code with new methods

--- taxi_meter.rb
+++ taxi_meter.rb
@@ -1,8 +1,7 @@
 require 'time'
 
 class TaxiMeter
-  attr_reader :start_time, :stop_time
-  attr_accessor :miles_driven
+  attr_accessor :start_time, :stop_time, :miles_driven, :amount_due
 
   def initialize(airport: false)
     @amount_due = 0
@@ -10,16 +9,12 @@ class TaxiMeter
     @start_time = nil
     @stop_time = nil
     @airport = airport
-    @prorate_distance = 1.0/6.0
+    @prorate = 1.0/6.0
   end
 
   def start
     @start_time = Time.now
-
-    if @airport then @amount_due += 1310 end
-
     if @start_time >= Time.parse('9 pm') || @start_time <= Time.parse('4 am') then @amount_due += 100 end
-
   end
 
   def stop
@@ -27,20 +22,27 @@ class TaxiMeter
   end
 
   def amount_due
-    if @miles_driven == 0
-      @amount_due
-    elsif @miles_driven <= @prorate_distance
-      @amount_due += 250
-    else
-      @amount_due += 250
-      @miles_driven -= @prorate_distance
-      distance_charges(@miles_driven)
-    end
+    total = @amount_due + distance_charge + time_charge
+    total < 1310 && @airport ? 1310 : total.to_i
+
   end
 
+  def distance_charge
+    distance = @miles_driven * 6
+    charge = 0
 
-  def distance_charges(distance)
-    distance <= @prorate_distance ? @amount_due += (240 * @prorate_distance).to_i : distance_charges(distance - @prorate_distance)
+    while distance > 0.00
+      # Charge $2.50 for first 1/6 mile and $0.40 for every 1/6 mile after that
+      distance <= 1 ? charge += 250 : charge += 40
+      distance -= 1
+    end
+
+    charge
+  end
+
+  def time_charge
+    @stop_time ? time = @stop_time : time = Time.now
+    ((time - start_time) / 60.0).ceil * (2900/60.0)
   end
 
 
