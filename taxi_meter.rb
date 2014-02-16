eca8ae7b3873a3d1d7ab38f1d821bbf6f310ca5e
Ian White <ianwhite@utexas.edu>
2014-02-16 12:20:24 -0600

    clean up unneccessary comments

--- taxi_meter.rb
+++ taxi_meter.rb
@@ -5,8 +5,6 @@ class TaxiMeter
   def initialize(airport: false)
     @amount_due = 0
     @miles_driven = 0
-    @start_time = nil
-    @stop_time = nil
     @wait_time = 0
     @airport = airport
   end
@@ -20,9 +18,7 @@ class TaxiMeter
   end
 
   def wait_time
-    # return 0 if @stop_time.nil? || @start_time.nil?
-
-    (Time.now - @start_time) / 60
+      (Time.now - @start_time) / 60
   end
 
   def time_amount_due
@@ -30,13 +26,11 @@ class TaxiMeter
   end
 
   def mileage_amount_due
-    # return 250 if @miles_driven.zero?
     return 0 if @miles_driven.zero?
     210 + 40 * (@miles_driven * 6.0).round
   end
 
   def airport_surcharge
-    # return 250 if @miles_driven.zero? && airport == false
     return 1310 if (@amount_due < 1310 && @airport == true); 0
   end
 
