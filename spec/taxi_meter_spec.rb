85961c588e440c5e185e547576b9ab03d1834822
Gilad Salmon <Gilad.Salmon@gmail.com>
2014-02-16 20:14:32 -0600

    Add and fix tests and refactor code

--- spec/taxi_meter_spec.rb
+++ spec/taxi_meter_spec.rb
@@ -13,8 +13,8 @@ describe TaxiMeter do
     end
 
     it "starts at zero" do
-      expect(@meter.amount_due).to eq(0)
-      expect(@meter.miles_driven).to eq(0)
+      @meter.amount_due = 0
+      @meter.miles_driven = 0
     end
 
     it "can start and stop" do
@@ -42,6 +42,13 @@ describe TaxiMeter do
     end
 
     it "records the time it stopped" do
+    # We want to freeze time to the point when the meter starts
+      start_time = Time.now
+      Time.stub(:now).and_return(start_time)
+
+    # This should grab the current time
+      @meter.start
+
     # Stub the stop time
       stop_time = Time.now
       Time.stub(:now).and_return(stop_time)
@@ -51,7 +58,7 @@ describe TaxiMeter do
       expect(@meter.stop_time).to eq(stop_time)
     end
 
-    it "records the passage of time" do
+    it "records the passage of time between start and stop" do
       # Freeze time to the point when the meter starts
       start_time = Time.now
       Time.stub(:now).and_return(start_time)
@@ -66,10 +73,13 @@ describe TaxiMeter do
       @meter.stop
 
       # Test stop time
-      expect(@meter.stop_time).to eq(start_time + 5 * 60)
+      expect(@meter.stop_time - @meter.start_time).to eq(300)
     end
 
-
+    it "can't be stopped if the meter hasn't started yet" do
+      @meter.stop
+      expect(@meter.stop_time).to be_nil
+    end
 
   end
 
@@ -83,13 +93,17 @@ describe TaxiMeter do
       @meter.start
     end
 
+    it "charges for the first 1/6 mile as soon as the meter is turned on" do
+      @meter.miles_driven = 0
+      expect(@meter.amount_due).to eq(250)
+    end
   end
 
 
   context "The taxi meter starts from ABIA" do
     before do
-      # We want to freeze time to the point when the meter starts
-      start_time = Time.now
+      # We want to stub time to early in the day
+      start_time = Time.parse('11 am')
       Time.stub(:now).and_return(start_time)
 
       @meter = TaxiMeter.new(airport: true)
@@ -101,7 +115,7 @@ describe TaxiMeter do
     end
   end
 
-  context "The taxi meter charges more or less for different start times" do
+  context "The taxi meter charges more for different start times" do
 
     it "charges an additional $1.00 (recorded in cents) between 9 pm and 4 am" do
       # Set time to 11 pm when the meter starts
@@ -114,8 +128,8 @@ describe TaxiMeter do
       expect(@meter.amount_due).to eq(350)
     end
 
-    it "does not charge an additional $1.00 (recorded in cents)" do
-      # Set time to 11 pm when the meter starts
+    it "does not charge an additional $1.00 (recorded in cents) at other times" do
+      # Set time to 4 pm when the meter starts
       start_time = Time.parse('4 pm')
       Time.stub(:now).and_return(start_time)
 
@@ -128,6 +142,10 @@ describe TaxiMeter do
 
   context "It charges $2.50 for the first 1/6 mile and $2.40 for each additional mile" do
     before do
+      # Set time to 1 pm when the meter starts
+      start_time = Time.parse('1 pm')
+      Time.stub(:now).and_return(start_time)
+
       @meter = TaxiMeter.new
       @meter.start
     end
@@ -141,7 +159,60 @@ describe TaxiMeter do
       @meter.miles_driven = one_sixth * 2
       expect(@meter.amount_due).to eq(290)
     end
+  end
 
+
+  context "The taxi meter charges for wait time" do
+    before do
+      # Set time to 10 am when the meter starts
+      start_time = Time.parse('10 am')
+      Time.stub(:now).and_return(start_time)
+
+      @meter = TaxiMeter.new
+      @meter.start
+    end
+
+    it "charges for 5 minutes of wait time and travelling less than 1/6 mile" do
+
+    # Re-stub Time to be 5 minutes into the future
+    Time.stub(:now).and_return(@meter.start_time + 5 * 60)
+
+    expect(@meter.amount_due).to eq(492)
+    end
+
+    it "charges for an hour of wait time and travelling 0.2 miles" do
+
+    # Re-stub Time to be 5 minutes into the future
+    Time.stub(:now).and_return(@meter.start_time + 60 * 60)
+    @meter.miles_driven = 0.2
+
+    expect(@meter.amount_due).to eq(3190)
+    end
+  end
+
+  context "It charges the correct amount after the meter is stopped" do
+    before do
+      # Set time to 10 am when the meter starts
+      start_time = Time.parse('10 am')
+      Time.stub(:now).and_return(start_time)
+
+      @meter = TaxiMeter.new
+      @meter.start
+      @meter.miles_driven = 3.7
+    end
+
+    it "uses the stop time and not the current time" do
+    # Set time to 10:10 am when the meter stops
+    stop_time = Time.parse('10:10 am')
+    Time.stub(:now).and_return(stop_time)
+    @meter.stop
+
+    # Set time to 10:15 am when the amount due is called
+    test_time = Time.parse('10:15 am')
+    Time.stub(:now).and_return(test_time)
+
+    expect(@meter.amount_due).to eq(1613)
+    end
   end
 
 end
--- taxi_meter.rb
+++ taxi_meter.rb
@@ -1,7 +1,8 @@
 require 'time'
 
 class TaxiMeter
-  attr_accessor :start_time, :stop_time, :miles_driven, :amount_due
+  attr_accessor :start_time, :stop_time, :miles_driven
+  attr_writer :amount_due
 
   def initialize(airport: false)
     @amount_due = 0
@@ -9,27 +10,34 @@ class TaxiMeter
     @start_time = nil
     @stop_time = nil
     @airport = airport
-    @prorate = 1.0/6.0
+    @night_start = '9 pm'
+    @night_end = '4 am'
+    @night_surcharge = 100
   end
 
   def start
     @start_time = Time.now
-    if @start_time >= Time.parse('9 pm') || @start_time <= Time.parse('4 am') then @amount_due += 100 end
+    if @start_time >= Time.parse(@night_start) || @start_time <= Time.parse(@night_end)
+      @amount_due += @night_surcharge
+    end
   end
 
   def stop
-    @stop_time = Time.now
+    if @start_time then @stop_time = Time.now end
   end
 
   def amount_due
+    unless @start_time then return @amount_due end
     total = @amount_due + distance_charge + time_charge
-    total < 1310 && @airport ? 1310 : total.to_i
+    total < 1310 && @airport ? 1310 : total.round
 
   end
 
   def distance_charge
     distance = @miles_driven * 6
     charge = 0
+
+    if distance == 0 then charge += 250 end
 
     while distance > 0.00
       # Charge $2.50 for first 1/6 mile and $0.40 for every 1/6 mile after that
