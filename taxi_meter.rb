71a9c5ab3014088977b723baaeb89cce1cd907b2
Matthew Van Dusen <imagineux@gmail.com>
2014-02-17 00:26:04 -0600

    rework .amount_due logic defaults and checks for strt/stp_time adding @wait_time

--- taxi_meter.rb
+++ taxi_meter.rb
@@ -2,14 +2,17 @@ gem 'rspec', '~> 2.14.0'
 gem 'pry-debugger', '~> 0.2.2'
 
 class TaxiMeter
-  attr_accessor :miles_driven, :amount_due
-  attr_reader :start_time, :stop_time, :sixth
+  attr_accessor :miles_driven, :amount_due, :start_time, :stop_time, :wait_time
+  attr_reader :stop_time, :sixth
 
   def initialize()
     @miles_driven = 0
     @sixth = 1.0 / 6.0
     @base_charge = 210
     @additional_sixth = 40
+    @wait_time = 2900.0 / 60.0
+    @start_time = nil
+    @stop_time = nil
   end
 
   def start
@@ -19,23 +22,32 @@ class TaxiMeter
   def stop
     if @start_time
     @stop_time = Time.now
-  end
-  end
-
-
-
-  def amount_due
-    fare = 0
-    if @miles_driven == 0 
-      fare
-    else
-      # Divide miles driven into sixths 
-      # and remove the first sixth
-      prorated = (@miles_driven / @sixth).ceil 
-
-      fare = @base_charge+ prorated* @additional_sixth
     end
-
-    fare
+  end
+def time
+    if @stop_time
+      @stop_time
+    else
+      Time.now
+    end
+  end
+  def amount_due
+    if @start_time != nil 
+      if @miles_driven <= @sixth 
+      @amount_due = 250
+      else
+        prorated = (@miles_driven / @sixth).ceil
+        fare = @base_charge + prorated * @additional_sixth
+        @amount_due = fare
+      end
+      if @stop_time 
+        @amount_due += wait_time*((@stop_time-@start_time)/60).ceil
+      else
+        @amount_due += wait_time*((Time.now-@start_time)/60).ceil
+      end
+        @amount_due.round(0)
+    else
+      0
+    end          
   end
 end
