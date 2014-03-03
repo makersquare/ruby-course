326728f7ec31248a5d3d6ad9733537b8e2e64d93
Brian Keane <briankeane@Rachels-MacBook-Pro.local>
2014-03-03 15:16:18 -0600

    All tests green! :)

--- diagnostic.rb
+++ diagnostic.rb
@@ -218,5 +218,8 @@ class ArraysAndHashes
     # TODO: Iterate over this array and `puts` each key and value
     # Example: if array is [{ egg: 12 }, { milk: 1 }],
     #          then `puts` both "egg: 12" and "milk: 1"
+    grocery_lists.each do |x|
+      x.each { |k, v| puts "#{k}: #{v}"}
+    end
   end
 end
