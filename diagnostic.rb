cf1fc87b14ac2c4773637ec26d02b6a3ce4eb48e
Brady Bryan <bradybryan@Bradys-MacBook-Pro.local>
2014-03-03 15:06:36 -0600

    First Commit

--- diagnostic.rb
+++ diagnostic.rb
@@ -4,7 +4,13 @@
 # # # # # # #
 
 # TODO: Write a method caled `toggle_oven`
-
+def toggle_oven(status)
+  if status == true
+    return "The oven is now on"
+  else
+    return "The oven is now off"
+  end
+end
 # # # # # # #
 # Problem 2 #
 # # # # # # #
@@ -14,7 +20,7 @@ def multiply(x, y)
 end
 
 def give_me_seven
-  # TODO: Use the `multiply` method
+  return multiply(1,7)
 end
 
 
@@ -27,7 +33,7 @@ module ClassesAndInstances
   class Animal
     attr_reader :name
     def initialize(name)
-      # TODO: Set name
+      @name = name
     end
   end
 
@@ -43,6 +49,9 @@ module ClassesAndInstances
 
     # TODO: Write a method `adopt` that takes one paramater `animal`
     # and adds it to its animals array
+    def adopt(animal)
+      @animals << animal
+    end
   end
 
   # # # # # # # # # # # #
@@ -52,11 +61,11 @@ module ClassesAndInstances
     attr_accessor :size
     # TODO: Fix incorrect use of local and instance variables
     def initialize(initial_size)
-      size = initial_size
+      @size = initial_size
     end
 
     def grow
-      size = size + 1
+      @size = size + 1
     end
   end
 end
@@ -72,13 +81,25 @@ module GettersSetters
     def initialize
       @secret = 50
     end
-    # TODO: Write getter and setter methods for secret
+
+    def secret
+      @secret
+    end
+
+    def secret=(val)
+      @secret = val
+      return @secret
+    end
+
   end
 
   # # # # # # # # # # #
   # Getters/Setters 2 #
   # # # # # # # # # # #
   class Person
+      attr_reader :age
+      attr_writer :secret
+      attr_accessor :name
 
     def initialize(name, age)
       @name = name
@@ -89,21 +110,7 @@ module GettersSetters
     # NOTE: Don't provide any more access than necessary.
     #       For example, don't use attr_accessible when all you really need is attr_writer
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-    def age
-      @age
-    end
 
-    def secret=(value)
-      @secret = value
-    end
-
-    def name
-      @name
-    end
-
-    def name=(value)
-      @name = value
-    end
   end
 end
 
@@ -116,7 +123,6 @@ class ArrayProblems
 
   def self.add_cake_to_array(array)
     # TODO: Complete this method
-    array.push 'cake'
   end
 
   # # # # # # #
@@ -125,7 +131,6 @@ class ArrayProblems
 
   def self.uppercase_third_element(array)
     # TODO: Complete this method
-    array[2].upcase!
   end
 
   # # # # # # #
@@ -134,7 +139,6 @@ class ArrayProblems
 
   def self.iterate_and_print(array)
     # TODO: Iterate over this array and `puts` each element.
-    array.each {|x| puts x}
   end
 
   # # # # # # #
@@ -143,7 +147,6 @@ class ArrayProblems
 
   def self.select_higher(array, min)
     # TODO: Select and return all numbers higher than `min`
-    array.select {|x| x > min }
   end
 
   # # # # # # #
@@ -152,7 +155,6 @@ class ArrayProblems
 
   def self.greet_everyone(people)
     # TODO: Select and return all numbers higher than `min`
-    people.map {|person| "Hello, #{person}" }
   end
 end
 
@@ -165,7 +167,6 @@ class HashProblems
 
   def self.create_empty_hash
     # TODO: Complete this method
-    {}
   end
 
   # # # # # # #
@@ -174,10 +175,6 @@ class HashProblems
 
   def self.create_veggie_color_hash
     # TODO: Complete this method by returning a hash
-    {
-      :tomato => 'red',
-      :kale => 'green'
-    }
   end
 
   # # # # # # #
@@ -186,7 +183,6 @@ class HashProblems
 
   def self.update_father_last_name(hash)
     # TODO: Complete this method by writing A SINGLE LINE
-    hash["father"]["name"][:last] = 'James XXX'
   end
 end
 
@@ -201,10 +197,5 @@ class ArraysAndHashes
     # TODO: Iterate over this array and `puts` each key and value
     # Example: if array is [{ egg: 12 }, { milk: 1 }],
     #          then `puts` both "egg: 12" and "milk: 1"
-    grocery_lists.each {|list|
-      list.each do |item, count|
-        puts "#{item}: #{count}"
-      end
-    }
   end
 end
