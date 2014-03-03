0af2fd7ff5c6571cdc8172fdf369cbcd095f5583
vagrant <vagrant@student-dev-box>
2014-03-03 21:15:20 +0000

    Finished exercises, all green.

--- diagnostic.rb
+++ diagnostic.rb
@@ -209,6 +209,7 @@ class HashProblems
 
   def self.update_father_last_name(hash)
     # TODO: Complete this method by writing A SINGLE LINE
+      hash["father"]["name"][:last] = "James XXX"
   end
 end
 
@@ -223,5 +224,8 @@ class ArraysAndHashes
     # TODO: Iterate over this array and `puts` each key and value
     # Example: if array is [{ egg: 12 }, { milk: 1 }],
     #          then `puts` both "egg: 12" and "milk: 1"
+    grocery_lists.each do |hash|
+      hash.each{ |key,value| puts "#{key}: #{value}"}
+    end
   end
 end
