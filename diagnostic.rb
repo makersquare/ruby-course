64cb18b7624a829d45ee220eb54b0975e0ea4c7e
Brady Bryan <bradybryan@Bradys-MacBook-Pro.local>
2014-03-03 15:43:24 -0600

    test 'know how to create a hash with keys and values' passes

--- diagnostic.rb
+++ diagnostic.rb
@@ -175,7 +175,7 @@ class HashProblems
   # # # # # # #
 
   def self.create_veggie_color_hash
-    # TODO: Complete this method by returning a hash
+    result = {tomato: "red", kale: "green"}
   end
 
   # # # # # # #
