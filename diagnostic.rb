7711db2d84466066d7e3f09cba9f9be4aba7013a
Christopher Palmer <snly2386@gmail.com>
2014-03-03 15:30:24 -0600

    ruby diagnostic

--- diagnostic.rb
+++ diagnostic.rb
@@ -4,6 +4,13 @@
 # # # # # # #
 
 # TODO: Write a method caled `toggle_oven`
+def toggle_oven(status)
+  if status == true
+    return "The oven is now on"
+  else
+    return "The oven is now off"
+end
+  end
 
 # # # # # # #
 # Problem 2 #
@@ -15,6 +22,7 @@ end
 
 def give_me_seven
   # TODO: Use the `multiply` method
+  multiply(3.5,2)
 end
 
 
@@ -28,6 +36,7 @@ module ClassesAndInstances
     attr_reader :name
     def initialize(name)
       # TODO: Set name
+      @name=name
     end
   end
 
@@ -43,6 +52,9 @@ module ClassesAndInstances
 
     # TODO: Write a method `adopt` that takes one paramater `animal`
     # and adds it to its animals array
+    def adopt (animal)
+      @animals << animal
+    end
   end
 
   # # # # # # # # # # # #
@@ -52,11 +64,13 @@ module ClassesAndInstances
     attr_accessor :size
     # TODO: Fix incorrect use of local and instance variables
     def initialize(initial_size)
-      size = initial_size
+        @size = initial_size
+        
     end
 
     def grow
-      size = size + 1
+      @size = @size + 1
+      @size
     end
   end
 end
@@ -73,13 +87,22 @@ module GettersSetters
       @secret = 50
     end
     # TODO: Write getter and setter methods for secret
+    def secret
+      return @secret
+    end
+
+    def secret=(set)
+      @secret = set
+    end
   end
 
   # # # # # # # # # # #
   # Getters/Setters 2 #
   # # # # # # # # # # #
   class Person
-
+    attr_accessor :name
+    attr_writer :secret
+    attr_reader :age
     def initialize(name, age)
       @name = name
       @age = age
@@ -89,16 +112,9 @@ module GettersSetters
     # NOTE: Don't provide any more access than necessary.
     #       For example, don't use attr_accessible when all you really need is attr_writer
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-    def age
-      @age
-    end
 
     def secret=(value)
       @secret = value
-    end
-
-    def name
-      @name
     end
 
     def name=(value)
@@ -116,7 +132,7 @@ class ArrayProblems
 
   def self.add_cake_to_array(array)
     # TODO: Complete this method
-    array.push 'cake'
+    array.push("cake")
   end
 
   # # # # # # #
@@ -143,7 +159,7 @@ class ArrayProblems
 
   def self.select_higher(array, min)
     # TODO: Select and return all numbers higher than `min`
-    array.select {|x| x > min }
+    array.select{|x|  x > min}
   end
 
   # # # # # # #
@@ -152,7 +168,7 @@ class ArrayProblems
 
   def self.greet_everyone(people)
     # TODO: Select and return all numbers higher than `min`
-    people.map {|person| "Hello, #{person}" }
+    people.map {|x| "Hello, #{x}"}
   end
 end
 
@@ -174,10 +190,8 @@ class HashProblems
 
   def self.create_veggie_color_hash
     # TODO: Complete this method by returning a hash
-    {
-      :tomato => 'red',
-      :kale => 'green'
-    }
+    {:tomato => "red",
+      :kale => "green"}
   end
 
   # # # # # # #
@@ -186,7 +200,7 @@ class HashProblems
 
   def self.update_father_last_name(hash)
     # TODO: Complete this method by writing A SINGLE LINE
-    hash["father"]["name"][:last] = 'James XXX'
+    hash["father"]["name"][:last] =  "James XXX"
   end
 end
 
@@ -201,10 +215,9 @@ class ArraysAndHashes
     # TODO: Iterate over this array and `puts` each key and value
     # Example: if array is [{ egg: 12 }, { milk: 1 }],
     #          then `puts` both "egg: 12" and "milk: 1"
-    grocery_lists.each {|list|
-      list.each do |item, count|
-        puts "#{item}: #{count}"
+    grocery_lists.each do |x| 
+      x.each do |a,b| puts "#{a}: #{b}" 
       end
-    }
+    end
   end
 end
