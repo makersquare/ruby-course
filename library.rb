80bd6cd945b98fb60a26b7252957f42e7c638fb9
Ifu Aniemeka <ianiemeka@gmail.com>
2014-03-05 10:33:02 -0600

    Final Commit

--- library.rb
+++ library.rb
@@ -12,7 +12,7 @@ class Book
 
   def check_out
     if @status == "available"
-      @status = 'checked_out'
+      @status = "checked_out"
       true
     else
       false
@@ -40,6 +40,7 @@ class Library
   def initialize(name)
     @name = name
     @books = []
+    @borr_books = {}
   end
 
   def books
@@ -48,31 +49,77 @@ class Library
 
   def register_new_book(title, author)
     new_book = Book.new(title, author)
-    new_book.id = rand(1..100)
+    new_book.id = rand(1..150)
     add_book(title, author)
     @books << new_book
   end
 
-  def add_book(title, author)
-  end
+   def add_book(title, author)
+   end
 
   def check_out_book(book_id, borrower)
+    borr_names = @borr_books.values
+    name_iter = 0
+
+    borr_names.each do |patron|
+      if borrower == patron
+        name_iter += 1
+      end
+    end
+
+    if name_iter >= 2
+      return nil
+    end
 
     @books.each do |book|
       if book.id == book_id
-      book.check_out
-      return book
+        if book.status == 'available'
+          book.check_out
+          @borr_books[book.id] = borrower
+          return book
+        end
+      end
+    end
+    return nil
+  end
+
+  def get_borrower(book_id)
+    @borr_books[book_id].name
+  end
+
+  def check_in_book(book)
+    book.check_in
+    books_out = @borr_books.keys
+
+    books_out.each do |bookID|
+      if book.id == bookID
+        @borr_books.delete(bookID)
       end
     end
 
   end
 
-  def check_in_book(book)
-  end
-
   def available_books
+    books_in = []
+
+    @books.each do |book|
+      if book.status == 'available'
+        books_in << book
+      end
+    end
+
+    return books_in
   end
 
   def borrowed_books
+    books_out = []
+    @books.each do |book|
+      if book.status == "checked_out"
+        books_out << book
+      end
+    end
+
+    return books_out
   end
+
 end
