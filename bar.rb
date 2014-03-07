f213f3a5799efcb88dd0008546d10fb0db430750
vagrant <vagrant@student-dev-box>
2014-03-07 21:28:08 +0000

    Initial commit

--- bar.rb
+++ bar.rb
@@ -1,4 +1,29 @@
 require 'time' # you're gonna need it
 
 class Bar
+
+  attr_reader :name, :menu_items
+  attr_accessor :happy_discount
+
+  def initialize(name)
+    @name = name
+    @menu_items = []
+    @happy_discount = 0
+  end
+
+  def add_menu_item(item, price)
+    new_item = Item.new(item, price)
+    @menu_items << new_item
+  end
+
+
+end
+
+class Item
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
 end
