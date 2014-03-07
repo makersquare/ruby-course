66af17a08372089102e9711c837413a7d40c0b89
Jose R. Borja <chakaitos@gmail.com>
2014-03-07 17:07:13 -0600

    Complete tests 5-7

--- bar.rb
+++ bar.rb
@@ -2,14 +2,34 @@ require 'time' # you're gonna need it
 
 class Bar
 	attr_reader :name
-	attr_accessor :menu_items
+	attr_accessor :menu_items, :happy_discount
 
 	def initialize(name)
 		@name = name
 		@menu_items = []
+		@happy_discount = 0
 	end
 
 	def add_menu_item(name, price)
-		@menu_items << { name => price }
+		@menu_items << item = Item.new(name, price)
+	end
+
+	def happy_discount
+		unless :happy_hour? == false
+			@happy_discount
+		end
+	end
+
+	# def happy_hour?
+		
+	# end
+end
+
+class Item
+	attr_accessor :name, :price #name maybe reader
+
+	def initialize(name, price)
+		@name = name
+		@price = price
 	end
 end
