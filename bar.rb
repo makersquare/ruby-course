6735b2a8d29361a926e10d1ee1030edbcc518034
Drew Garcia <drewgarcia23@gmail.com>
2014-03-07 16:17:34 -0600

    Getting changes

--- bar.rb
+++ bar.rb
@@ -2,13 +2,31 @@ require 'time' # you're gonna need it
 
 class Bar
   attr_reader :name
+  attr_accessor :menu_items
+
   def initialize(name)
     @name = name
+    @menu_items = []
   end
-  def menu_items
-    @items = []
-  end
-  def add_menu_item(item_name, price)
 
+  def add_menu_item(item_name, price)
+    @menu_items << {item_name => price}
   end
+
+  def name
+    @menu_items.each do |x|
+      x.each do |k,v|
+        puts k
+      end
+    end
+  end
+
 end
+
+# class Item
+#   def initialize(name, price)
+#     @name = name
+#     @price = price
+#   end
+# end
+
