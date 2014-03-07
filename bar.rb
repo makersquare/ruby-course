c0ccc35177c0e0cae2557723415c629597a1488d
Parth Shah <parthshahva@gmail.com>
2014-03-07 15:25:08 -0600

    passing first 3 tests

--- bar.rb
+++ bar.rb
@@ -1,4 +1,25 @@
 require 'time' # you're gonna need it
 
 class Bar
+  attr_reader :name
+  attr_accessor :menu_items, :happy_discount
+  def initialize(name)
+    @name = name
+    @menu_items = []
+  end
+  def add_menu_item(name, price)
+    @menu_items.push(Item.new(name, price))
+  end
+  def happy_discount(discount=0)
+    discount
+  end
+end
+
+
+class Item
+attr_reader :name, :price
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
 end
