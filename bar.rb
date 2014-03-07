285b20bdd65602ba4fb6633e990381bfadf19d66
Drew Garcia <drewgarcia23@gmail.com>
2014-03-07 15:28:34 -0600

    My work

--- bar.rb
+++ bar.rb
@@ -1,4 +1,14 @@
 require 'time' # you're gonna need it
 
 class Bar
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+  def menu_items
+    @items = []
+  end
+  def add_menu_item(item_name, price)
+
+  end
 end
