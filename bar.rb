ae621c68b0a62acd22af38ba66b73cc1fad8fb62
Jose R. Borja <chakaitos@gmail.com>
2014-03-07 16:17:58 -0600

    Complete tests 1-4

--- bar.rb
+++ bar.rb
@@ -1,4 +1,15 @@
 require 'time' # you're gonna need it
 
 class Bar
+	attr_reader :name
+	attr_accessor :menu_items
+
+	def initialize(name)
+		@name = name
+		@menu_items = []
+	end
+
+	def add_menu_item(name, price)
+		@menu_items << { name => price }
+	end
 end
