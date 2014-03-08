b3d984b6414fd7657eda8a6a5714aa72d5e3d36e
Brian Keane <lonesomewhistle@gmail.com>
2014-03-07 19:36:49 -0600

    Added happy_discount and set its default to 0

--- bar.rb
+++ bar.rb
@@ -1,12 +1,13 @@
 require 'time' # you're gonna need it
 
 class Bar
-  attr_reader :name
+  attr_reader :name, :happy_discount
   attr_accessor :menu_items
 
   def initialize(name)
     @name = name
     @menu_items = []
+    @happy_discount = 0
   end
 
   def add_menu_item(name, price)
--- spec/bar_spec.rb
+++ spec/bar_spec.rb
@@ -30,14 +30,14 @@ describe Bar do
     expect(@bar.menu_items.count).to eq(2)
   end
 
-  xit "can retrieve menu items" do
+  it "can retrieve menu items" do
     @bar.add_menu_item('Little Johnny', 9.95)
     item = @bar.menu_items.first
     expect(item.name).to eq 'Little Johnny'
     expect(item.price).to eq 9.95
   end
 
-  xit "has a default happy hour discount of zero" do
+  it "has a default happy hour discount of zero" do
     expect(@bar.happy_discount).to eq 0
   end
 
