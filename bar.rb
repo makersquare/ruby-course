bb0e9d4e5ce4051184499c32610daca69af98241
Antonio Felguerez <agfelgue@gmail.com>
2014-03-08 10:52:55 -0600

    Fixed the following tests: Bar initializes with a name (added instance var) Bar cannot change its name (attr_reader vs attr_accessor) Bar initializes with an empty menu (added @menu_items = [] to initialize method) Bar can add menu items (added method that pushes Item to @menu_items) Bar can retrieve its menu items (Added Item attr_accessors) Bar has a default happy hour discount of 0 (Added happy_discount setter that defaults to 0) Bar can set its happy hour discount (already solved with above fix) Currently failing on the test 'Bar only returns a discount when it's happy hour'

--- bar.rb
+++ bar.rb
@@ -1,4 +1,28 @@
 require 'time' # you're gonna need it
 
 class Bar
+	attr_reader :name, :menu_items
+	attr_writer :happy_discount
+	def initialize(name)
+		@name = name
+		@menu_items = []
+	end
+
+	def add_menu_item(item, price)
+		@item = item
+		@price = price
+		@menu_items << Item.new(item, price)
+	end
+	
+	def happy_discount
+		@happy_discount || 0
+	end
+end
+
+class Item
+	attr_reader :name, :price
+	def initialize(name, price)
+		@name = name
+		@price = price
+	end
 end
