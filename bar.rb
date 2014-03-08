0738d6b3b2b10a806be0ca0f087e37b59543ba83
vagrant <vagrant@student-dev-box>
2014-03-08 17:32:41 +0000

    Create happy_hour? method set to a placeholder value of true. Create happy_discount getter method that returns @happy_discount if happy_hour? is true, 0 otherwise

--- bar.rb
+++ bar.rb
@@ -2,7 +2,7 @@ require 'time' # you're gonna need it
 
 class Bar
 attr_reader :name, :menu_items
-attr_accessor :happy_discount
+attr_writer :happy_discount
 
   def initialize(name)
     @name = name
@@ -13,7 +13,17 @@ attr_accessor :happy_discount
   def add_menu_item(name,price)
     @menu_items.push(Item.new(name,price))
   end
+
+  def happy_hour?
+    true
+  end
+
+  def happy_discount
+    return @happy_discount if self.happy_hour?
+    0
+  end
 end
+
 
 class Item
   attr_reader :name, :price
