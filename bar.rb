173439f4b0aaf36cd523f9de78d998e2a982fa32
Brian Keane <lonesomewhistle@gmail.com>
2014-03-07 19:31:36 -0600

    Added empty menu_items array

--- bar.rb
+++ bar.rb
@@ -2,9 +2,12 @@ require 'time' # you're gonna need it
 
 class Bar
   attr_reader :name
+  attr_accessor :menu_items
 
   def initialize(name)
     @name = name
+    @menu_items = []
   end
+
 
 end
