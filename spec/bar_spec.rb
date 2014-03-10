463fb8915a8ebc82d3938b8292cfbd5d8315cbee
Christopher Palmer <snly2386@gmail.com>
2014-03-09 19:46:17 -0500

    finished through ext. 11

--- spec/bar_spec.rb
+++ spec/bar_spec.rb
@@ -31,25 +31,18 @@ describe Bar do
   end
 
   it "can retrieve menu items" do
-    @bar.add_menu_item("Little Johnny", 9.95)
+    @bar.add_menu_item('Little Johnny', 9.95)
     item = @bar.menu_items.first
     expect(item.name).to eq 'Little Johnny'
     expect(item.price).to eq 9.95
   end
 
   it "has a default happy hour discount of zero" do
-    expect(@bar.happy_discount).to eq 0
-  end
-
-
-  xit "can set its happy hour discount" do
-    @bar.happy_discount = 0.5
-    expect(@bar.happy_discount).to eq 0.5
+    expect(@bar.happy_discount(Item.new("tequila",2,true))).to eq 0
   end
 
   it "can set its happy hour discount" do
     expect { @bar.happy_discount = 0.5 }.to_not raise_error
-
   end
 
   it "only returns a discount when it's happy hour" do
@@ -59,57 +52,96 @@ describe Bar do
     # We are STUBBING `happy_hour?` to return a specified value.
     # Because of this, you don't have to write a happy_hour? method (yet)
     expect(@bar).to receive(:happy_hour?).and_return(false)
-    expect(@bar.happy_discount).to eq 0
+    expect(@bar.happy_discount(Item.new("tequila",2,true))).to eq 0
 
     expect(@bar).to receive(:happy_hour?).and_return(true)
-    expect(@bar.happy_discount).to eq 0.5
+    expect(@bar.happy_discount(Item.new("tequila",2,true))).to eq 0.5
 
     # Take 2
     @bar.happy_discount = 0.3
     expect(@bar).to receive(:happy_hour?).and_return(false)
-    expect(@bar.happy_discount).to eq 0
+    expect(@bar.happy_discount(Item.new("tequila",2,true))).to eq 0
 
     expect(@bar).to receive(:happy_hour?).and_return(true)
-    expect(@bar.happy_discount).to eq 0.3
+    expect(@bar.happy_discount(Item.new("tequila",2,true))).to eq 0.3
   end
 
-<<<<<<< Updated upstream
-  xit "constrains its happy hour discount to between zero and one" do
+  it "constrains its happy hour discount to between zero and one" do
     expect(@bar).to receive(:happy_hour?).twice.and_return(true)
 
-=======
-  it "constrains its happy hour discount to between zero and one" do
->>>>>>> Stashed changes
     # HINT: You need to write your own setter
     @bar.happy_discount = 2
-    expect(@bar.happy_discount).to eq 1
+    expect(@bar.happy_discount(Item.new("tequila",2,true))).to eq 1
 
     @bar.happy_discount = -3
-    expect(@bar.happy_discount).to eq 0
+    expect(@bar.happy_discount(Item.new("tequila",2,true))).to eq 0
   end
 
 # # # # # # # # # # # # # # # # # # # # # #
   # DO NOT CHANGE SPECS ABOVE THIS LINE #
 # # # # # # # # # # # # # # # # # # # # # #
 
-  describe '#happy_hour', :pending => true do
+  describe '#happy_hour'  do
     it "knows when it is happy hour (3:00pm to 4:00pm)" do
       # TODO: CONTROL TIME
-      current_time = Time.now
+       current_time = Time.parse('3 pm')
+      Time.stub(:now).and_return(current_time)
+
       expect(@bar.happy_hour?).to eq(true)
     end
 
     it "is not happy hour otherwise" do
       # TODO: CONTROL TIME
+      current_time = Time.parse('5 pm')
+      Time.stub(:now).and_return(current_time)
       expect(@bar.happy_hour?).to eq(false)
     end
   end
 
-  context "During normal hours" do
+  it "knows not to discount during normal hours" do
     # TODO: WRITE TESTS TO ENSURE BAR KNOWS NOT TO DISCOUNT
+    current_time = Time.parse ('1 pm')
+    Time.stub(:now).and_return(current_time)
+
+    expect(@bar.happy_hour?).to eq(false)
+    expect(@bar.happy_discount(Item.new("tequila",2,true))).to eq (0)
+
   end
 
-  context "During happy hours" do
+  it "knows to discount during happy hours" do
     # TODO: WRITE TESTS TO ENSURE BAR DISCOUNTS DURING HAPPY HOUR
+    current_time = Time.parse('3 pm')
+    Time.stub(:now).and_return(current_time)
+    @bar.happy_discount = 1
+    expect(@bar.happy_hour?).to eq(true)
+    expect(@bar.happy_discount(Item.new("tequila",2,true))).to eq (1)
   end
+
+  it "applies .50 discount on Wednesdays and Mondays only" do
+    current_time = Time.parse('3 pm')
+    expect(@bar).to receive(:slowday?).and_return(true)
+    expect(@bar).to receive(:happy_hour?).and_return(true)
+    @bar.happy_discount = 0.5
+    expect(@bar.happy_discount(Item.new("tequila",2,true))).to eq(0.5)
+end
+
+  it "determines whether a drink is eligible for discount" do
+    drink = Item.new("tequila", 2, true)
+    current_time = Time.parse('3 pm')
+    Time.stub(:now).and_return(current_time)
+    @bar.happy_discount = 1
+    expect(@bar.happy_hour?).to eq(true)
+    expect(@bar.happy_drink?(drink)).to eq(true)
+    expect(@bar.happy_discount(drink)).to eq(1)
+    end
+
+  it "records drink purchase and keeps track of most popular drinks" do
+  tequila = Item.new("tequila",2,true)
+  jose = Item.new("tequila",2,true)
+  gin = Item.new("gin",2,true)
+  @bar.purchase_drink(tequila)
+  @bar.purchase_drink(gin)
+  @bar.purchase_drink(jose)
+  expect(@bar.most_popular).to eq(["tequila", "gin"])
+end
 end
