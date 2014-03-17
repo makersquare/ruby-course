404c86de54059230e96476c95dea3df313baebd2
Brandon Beasley <brandontbeasley@gmail.com>
2014-03-17 09:45:55 -0500

    created second ex0 test

--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -3,7 +3,13 @@ require "./exercises.rb"
 
 describe Exercises do
   it 'triples a given string' do
-    result = Exercises.self.ex0('ha')
+    result = Exercises.ex0('ha')
     expect(result).to eq 'hahaha'
   end
+
+  it 'returns "nope" if string is "wishes"' do
+    result = Excercises.ex0('wishes')
+    expect(result).to eq 'nope'
+  end
+
 end
