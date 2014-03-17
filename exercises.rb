0f0149e2114d4cdeb28d8550d150aaae52b0b0f9
vagrant <vagrant@student-dev-box>
2014-03-17 19:46:31 +0000

    Completed remaining exercises; implemented RPS class and player

--- exercises.rb
+++ exercises.rb
@@ -1,3 +1,4 @@
+require 'time'
 
 module Exercises
   # Exercise 0
@@ -33,20 +34,30 @@ module Exercises
   #  - Returns the max number of the given array
   def self.ex4(array)
     # TODO
+    array.max
   end
 
   # Exercise 5
   #  - Iterates through an array and `puts` each element
   def self.ex5(array)
     # TODO
+    array.each do |i|
+      puts i
+    end
   end
 
   # Exercise 6
   #  - Updates the last item in the array to 'panda'
   #  - If the last item is already 'panda', update
   #    it to 'GODZILLA' instead
-  def self.ex6(array, str)
+  def self.ex6(array)
     # TODO
+    if array.last == 'panda'
+      array[-1] = 'GODZILLA'
+    else
+      array[-1] = 'panda'
+    end
+    array
   end
 
   # Exercise 7
@@ -54,6 +65,10 @@ module Exercises
   #    add `str` to the end of the array
   def self.ex7(array, str)
     # TODO
+    if array.include?(str)
+      array.push(str)
+    end
+    array
   end
 
   # Exercise 8
@@ -62,6 +77,9 @@ module Exercises
   #    Iterate through `people` and print out their name and occupation.
   def self.ex8(people)
     # TODO
+    people.each do |person|
+      puts person[:name] + ": " + person[:occupation]
+    end
   end
 
   # Exercise 9
@@ -70,7 +88,9 @@ module Exercises
   # Hint: Google for the wikipedia article on leap years
   def self.ex9(time)
     # TODO
+  time.year % 4 == 0 ? true : false
   end
+
 end
 
 
@@ -87,6 +107,58 @@ class RPS
   #
   # You will be using this class in the following class, which will let players play
   # RPS through the terminal.
+
+  attr_reader :player1, :player2, :num_rounds, :p1_wins, :p2_wins
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @p1_wins = 0
+    @p2_wins = 0
+  end
+
+  def play(p1_move, p2_move)
+  # don't forget draws
+  return "Draw! Play again!" if p1_move == p2_move
+
+  if @p1_wins >= 2 || @p2_wins >=2
+    return "The game is over."
+  end
+
+    if (p1_move == "rock")
+      winner = p2_move == "scissors" ? player1 : player2
+      winner == player1 ? @p1_wins+=1 : @p2_wins +=1
+      if @p1_wins >= 2
+        winner + " wins the game!"
+      elsif @p2_wins >= 2
+        winner + " wins the game!"
+      else
+        winner + " wins the round!"
+      end
+    elsif (p1_move == "paper")
+        winner = p2_move == "rock" ? player1 : player2
+        winner == player1 ? @p1_wins+=1 : @p2_wins +=1
+        if @p1_wins >= 2
+        winner + " wins the game!"
+        elsif @p2_wins >= 2
+        winner + " wins the game!"
+        else
+        winner + " wins the round!"
+        end
+    else
+      winner = p2_move == "paper" ? player1 : player2
+      winner == player1 ? @p1_wins+=1 : @p2_wins +=1
+      if @p1_wins >= 2
+        winner + " wins the game!"
+      elsif @p2_wins >= 2
+        winner + " wins the game!"
+      else
+        winner + " wins the round!"
+      end
+    end
+
+  end
+
 end
 
 
@@ -103,7 +175,6 @@ class RPSPlayer
   #
   # When the game ends, ask if the player wants to play again.
   def start
-
     # TODO
 
     # PRO TIP: Instead of using plain `gets` for grabbing a player's
@@ -111,7 +182,29 @@ class RPSPlayer
     #          what the player is typing! :D
     # This is also why we needed to require 'io/console'
     # move = STDIN.noecho(&:gets)
+
+    puts "Enter your name, Player 1: "
+    player_1 = gets.chomp
+    puts "Enter your name, Player 2: "
+    player_2 = gets.chomp
+
+    @game = RPS.new(player_1, player_2)
+
+    winner = ""
+
+    loop do
+      puts "Enter your move " + player_1 + ":"
+      p1move = STDIN.noecho(&:gets)
+      p1move = p1move.chomp
+      puts "Enter your move " + player_2 + ":"
+      p2move = STDIN.noecho(&:gets)
+      p2move = p2move.chomp
+      winner = @game.play(p1move, p2move)
+      puts winner
+      break if winner == "The game is over." || winner.split.last == "game!"
+    end
   end
+
 end
 
 
--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -34,3 +34,110 @@ describe 'Exercise 3' do
   end
 end
 
+describe 'Exercise 4' do
+  it "returns the max number of the given array" do
+    result = Exercises.ex4([1,44, 2, 98, 15])
+    expect(result).to eq(98)
+  end
+end
+
+describe 'Exercise 5' do
+  it "iterates through an array and puts each element" do
+    expect(STDOUT).to receive(:puts).with(1)
+    expect(STDOUT).to receive(:puts).with("blue")
+    expect(STDOUT).to receive(:puts).with("dog")
+    Exercises.ex5([1, "blue", "dog"])
+  end
+end
+
+describe 'Exercise 6' do
+
+  it "updates the last item in the array to panda" do
+    result = Exercises.ex6(["cat", "lemming", "elephant"])
+    expect(result.count).to eq(3)
+    expect(result.last).to eq("panda")
+  end
+
+  it "updates last item in array to GODZILLA if already panda" do
+    result = Exercises.ex6(["cat", "lemming","bird", "panda" ])
+    expect(result.count).to eq(4)
+    expect(result.last).to eq("GODZILLA")
+  end
+
+end
+
+describe 'Exercise 7' do
+  it "adds str to end of array if already exists in array" do
+    result = Exercises.ex7(["Hello", 17 ,"Goodbye", 12], "Goodbye")
+    expect(result.count).to eq(5)
+    expect(result.last).to eq("Goodbye")
+    no_str = Exercises.ex7(["Hello", 17 ,"Goodbye", 12], "Hey")
+    expect(no_str.count).to eq(4)
+    expect(no_str.last).to eq(12)
+  end
+end
+
+
+describe 'Exercise 8' do
+  it "prints name and occupation values within array of hashes" do
+    expect(STDOUT).to receive(:puts).with("Mary: engineer")
+    expect(STDOUT).to receive(:puts).with("Jane: president")
+    expect(STDOUT).to receive(:puts).with("Sophie: mathematician")
+    Exercises.ex8([{:name =>"Mary", :occupation => "engineer"}, {:name => "Jane", :occupation => "president"}, {:name => "Sophie", :occupation =>"mathematician"}])
+  end
+end
+
+describe "Exercise 9" do
+  it "returns true if the given time is in a leap year" do
+  leap_year = Time.new(2012,6,21, 13,30,0, "+09:00")
+    result = Exercises.ex9(leap_year)
+    expect(result).to eq(true)
+  end
+end
+
+describe "RPS" do
+
+  before do
+    @new_game = RPS.new("Rambo", "Killa")
+  end
+
+  it "initializes with two players" do
+    expect(@new_game.player1).to eq("Rambo")
+    expect(@new_game.player2).to eq("Killa")
+  end
+
+  # it "inializes with 0 rounds" do
+  #   expect(@new_game.num_rounds).to eq(0)
+  # end
+
+  describe "play" do
+    it "determines the winner of the round" do
+      result = @new_game.play("rock", "scissors")
+      expect(result).to eq("Rambo wins the round!")
+      result = @new_game.play("scissors", "rock")
+      expect(result).to eq("Killa wins the round!")
+    end
+
+    it "returns the winner of the game" do
+      result = @new_game.play("rock", "scissors")
+      expect(result).to eq("Rambo wins the round!")
+      result = @new_game.play("scissors", "rock")
+      expect(result).to eq("Killa wins the round!")
+      result = @new_game.play("paper", "scissors")
+      expect(result).to eq("Killa wins the game!")
+      expect(@new_game.p2_wins).to eq(2)
+      expect(@new_game.p1_wins).to eq(1)
+    end
+
+    it "tells players when game is over" do
+      result = @new_game.play("rock", "scissors")
+      expect(result).to eq("Rambo wins the round!")
+      result = @new_game.play("scissors", "rock")
+      expect(result).to eq("Killa wins the round!")
+      result = @new_game.play("paper", "scissors")
+      expect(result).to eq("Killa wins the game!")
+      result = @new_game.play("rock", "scissors")
+      expect(result).to eq("The game is over.")
+    end
+  end
+end
