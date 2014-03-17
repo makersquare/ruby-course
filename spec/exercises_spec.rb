9d02833c1e810069d6969ecaac02102ed4864fc7
Jose R. Borja <chakaitos@gmail.com>
2014-03-17 09:53:02 -0500

    Add spec for exercise 0 and implement code for it

--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -5,4 +5,9 @@ describe "Exercise 0" do
 		result = Exercises.ex0('ho')
 		expect(result).to eq 'hohoho'
 	end
+
+	it "Returns 'nope' if string is 'wishes'" do
+		result = Exercises.ex0('wishes')
+		expect(result).to eq 'nope'
+	end
 end
