e444b78d5a67bccb300f17ceb2bc9c88a950fd57
vagrant <vagrant@student-dev-box>
2014-03-17 15:03:43 +0000

    Implement ex0 - ex3; run tests on methods

--- exercises.rb
+++ exercises.rb
@@ -5,24 +5,28 @@ module Exercises
   #  - Returns "nope" if `str` is "wishes"
   def self.ex0(str)
     # TODO
+    str == 'wishes' ? "nope": str*3
   end
 
   # Exercise 1
   #  - Returns the number of elements in the array
   def self.ex1(array)
     # TODO
+    array.length
   end
 
   # Exercise 2
   #  - Returns the second element of an array
   def self.ex2(array)
     # TODO
+    array[1]
   end
 
   # Exercise 3
   #  - Returns the sum of the given array of numbers
   def self.ex3(array)
     # TODO
+    array.inject{|memo, i| memo + i}
   end
 
   # Exercise 4
---
+++ spec/exercises_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe 'Exercise 0' do
+  it "triples the length of a string" do
+    result = Exercises.ex0("ha")
+    expect(result).to eq("hahaha")
+  end
+
+  it "returns 'nope' if the string is 'wishes'" do
+    result = Exercises.ex0("wishes")
+    expect(result).to eq("nope")
+  end
+end
+
+
+describe 'Exercise 1' do
+    it "returns the number of elements in the array" do
+      result = Exercises.ex1([1,2,3,4,5])
+      expect(result).to eq(5)
+    end
+end
+
+describe 'Exercise 2' do
+    it "returns the second element of an array" do
+      result = Exercises.ex2(["cat","dog", "mouse","bird"])
+      expect(result).to eq("dog")
+    end
+end
+
+describe 'Exercise 3' do
+  it "returns the sum of the given array of numbers" do
+    result = Exercises.ex3([1,2,3,4,5])
+    expect(result).to eq(15)
+  end
+end
+
