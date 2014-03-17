17fd4bb6155aebaed17d555d885a5402607bb4be
Brady Bryan <jbraden.bryan@gmail.com>
2014-03-17 10:10:46 -0500

    adds and passes test for counts objects in an array

--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -6,9 +6,16 @@ describe Exercises do
     result = Exercises.ex0("brady")
     expect(result).to eq("bradybradybrady")
   end
+
   it "returns 'nope' if string == wishes" do
     result = Exercises.ex0("wishes")
     expect(result).to eq("nope")
   end
+
+  it "returns the number of elements in an array" do
+    result = Exercises.ex1([1,2,3])
+    expect(result).to eq(3)
+  end
+
 end
 
