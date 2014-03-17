e8a9558ad68fe32736bc3bab9d05b100498d45fd
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-03-17 10:51:06 -0500

    rspec tests 1 thru 6 functional

--- exercises.rb
+++ exercises.rb
@@ -42,7 +42,12 @@ module Exercises
   #  - If the last item is already 'panda', update
   #    it to 'GODZILLA' instead
   def self.ex6(array, str)
-    # TODO
+    if array[-1] == "panda"
+      array[-1] = "GODZILLA"
+    else
+      array[-1] = str
+    end
+    return array
   end
 
   # Exercise 7
