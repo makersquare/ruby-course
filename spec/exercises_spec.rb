42c468dfcbce52351fa3a7721ff203681dac16c0
hubertanguyen@gmail.com <hubertanguyen@gmail.com>
2014-03-17 09:44:40 -0500

    1st test passed

--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -5,5 +5,8 @@ describe Exercises do
   it "triples a given string" do
     tripleword = Exercises.ex0("hi")
     expect(tripleword).to eq ("hihihi")
+
+    wishes = Exercises.ex0("wishes")
+    expect(wishes).to eq ("nope")
   end
 end
