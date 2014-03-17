aaed7fb5e2a4953c11b4fddcc7922af86fbc3a25
Jose R. Borja <chakaitos@gmail.com>
2014-03-17 13:24:31 -0500

    Add files for rock, paper, scissors project

---
+++ rps.rb
@@ -0,0 +1,56 @@
+class RPS
+  # Rock, Paper, Scissors
+  # Make a 2-player game of rock paper scissors. It should have the following:
+  #
+  # It is initialized with two strings (player names).
+  # It has a `play` method that takes two strings:
+  #   - Each string reperesents a player's move (rock, paper, or scissors)
+  #   - The method returns the winner (player one or player two)
+  #   - If the game is over, it returns a string stating that the game is already over
+  # It ends after a player wins 2 of 3 games
+  #
+  # You will be using this class in the following class, which will let players play
+  # RPS through the terminal.
+end
+
+
+require 'io/console'
+class RPSPlayer
+  # (No specs are required for RPSPlayer)
+  #
+  # Complete the `start` method so that it uses your RPS class to present
+  # and play a game through the terminal.
+  #
+  # The first step is to read (gets) the two player names. Feed these into
+  # a new instance of your RPS class. Then `puts` and `gets` in a loop that
+  # lets both players play the game.
+  #
+  # When the game ends, ask if the player wants to play again.
+  def start
+
+    # TODO
+
+    # PRO TIP: Instead of using plain `gets` for grabbing a player's
+    #          move, this line does the same thing but does NOT show
+    #          what the player is typing! :D
+    # This is also why we needed to require 'io/console'
+    # move = STDIN.noecho(&:gets)
+  end
+end
+
+
+module Extensions
+  # Extension Exercise
+  #  - Takes an `array` of strings. Returns a hash with two keys:
+  #    :most => the string(s) that occures the most # of times as its value.
+  #    :least => the string(s) that occures the least # of times as its value.
+  #  - If any tie for most or least, return an array of the tying strings.
+  #
+  # Example:
+  #   result = Extensions.extremes(['x', 'x', 'y', 'z'])
+  #   expect(result).to eq({ :most => 'x', :least => ['y', 'z'] })
+  #
+  def self.extremes(array)
+    # TODO
+  end
+end
