834f291ff04d74e2fa95631d67d430a659143d5f
Jose R. Borja <chakaitos@gmail.com>
2014-03-17 10:10:03 -0500

    Add spec to return the 2nd element of an array - Ex2

--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -1,21 +1,34 @@
 require './exercises'
 
-describe "Exercise 0" do
-	it "triples a given string" do
-		result = Exercises.ex0('ho')
-		expect(result).to eq 'hohoho'
+describe Exercises do
+	before do
+		@numbers = [5, 10, 15, 20, 25]
+		@animals = ['tiger', 'lion', 'crocodile', 'snake']
 	end
+	
+	describe "Exercise 0" do
+		it "triples a given string" do
+			result = Exercises.ex0('ho')
+			expect(result).to eq 'hohoho'
+		end
 
-	it "returns 'nope' if string is 'wishes'" do
-		result = Exercises.ex0('wishes')
-		expect(result).to eq 'nope'
-	end
+		it "returns 'nope' if string is 'wishes'" do
+			result = Exercises.ex0('wishes')
+			expect(result).to eq 'nope'
+		end
 
-	describe "Exercise 1" do
-		it "returns the number of elements in the array" do
-			animals = ['tiger', 'lion', 'crocodile', 'snake']
-			result = Exercises.ex1(animals)
-			expect(result).to eq(4)
+		describe "Exercise 1" do
+			it "returns the number of elements in the array" do
+				result = Exercises.ex1(@animals)
+				expect(result).to eq(4)
+			end
+		end
+
+		describe "Exercise 2" do
+			it "returns the second element of an array" do
+				result = Exercises.ex2(@animals)
+				expect(result).to eq 'lion'
+			end
 		end
 	end
 end
