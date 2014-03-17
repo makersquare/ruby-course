14206ec0df3fd6a9f91881eecdc1f10ec357bc3d
Jose R. Borja <chakaitos@gmail.com>
2014-03-17 10:00:56 -0500

    Add spec to return the number of elements of an array - Ex1

--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -1,13 +1,21 @@
 require './exercises'
 
 describe "Exercise 0" do
-	it "Triples a given string" do
+	it "triples a given string" do
 		result = Exercises.ex0('ho')
 		expect(result).to eq 'hohoho'
 	end
 
-	it "Returns 'nope' if string is 'wishes'" do
+	it "returns 'nope' if string is 'wishes'" do
 		result = Exercises.ex0('wishes')
 		expect(result).to eq 'nope'
+	end
+
+	describe "Exercise 1" do
+		it "returns the number of elements in the array" do
+			animals = ['tiger', 'lion', 'crocodile', 'snake']
+			result = Exercises.ex1(animals)
+			expect(result).to eq(4)
+		end
 	end
 end
