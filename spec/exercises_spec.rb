83aa60dff8176e85d5df5c10d762e4db651afd74
Ronak Patel <patel66@sbcglobal.net>
2014-03-17 15:13:58 +0000

    Working tests for exercise 1

--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -13,14 +13,16 @@ describe 'Exercise 0' do
   it 'Returns nope if the string is "wishes" ' do
 
     result = Exercises.ex0("wishes")
-
     expect(result).to eq("nope")
   end
 
 end
 
-#describe 'Exercises 1' do
-#  it 'Returns # of elements in a array' do
-#
-#    result =
-#end
+describe 'Exercises 1' do
+  it 'Returns # of elements in a array' do
+    array = [2,4,6,8]
+    result = Exercises.ex1(array)
+    expect(result).to eq(4)
+  end
+
+end
