432f1db11f12cabee6d1de4d41fee953c522a413
Christopher Palmer <snly2386@gmail.com>
2014-03-18 11:41:35 -0500

    initailized task class

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,12 @@
 
 class TM::Project
+	attr_reader :name, :id
+	def initialize (name)
+		@name = name
+		@@id = (0...8).map { (65 + rand(26)).chr }.join
+	end
+
+	def self.id 
+		@@id 
+	end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -4,4 +4,11 @@ describe 'Task' do
   it "exists" do
     expect(TM::Task).to be_a(Class)
   end
+
+  it 'can be created with project id, description, and priority number' do
+  	newproject = TM::Project.new("cp")
+
+  	newtask = TM::Task.new("red",2,TM::Project.id)
+  	expect(newtask.project_id).to eq(TM::Project.id)
+  end
 end
