f32f14b7d4dc4dc363b8e296942b749e5dafb261
Christopher Palmer <snly2386@gmail.com>
2014-03-18 11:22:32 -0500

    1st task

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,4 +4,13 @@ describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
+  it "can be created with a name and unique id" do
+	newproject = TM::Project.new ('project1')
+	expect(newproject.name).to eq('project1')
+
+	id = (0...8).map { (65 + rand(26)).chr }.join
+	expect(TM::Project.id).not_to be_empty
+	end
+
 end
