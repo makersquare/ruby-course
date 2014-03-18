02897831a3c0b5c0ebfa0f30b66768f957ef6b27
Drew Garcia <drewgarcia23@gmail.com>
2014-03-18 11:03:20 -0500

    Project has a name

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,4 +4,10 @@ describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
+  it "has a name" do
+    test = TM::Project.new("Test")
+    expect(test.name).to eq("Test")
+  end
+
 end
