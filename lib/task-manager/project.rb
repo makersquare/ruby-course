0bb9576109a074cf0ddc0a349f639694ad34036e
Jose R. Borja <chakaitos@gmail.com>
2014-03-18 11:08:10 -0500

    Add functionality to initialize project with a name with its respective spec

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,8 @@
 
 class TM::Project
+	attr_reader :name
+
+	def initialize(name)
+		@name = name
+	end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,4 +4,9 @@ describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
+  it "should be initialized with a name" do
+  	project = TM::Project.new("SXSW")
+  	expect(project.name).to eq 'SXSW'
+  end
 end
