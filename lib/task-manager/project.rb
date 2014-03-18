a192f8213aa1784883c360b101a0367b272810b5
Parth Shah <parthshahva@gmail.com>
2014-03-18 13:58:22 -0500

    add task from project

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,7 +1,25 @@
 
 class TM::Project
-attr_accessor :name
+attr_accessor :name, :id, :tasks
+@@id_counter = 1
 def initialize(name)
   @name = name
+  @id = @@id_counter
+  @@id_counter += 1
+  @tasks = []
+end
+
+def self.id_counter=(value)
+    @@id_counter=value
+  end
+
+def create_new_task(description, priority_number)
+  blue = TM::Task.new(@id, description, priority_number)
+  @tasks.push(blue)
+  blue
+end
+
+def complete_task(task_id)
+
 end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -5,7 +5,29 @@ describe 'Project' do
     expect(TM::Project).to be_a(Class)
   end
   it "should be initialized with a name" do
+    TM::Project.id_counter = 1
     blue = TM::Project.new("name")
     expect(blue.name).to eq("name")
+    expect(blue.id).to eq(1)
+  end
+  it "should be initialized with a unique id" do
+     TM::Project.id_counter = 1
+    project_one = TM::Project.new('one')
+     expect(project_one.id).to eq(1)
+  end
+  it "creates new task from a project" do
+  project_one = TM::Project.new('one')
+  project_one.create_new_task("green", 5)
+  expect(project_one.tasks.count).to eq(1)
+  project_one.create_new_task("red", 4)
+  expect(project_one.tasks.count).to eq(2)
+  end
+  it "a task can be completed by calling upon its task id" do
+  TM::Task.id_counter = 1
+  project_one = TM::Project.new('one')
+  red = project_one.create_new_task("green", 5)
+  expect(red.status).to eq("incomplete")
+  project_one.complete_task(1)
+  expect(red.status).to eq("complete")
   end
 end
