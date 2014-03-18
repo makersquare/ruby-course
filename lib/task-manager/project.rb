95fa3943d6b7ded0cc0f9684ae15dcdeaef15a98
vagrant <vagrant@student-dev-box>
2014-03-18 16:09:44 +0000

    New project can be created with name. Unique ID is assigned.

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,12 @@
 
 class TM::Project
+  attr_reader :name, :id
+
+  @@projectcount = 0
+
+  def initialize(name)
+    @name = name
+    @@projectcount += 1
+    @id = @@projectcount
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,4 +4,18 @@ describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
+  it "can create a new project with a name" do
+    project = TM::Project.new("Name")
+    expect(project.name).to eq("Name")
+  end
+
+  it "is assigned a unique id" do
+    project = TM::Project.new("Name")
+    expect(project.id).to eq(2)
+    project = TM::Project.new("Name")
+    expect(project.id).to eq(3)
+  end
+
+
 end
