dba316c21516bab7e5e36d55794b6b5525d47586
Ronak Patel <patel66@sbcglobal.net>
2014-03-18 16:47:41 +0000

    Task can be marked by an id

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -1,3 +1,9 @@
 
 class TM::Task
+  attr_reader :id
+  def initialize
+    @id = self.object_id
+
+  end
+
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -4,4 +4,24 @@ describe 'Task' do
   it "exists" do
     expect(TM::Task).to be_a(Class)
   end
+
+  describe 'Is created with project id with creation date and priority' do
+    before do
+      @project = TM::Project.new("d")
+      @task = TM::Task.new()
+    end
+    it 'created with project id' do
+      expect(@task.id).to eq(@task.object_id)
+
+    end
+
+
+
+
+
+  end
+
+
+
 end
+
