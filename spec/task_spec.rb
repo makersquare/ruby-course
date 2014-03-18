7c49118b0b20c59859b1dbcb04644c1b14e8d39e
vagrant <vagrant@student-dev-box>
2014-03-18 16:13:57 +0000

    New task can be created

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -4,4 +4,11 @@ describe 'Task' do
   it "exists" do
     expect(TM::Task).to be_a(Class)
   end
+
+  it "Can create a new task with id, description and priority number" do
+    task = TM::Task.new(10,"Description",1)
+    expect(task.project_id).to eq(10)
+    expect(task.description).to eq("Description")
+    expect(task.priority).to eq(1)
+  end
 end
