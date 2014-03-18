b02f631420ddfe389051a57d8b491d19b7b803ea
Brandon Beasley <brandontbeasley@gmail.com>
2014-03-18 11:23:38 -0500

    initializes project with name and unique id

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,14 @@
 
 class TM::Project
+  @@id_counter = 0
+  attr_reader :name
+  attr_accessor :id_counter
+  def initialize(name)
+    @name = name
+    @@id_counter +=1
+  end
+
+  def id_counter
+    @@id_counter
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,7 +1,23 @@
 require 'spec_helper'
 
 describe 'Project' do
+
+  describe '.initialize' do
+    it 'assigns a name to new project' do
+      new_project = TM::Project.new('projname')
+      result = new_project.name
+      expect(result).to eq 'projname'
+    end
+
+    it "automatically assigns a new unique id" do
+      new_project = TM::Project.new('projname')
+      result = new_project.id_counter
+      expect(result).to eq 2
+    end
+  end
+
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
 end
