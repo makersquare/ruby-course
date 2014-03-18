3fbd77a9e8357363c06d4844e23fa6dbb8ef1507
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-03-18 11:35:32 -0500

    project initializes and adds id

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,14 @@
 
 class TM::Project
+attr_reader :name, :id
+
+  @@id_counter = 0
+
+  def initialize(name)
+    @name = name
+    @@id_counter += 1
+    @id = @@id_counter
+  end
+
 end
+
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,7 +1,23 @@
 require 'spec_helper'
+require './lib/task-manager/project.rb'
 
 describe 'Project' do
+  before do
+    @new_project = TM::Project.new("New Project Name")
+  end
+
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
+  it "initializes and has a project name" do
+    result = @new_project.name
+    expect(result).to eq("New Project Name")
+  end
+
+  it "initializes with an id" do
+    result = @new_project.id
+    expect(result).to eq(3)
+  end
+
 end
