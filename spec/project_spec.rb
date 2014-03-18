a4e36c69268554ed31638fc268574ad7a397796b
Jose R. Borja <chakaitos@gmail.com>
2014-03-18 18:05:46 -0500

    Add method to return a list of incomplete tasks based on priority along with its tests

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -20,14 +20,14 @@ describe 'Project' do
 	  end
 	end
 
-	describe "#add_task" do
+	describe ".add_task" do
 		it "should add a task to the project" do
 			add_task = @project.add_task('wash clothes', 4)
 			expect(@project.tasks.count).to eq(1)
 		end
 	end
 
-	describe "#mark_task_complete" do
+	describe ".mark_task_complete" do
 		it "should change the status of a task to 'complete'" do
 			add_task = @project.add_task('wash clothes', 4)
 			task_id = @project.tasks.first.id
@@ -36,4 +36,20 @@ describe 'Project' do
 			expect(@project.tasks.first.status).to eq 'complete'
 		end
 	end
+
+	describe ".incomplete_tasks" do
+		it "shows a list of incomplete tasks ordered by priority" do
+			Time.stub(:now).and_return(Time.parse('12pm'))
+			eat = @project.add_task('eat', 3)
+			Time.stub(:now).and_return(Time.parse('8pm'))
+			sleep = @project.add_task('sleep', 5)
+			Time.stub(:now).and_return(Time.parse('6pm'))
+			shower = @project.add_task('shower', 5)
+
+			expect(@project.tasks.count).to eq(3)
+			expect(@project.incomplete_tasks).to eq([shower, sleep, eat])
+		end
+	end
 end
+
+# @books.select { |book| book if book.status == 'available' }
