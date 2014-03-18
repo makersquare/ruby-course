0712610d485a5604dd8b874c5b7075b7ab6ddc1d
Gene Le <gene.gkl@gmail.com>
2014-03-18 16:33:23 -0500

    first commit

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,7 @@
 
 class TM::Project
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,4 +4,9 @@ describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
+  it "initializes with a name" do
+    project = TM::Project.new("Bird Watching")
+    expect(project.name).to eq("Bird Watching")
+  end
 end
