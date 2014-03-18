fb9398a10263837f5f18c06b3d0a7ce98bac64fc
hubertanguyen@gmail.com <hubertanguyen@gmail.com>
2014-03-18 11:28:39 -0500

    Makes a project with id, description, and priotiy_num

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,16 @@
 
 class TM::Project
+@@id = 0
+attr_reader :id, :description, :priority_number
+attr_accessor :id
+  def initialize(description, priority_number)
+    @@id += 1
+    @description = description
+    @priority_number = priority_number
+  end
+
+   def self.id
+    @@id
+  end
+
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,4 +4,16 @@ describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
+
+
+  it "A new task can be created with a project id, description, and priority number" do
+
+    newproject = TM::Project.new("MakerSquare", 3)
+    expect(newproject.description).to eq ("MakerSquare")
+    expect(newproject.priority_number).to eq (3)
+    expect(TM::Project.id).to eq (1)
+
+  end
+
 end
