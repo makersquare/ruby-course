00ad6854c4630a0774b1943f73e6cbff1e91e538
Jose R. Borja <chakaitos@gmail.com>
2014-03-18 13:57:47 -0500

    Add method to add a task to a tasks array along with its test

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,11 +1,16 @@
 
 class TM::Project
-	attr_reader :name, :id
+	attr_reader :name, :id, :tasks
 
 	@@id_counter = 0
 
 	def initialize(name)
 		@name = name
 		@id = @@id_counter += 1
+		@tasks = []
+	end
+
+	def add_task(description, priority)
+		@tasks << TM::Task.new(@id, description, priority)
 	end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,16 +1,15 @@
 require 'spec_helper'
 
 describe 'Project' do
-	
+	before do
+		@project = TM::Project.new("SXSW")
+	end
+
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
 
   describe ".initialize" do
-  	before do
-			@project = TM::Project.new("SXSW")
-		end
-
 	  it "should be initialized with a name" do
 	  	expect(@project.name).to eq 'SXSW'
 	  end
@@ -19,5 +18,12 @@ describe 'Project' do
 	  	# The id here is 3 because two instances of Project were created before
 	  	expect(@project.id).to eq(3)
 	  end
+	end
+
+	describe "#add_task" do
+		it "should add a task to the project" do
+			add_task = @project.add_task('wash clothes', 4)
+			expect(@project.tasks.count).to eq(1)
+		end
 	end
 end
