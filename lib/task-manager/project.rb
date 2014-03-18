2f71c04b8bc60cfc19836dab37afe9a26f2779d9
Drew Garcia <drewgarcia23@gmail.com>
2014-03-18 11:20:58 -0500

    Has and id with name

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,7 +1,13 @@
 
 class TM::Project
-  attr_reader :name
+
+  attr_reader :name, :id
   def initialize(name)
     @name = name
+    @@id = (0...8).map {(65+rand(26)).chr}.join
+  end
+
+  def self.id
+    @@id
   end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -10,4 +10,12 @@ describe 'Project' do
     expect(test.name).to eq("Test")
   end
 
+  it "has an id with the name" do
+    test = TM::Project.new("Test")
+    expect(test.name).to eq("Test")
+
+    id = (0...8).map {(65+rand(26)).chr}.join
+    expect(TM::Project.id).not_to be_empty
+  end
+
 end
