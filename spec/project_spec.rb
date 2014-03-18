a8240a098dd56e83484689a5dcf9539d926785d1
Antonio Felguerez <agfelgue@gmail.com>
2014-03-18 11:04:11 -0500

    Adds test for TM::Project to initialize with a name. TM::Project initializes with a name.

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 
 describe 'Project' do
-  it "exists" do
-    expect(TM::Project).to be_a(Class)
-  end
+	it "exists" do
+		expect(TM::Project).to be_a(Class)
+	end
+
+	it "has a name" do
+		project = TM::Project.new("website")
+		expect(project.name).to eq("website")
+	end
 end
