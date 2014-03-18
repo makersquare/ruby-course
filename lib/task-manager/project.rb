0fdd1ddcfb6eb50d560dc99d786741887740f64d
hubertanguyen@gmail.com <hubertanguyen@gmail.com>
2014-03-18 11:35:23 -0500

    added name

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,10 +1,11 @@
 
 class TM::Project
 @@id = 0
-attr_reader :id, :description, :priority_number
+attr_reader :id, :description, :priority_number, :name
 attr_accessor :id
-  def initialize(description, priority_number)
+  def initialize(name,description, priority_number)
     @@id += 1
+    @name = name
     @description = description
     @priority_number = priority_number
   end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -7,12 +7,25 @@ describe 'Project' do
 
 
 
-  it "A new task can be created with a project id, description, and priority number" do
+  it "A new task can be created with a name,project id, description, and priority number" do
 
-    newproject = TM::Project.new("MakerSquare", 3)
-    expect(newproject.description).to eq ("MakerSquare")
+    newproject = TM::Project.new("MakerSquare","Lessons on Ruby", 3)
+    expect(newproject.description).to eq ("Lessons on Ruby")
+    expect(newproject.name).to eq ("MakerSquare")
     expect(newproject.priority_number).to eq (3)
     expect(TM::Project.id).to eq (1)
+
+  end
+
+  it "task can be marked as complete, specified by id"  do
+
+  end
+
+  it "A project can retreve a list of all complete task, via creation date"  do
+
+  end
+
+  it "A project can retreive a list of all incomplete tasks, sorted by priority"  do
 
   end
 
