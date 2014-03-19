ff62979cf770f321324ba693d3f27d39bef19e67
Ronak Patel <patel66@sbcglobal.net>
2014-03-19 20:53:10 +0000

    Test works for creating a project

--- spec/project_list_spec.rb
+++ spec/project_list_spec.rb
@@ -1,2 +1,19 @@
 require 'spec_helper'
 
+describe 'Project List' do
+  before do
+    @pl = TM::ProjectList.new
+  end
+
+  it "exists" do
+    expect(TM::ProjectList).to be_a(Class)
+  end
+
+  it 'can create a project' do
+    new_project = @pl.create_project('nato')
+
+    expect(new_project).to eq(@pl.project_list)
+
+  end
+
+end
