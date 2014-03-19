52cf1b60902e9879e60ca56db4a4118a4613a0ea
Antonio Felguerez <agfelgue@gmail.com>
2014-03-19 16:10:26 -0500

    Adds a test to check whether TM::ProjectList class can create projects.
    Also adds pry-debugger Gem to project_list_spec.rb for testing purposes.

--- spec/project_list_spec.rb
+++ spec/project_list_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
+require 'pry-debugger'
 
 describe "TM::ProjectList" do
 	before  do
-		pl = TM::ProjectList.new
+		@pl = TM::ProjectList.new
 	end
 
 	it "exists" do
 		expect(TM::ProjectList).to be_a(Class)
 	end
 
+	it "can create projects" do
+		new_project = @pl.create_project("cool project")
+		expect(new_project).to eq(@pl.project_list)
+	end
 end
