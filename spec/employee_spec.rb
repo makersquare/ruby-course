50d1210e4496217f4a34758371ee969c03fdf002
Gene Le <gene.gkl@gmail.com>
2014-03-20 22:05:26 -0500

    test and method for Employee name

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 
 describe 'Employee' do
+  
+  before do
+    @emp = TM::Employee.new('Pim')
+  end
+
   it "exists" do
     expect(TM::Employee).to be_a(Class)
+  end
+
+  describe '.initialize' do
+    it "gives a name" do
+      expect(@emp.name).to eq("Pim")
+    end
   end
 end
