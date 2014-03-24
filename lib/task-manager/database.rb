5af43f1ec2084bf5c09d8356a569fafa79390768
Gene Le <gene.gkl@gmail.com>
2014-03-23 20:31:18 -0500

    fix tests for showing complete and incomplete tasks in DB. Also, refactor to use task hash in db instead of relying on Project class to keep track of task data

--- lib/task-manager/database.rb
+++ lib/task-manager/database.rb
@@ -3,7 +3,9 @@ require 'singleton'
 # made a singleton verion of ProjectList
 
 class TM::DB
-  attr_reader :projects
+  include Singleton
+
+  attr_reader :projects, :tasks
 
   # examples
   # proj_employees = @memberships.select do |memb|
@@ -17,13 +19,9 @@ class TM::DB
 
   def initialize
     @projects = {}
-    @employees = {}
-  end
+    @tasks = {}
 
-  @@instance = TM::DB.new
-
-  def self.instance
-    return @@instance
+    # @employees = {}
   end
 
   def create_project(title)
@@ -35,54 +33,80 @@ class TM::DB
     proj
   end
 
-  def show_proj_tasks_remaining(pid)
-    # ensure id is an integer
-    pid = pid.to_i
-
-    proj = @projects[pid]
-
-    proj.list_incomplete_tasks
-  end
-
-  def show_proj_tasks_complete(pid)
-    # ensure id is an integer
-    pid = pid.to_i
-
-    proj = @projects[pid]
-
-    proj.list_completed_tasks
-  end
-
   def add_task_to_proj(pid, desc, priority)
+
     # ensure id and priority are integers
     pid = pid.to_i
     priority = priority.to_i
 
     proj = @projects[pid]
 
-    added_task = proj.add_task(desc, priority)
+    added_task = TM::Task.new(pid, desc, priority)
+    @tasks[added_task.id] = added_task
+
+    added_task
+
+    # code prior to refactoring to hashes
+    # added_task = proj.add_task(desc, priority)
   end
+
+  def show_proj_tasks_remaining(pid)
+    # ensure id is an integer
+    pid = pid.to_i
+
+    # binding.pry
+
+    # array of tasks belonging to this pid
+    # @tasks.values creates an array from values in @tasks hash
+    pid_tasks = @tasks.values.select { |task| task.proj_id == pid }
+
+    incomplete_tasks = pid_tasks.select do |task|
+        task.completed == false
+      end
+
+    # code prior to refactoring
+    # no longer listing tasks through projects
+    # proj = @projects[pid]
+    # proj.list_incomplete_tasks
+  end
+
+  def show_proj_tasks_complete(pid)
+    # ensure id is an integer
+    pid = pid.to_i
+
+    pid_tasks = @tasks.values.select { |task| task.proj_id == pid }
+
+    completed_tasks = pid_tasks.select { |task| task.completed == true }
+
+    # code prior to refactoring to use Hashes
+    # proj = @projects[pid]
+    # proj.list_completed_tasks
+  end
+
 
   def mark_task_as_complete(tid)
     # ensure id is an integer
     tid = tid.to_i
 
-    project = nil
+    @tasks[tid].completed = true
 
-    @projects.each do |pid, proj|
-      if proj.include_task?(tid)
-        proj.mark_as_complete(tid)
+    @tasks[tid]
 
-        # set project variable to point to the project
-        # that has the task we are looking for
-        project = proj
-      end
-    end
+    # code prior to refactoring to use hash for @projects and @tasks
+    # project = nil
 
-    # return the task with id = tid
-    marked_task = project.tasks.find { |task| task.id == tid }
+    # @projects.each do |pid, proj|
+    #   if proj.include_task?(tid)
+    #     proj.mark_as_complete(tid)
+
+    #     # set project variable to point to the project
+    #     # that has the task we are looking for
+    #     project = proj
+    #   end
+    # end
+
+    # # return the task with id = tid
+    # marked_task = project.tasks.find { |task| task.id == tid }
   end
-
-  private_class_method :new
 
 end
--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -34,10 +34,14 @@ describe 'DB' do
     # project_1 = @db.projects.first
     # puts project_1.id
     project_1_tasks = project_1.tasks
-    project_1_task_1 = project_1.tasks.first
 
-    expect(project_1_tasks.size).to eq(1)
-    expect(project_1_task_1.description).to eq("Eat Tacos")
+    # project_1_task_1 = project_1.tasks.first
+    # removing because projects will no longer keep task data
+    # expect(project_1_tasks.size).to eq(1)
+    # expect(project_1_task_1.description).to eq("Eat Tacos")
+
+    expect(@db.tasks[1]).to eq(added_task)
+    expect(@db.tasks[1].description).to eq("Eat Tacos")
 
     # expec the add task method to return the added task
     expect(added_task.description).to eq("Eat Tacos")
@@ -50,22 +54,37 @@ describe 'DB' do
 
     tasks_remaining = @db.show_proj_tasks_remaining(proj_1.id)
 
-    expect(tasks_remaining).to eq(proj_1.list_incomplete_tasks)
+    # This was a bad test because it did not fail when I refactored
+    # expect(tasks_remaining).to eq(proj_1.list_incomplete_tasks)
+
+    expect(tasks_remaining).to be_a(Array)
+    expect(tasks_remaining.size).to be(1)
+    expect(tasks_remaining.first.description).to eq("Eat Tacos")
   end
 
   it "can show completed tasks for a project given PID" do
     proj_1 = @db.create_project("The Best Project")
-    @db.add_task_to_proj(proj_1.id, "Eat Tacos", 3)
+    added_task = @db.add_task_to_proj(proj_1.id, "Eat Tacos", 3)
 
     tasks_complete = @db.show_proj_tasks_complete(proj_1.id)
 
-    expect(tasks_complete).to eq(proj_1.list_completed_tasks)
+    # This was a bad test because it did not fail when I refactored
+    # expect(tasks_remaining).to eq(proj_1.list_incomplete_tasks)
+    # expect(tasks_complete).to eq(proj_1.list_completed_tasks)
+
+    @db.mark_task_as_complete(added_task.id)
+    tasks_complete = @db.show_proj_tasks_complete(proj_1.id)
+
+    expect(tasks_complete).to be_a(Array)
+    expect(tasks_complete.size).to be(1)
+    expect(tasks_complete.first.description).to eq("Eat Tacos")
   end
 
   it "can mark a task as completed based on its id" do
     proj_1 = @db.create_project("The Best Project")
-    @db.add_task_to_proj(proj_1.id, "Eat Tacos", 3)
-    task_id = proj_1.tasks.first.id
+    added_task = @db.add_task_to_proj(proj_1.id, "Eat Tacos", 3)
+    task_id = added_task.id
+
     completed_task = @db.mark_task_as_complete(task_id)
 
     expect(completed_task).to be_a(TM::Task)
