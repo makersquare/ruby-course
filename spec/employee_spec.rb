91eff3e20b79f724d328ddc5b2751cc6bf3b2e1e
Brandon Beasley <brandontbeasley@gmail.com>
2014-03-26 14:16:29 -0500

    subbed the counter for testing

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'Employee' do
   before do
     @new_employee = TM::Employee.new('brandon')
+    TM::Employee.class_variable_set :@@counter, 0
   end
 
   it 'exists' do
@@ -16,7 +17,8 @@ describe 'Employee' do
 
   it 'automatically assigns a unique id' do
     result = @new_employee.id
-    expect(result).to eq 3
-
+    expect(result).to eq 1
   end
+
+
 end
