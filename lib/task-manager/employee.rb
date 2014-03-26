44c24d9894c62a3bd20df44a90009172ee98fa04
Brandon Beasley <brandontbeasley@gmail.com>
2014-03-26 14:12:28 -0500

    passed initialized with name, made test for unique id

--- lib/task-manager/employee.rb
+++ lib/task-manager/employee.rb
@@ -1,3 +1,9 @@
 class TM::Employee
 
+attr_accessor :name
+
+
+def initialize(name)
+  @name = name
+end
 end
--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -1,14 +1,22 @@
 require 'spec_helper'
 
 describe 'Employee' do
+  before do
+    @new_employee = TM::Employee.new('brandon')
+  end
+
   it 'exists' do
     expect(TM::Employee).to be_a(Class)
   end
 
   it 'is initialized with a name' do
-    name = "brandon"
-    new_employee = TM::Employee.new(name)
-    result = new_employee.name
-    expect(result).to eq "brandon"
+    result = @new_employee.name
+    expect(result).to eq 'brandon'
+  end
+
+  it 'automatically assigns a unique id' do
+    result = @new_employee.id
+    expect(result).to eq 2
+
   end
 end
