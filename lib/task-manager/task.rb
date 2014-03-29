7b12fa334086a6c24c78c6aaf33d452d848e7467
Brian Keane <lonesomewhistle@gmail.com>
2014-03-29 16:16:31 -0500

    Added id to task

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -1,12 +1,18 @@
+
+
 module TM
   class TM::Task
 
-    attr_reader :project_id, :description, :priority
+    attr_reader :project_id, :description, :priority, :id
+
+    @@current_id = 1
 
     def initialize(data)
       @project_id = data[:project_id]
-      @desription = data[:description]
+      @description = data[:description]
       @priority = data[:priority]
+      @id = @@current_id
+      @@current_id += 1
     end
 
   end
