efb4f409633defc7e15dd6eb3009b1f947500dec
Brian Provost <brianmprovost@gmail.com>
2014-04-15 20:33:57 -0500

    20 green tests

--- lib/timeline/database/sqlite.rb
+++ lib/timeline/database/sqlite.rb
@@ -1,4 +1,5 @@
 require 'active_record'
+require 'yaml'
 
 module Timeline
   module Database
@@ -8,6 +9,8 @@ module Timeline
           :adapter => 'sqlite3',
           :database => 'ar-ex_test'
           )
+        # configs = YAML.load_file('db/config.yml')
+        # ActiveRecord::Base.establish_connection(configs[test])
       end
 
       class User < ActiveRecord::Base
@@ -29,6 +32,10 @@ module Timeline
       end
 
       def clear_everything
+        User.delete_all
+        Team.delete_all
+        Tag.delete_all
+        Event.delete_all
       end
 
       # Users
@@ -43,26 +50,63 @@ module Timeline
       end
 
       def all_users
+        users = User.all
+        array = []
+        users.each do |user|
+          array.push(Timeline::User.new({id: user.id, name: user.name}))
+        end
+        array
       end
 
       # Teams
       def create_team(attrs)
+        team = Team.create(name: attrs[:name])
+        Timeline::Team.new({id: team.id, name: team.name})
       end
 
       def get_team(id)
+        team = Team.find(id)
+        Timeline::Team.new({id: team.id, name: team.name})
       end
 
       def all_teams
+        teams = Team.all
+        array = []
+        teams.each do |team|
+          array.push(Timeline::Team.new({id: team.id, name: team.name}))
+        end
+        array
       end
 
       # Events
       def create_event(attrs)
+        event = Event.create(name: attrs[:name], user_id: attrs[:user_id], team_id: attrs[:team_id], content: attrs[:content], created_at: Time.now)
+
+        attrs[:tags].each {|tag| Tag.create(tag: tag, event_id: event.id)} if attrs[:tags]
+        Timeline::Event.new({id: event.id, name: event.name, user_id: event.user_id, team_id: event.team_id, content: event.content, created_at: event.created_at, tags: attrs[:tags]})
       end
 
       def get_event(id)
+        event = Event.find(id)
+        Timeline::Event.new({id: event.id, name: event.name, user_id: event.user_id, team_id: event.team_id, content: event.content, created_at: event.created_at, tags: get_tags_for_event(event.id)})
       end
 
       def get_events_by_team(team_id)
+        events = Event.where(team_id: team_id)
+        array = []
+        events.each do |event|
+          array.push(Timeline::Event.new({id: event.id, name: event.name, user_id: event.user_id, team_id: event.team_id, content: event.content, created_at: event.created_at, tags: get_tags_for_event(event.id)}))
+        end
+        array.sort { |a,b| a.created_at <=> b.created_at }
+      end
+
+      def get_tags_for_event(event_id)
+        tags = Tag.where(event_id: event_id)
+        array = []
+        tags.each do |tag|
+          array.push(tag.tag)
+        end
+        array
       end
     end
   end
