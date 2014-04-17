c71d3ceef1f047d2f36c754e5ea511a44a19a9df
hubertanguyen@gmail.com <hubertanguyen@gmail.com>
2014-04-17 17:58:22 -0500

    get team events use case

--- lib/usecase/get_team_events.rb
+++ lib/usecase/get_team_events.rb
@@ -1,7 +1,20 @@
 module Timeline
   class GetTeamEvents < UseCase
     def run(inputs)
-
+      team_id = inputs[:team_id]
+      return failure(:no_team_with_that_id) if Timeline.db.get_team(team_id) == nil
+      events = Timeline.db.get_events_by_team(team_id)
+      success(:event=>events)
     end
   end
 end
+
+# def run(inputs)
+#       name = inputs[:name]
+#       team_id = inputs[:team_id]
+
+#       return failure(:no_team_with_that_id) if Timeline.db.get_team(team_id) ==nil
+#       event = Timeline.db.create_event(name: inputs[:name], team_id: inputs[:team_id])
+
+#       success(:event=>event)
+   #It should error check for user_id, team_id,
--- spec/usecase/get_team_events_spec.rb
+++ spec/usecase/get_team_events_spec.rb
@@ -10,7 +10,33 @@ describe Timeline::GetTeamEvents do
     event1 = @db.create_event(:name =>"waterpark", :team_id=>team.id)
     event2 = @db.create_event(:name=>"southbywest",:team_id=>team.id)
     event3 = @db.create_event(:name=>"hackathon",:team_id=>team.id)
+    result = subject.run(:team_id => team.id)
+    expect(result.success?).to eq true
+    expect(result.event.length).to eq 3
+  end
 
+  it "should return an error if a team does not exist" do
+    result = subject.run(:team_id => 56)
+    expect(result.error?).to eq true
+    expect(result.error).to eq :no_team_with_that_id
   end
 
 end
+
+
+ # it "I want to create an Event that is associated with a team" do
+ #    team1 = @db.create_team :name => 'Sungod'
+
+ #    event = @db.create_event(:team_id => team1.id)
+ #    expect(event.team_id).to eq(team1.id)
+ #    result = subject.run({:name =>team1.name, :team_id=>team1.id})
+ #    expect(result.success?).to eq true
+ #    expect(result.event.team_id).to eq (team1.id)
+ #  end
+
+ #  it "Will throw an error when creating an Event, if that associated team does not exists" do
+ #    # event = @db.create_event(:name => "Funtime",:team_id => team2.id)
+ #    result = subject.run({:name => "Funtime", :team_id =>15})
+ #    expect(result.error?).to eq true
+ #    expect(result.error).to eq :no_team_with_that_id
+ #  end
