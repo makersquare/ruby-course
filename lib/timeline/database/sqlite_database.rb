c22d72a86da4e7a5dab4e172a8f8933d3f85f549
Jose R. Borja <chakaitos@gmail.com>
2014-04-17 15:21:53 -0500

    Add methods for sqlite database

--- lib/timeline/database/sqlite_database.rb
+++ lib/timeline/database/sqlite_database.rb
@@ -1,20 +1,14 @@
+require 'active_record'
+
 module Timeline
   module Database
-
-    # def self.db
-    #   @__db_instance ||= DB.new(@app_db_name)
-    # end
-
-    # def self.db_name=(db_name)
-    #   @app_db_name = db_name
-    # end
 
     class SQLiteDatabase
 
       def initialize
         ActiveRecord::Base.establish_connection(
           :adapter => 'sqlite3',
-          :database => 'db-test.db'
+          :database => 'timeline-test.db'
         )
       end
 
@@ -27,19 +21,109 @@ module Timeline
         has_many :events
       end
 
-      class Events < ActiveRecord::Base
-        belongs_to :user, :team
+      class Event < ActiveRecord::Base
+        belongs_to :user
+        belongs_to :team
+        has_many :tags
+      end
+
+      class Tag < ActiveRecord::Base
+        belongs_to :event
       end
 
       # Now implement you database methods here
 
+      def clear_everything
+        User.delete_all
+        Team.delete_all
+        Event.delete_all
+        Tag.delete_all
+      end
+
+      # # # # #
+      # Users #
+      # # # # #
+
       def create_user(attrs)
-        # NOTE the difference betwee the two `User` classes.
+        # NOTE the difference between the two `User` classes.
         # The first inherits from ActiveRecord, while
         # the second is your app's entity class
         ar_user = User.create(attrs)
-        MyApp::User.new(ar_user.attributes)
+        Timeline::User.new(ar_user.attributes)
       end
+
+      def get_user(id)
+        # user = User.find(id)
+        Timeline::User.new(User.find(id).attributes)
+      end
+
+      def all_users
+        users = User.all
+      end
+
+      # # # # #
+      # Teams #
+      # # # # #
+
+      def create_team(attrs)
+        ar_team = Team.create(attrs)
+        Timeline::Team.new(ar_team.attributes)
+      end
+
+      def get_team(id)
+        # team = Team.find(id)
+        Timeline::Team.new(Team.find(id).attributes)
+      end
+
+      def all_teams
+        teams = Team.all
+      end
+
+      ## # # # #
+      # Events #
+      # # # # ##
+
+      def create_event(attrs)
+        # puts "ATTRS: #{attrs}"
+        tags = attrs[:tags]
+        attrs.delete(:tags)
+        ar_event = Event.create(attrs)
+        event = Timeline::Event.new(ar_event.attributes)
+        event.tags = []
+          if tags != nil
+            tags.each do |tag|
+              Tag.create( { event_id: ar_event.id, name: tag } )
+            end
+          end
+        event
+      end
+
+      def get_event(id)
+        event = Event.find(id)
+        event_tags = event.tags
+       
+        Timeline::Event.new({ id: event.id, name: event.name, tags: event_tags.map { |tag| tag.name }})
+      end
+
+      def get_events_by_team(team_id)
+        team = Team.find(team_id)
+        team.events.sort_by(&:created_at)
+      end
+
+      # # # # #
+      # Tags  #
+      # # # # #
+
+      # def create_tag(attrs)
+      #   ar_tag = Tag.create(attrs)
+      #   Timeline::Tag.new(ar_tag.attributes)
+      # end
+
+      # def get_tag(id)
+      #   tag = Tag.find(id)
+      #   binding.pry
+      #   # Timeline::Event.new(Event.find(id).attributes)
+      # end
 
     end
   end
