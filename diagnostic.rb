d26a74ffc744ad0a58ffca5b3d2129af92a77f2a
Benjamin Poon <benjamintpoon@gmail.com>
2014-04-21 15:47:15 -0500

    implement HashProblems::create_empty_hash

--- diagnostic.rb
+++ diagnostic.rb
@@ -160,6 +160,7 @@ class ArrayProblems
 
   def self.greet_everyone(people)
     # TODO: Select and return all numbers higher than `min`
+    people.map { |person| "Hello, " + person}
   end
 end
 
@@ -172,6 +173,7 @@ class HashProblems
 
   def self.create_empty_hash
     # TODO: Complete this method
+    return {}
   end
 
   # # # # # # #
