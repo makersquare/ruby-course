420a1cf7aac9fb3ac67386318efe7b71e2991bdc
Shehzan Devani <shehzandevani@gmail.com>
2014-04-22 17:29:43 -0500

    Finished exercises 0-4. Starting exercise 5

--- exercises.rb
+++ exercises.rb
@@ -4,25 +4,28 @@ module Exercises
   #  - Triples a given string `str`
   #  - Returns "nope" if `str` is "wishes"
   def self.ex0(str)
-    # TODO
+    if str == "wishes"
+      return 'nope'
+    end
+    str*3
   end
 
   # Exercise 1
   #  - Returns the number of elements in the array
   def self.ex1(array)
-    # TODO
+    array.size
   end
 
   # Exercise 2
   #  - Returns the second element of an array
   def self.ex2(array)
-    # TODO
+    array[1]
   end
 
   # Exercise 3
   #  - Returns the sum of the given array of numbers
   def self.ex3(array)
-    # TODO
+    array.inject(:+)
   end
 
   # Exercise 4
--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -1,8 +1,66 @@
 require 'rubygems'
 require 'rspec'
+require_relative "../exercises.rb"
 
 describe "Exercises" do
   it "should work" do
     expect(true).to eq(true)
   end
+
+  describe "ex0" do
+    it "triples the length of a string" do
+      result = Exercises.ex0 "ha"
+      expect(result).to eq("hahaha")
+    end
+
+    it "returns 'nope' if the string is 'wishes'" do
+      result = Exercises.ex0 "wishes"
+      expect(result).to eq("nope")
+    end
+  end
+
+  describe "ex1" do
+    it "should return 0 for an empty array" do
+      result = Exercises.ex1 []
+      expect(result).to eq(0)
+    end
+
+    it "should return the size of an array" do
+      result = Exercises.ex1 [4, 3, 2, 5]
+      expect(result).to eq(4)
+    end
+  end
+
+  describe "ex2" do
+    it "should return nil for an array shorter than 2 elements" do
+      result = Exercises.ex2 []
+      expect(result).to eq(nil)
+
+      result = Exercises.ex2 [1]
+      expect(result).to eq(nil)
+    end
+
+    it "should return the second element of a longer array" do
+      result = Exercises.ex2 [4, 5]
+      expect(result).to eq(5)
+
+      result = Exercises.ex2 [1, 2, 3]
+      expect(result).to eq(2)
+    end
+  end
+
+  describe "ex3" do
+    it "should sum up an array" do
+      result = Exercises.ex3 [5,6,7]
+      expect(result).to eq(18)
+    end
+  end
+
+  describe "ex4" do
+    it "should return the largest number in this array" do
+      result = Exercises.ex4 [9,3,2,5]
+      expect(result).to eq(9)
+    end
+  end
+
 end
