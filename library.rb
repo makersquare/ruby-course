cc30af24e90d1c4b582c0b058b71172d2ac32bdd
David Colby <davidlcolby@gmail.com>
2014-04-22 14:12:16 -0500

    Books can be added to librarys and checked out

--- library.rb
+++ library.rb
@@ -1,26 +1,47 @@
 
 class Book
-  attr_reader :author
+  attr_accessor :author, :title, :id, :status
 
   def initialize(title, author)
     @author = author
+    @title = title
+    @status = "available"
+    @id = nil
+  end
+
+  def check_out
+    if @status == 'checked_out'
+      false
+    else
+      @status = 'checked_out'
+    end
+  end
+
+  def check_in
+    if @status == 'checked_out'
+      @status = 'available'
+    end
   end
 end
 
 class Borrower
+  attr_accessor :name
+
   def initialize(name)
+    @name = name
   end
 end
 
 class Library
+  attr_reader :books
+
   def initialize(name)
     @books = []
   end
 
-  def books
-  end
-
-  def add_book(title, author)
+  def register_new_book(book)
+    book.id = rand()
+    @books << book
   end
 
   def check_out_book(book_id, borrower)
