bc8cac9ca866dbbc61c7ace3484f933fbb8a22a0
Tony Fazio <antoniojfazio@gmail.com>
2014-04-22 16:09:41 -0500

    13  down

--- library.rb
+++ library.rb
@@ -86,6 +86,7 @@ class Library
   end
 
   def check_in_book(book)
+    book.check_in
   end
 
   def available_books
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -128,7 +128,7 @@ describe Library do
     expect(book_again).to be_nil
   end
 
-  xit "allows a Borrower to check a book back in" do
+  it "allows a Borrower to check a book back in" do
     lib = Library.new("LBJ")
     lib.register_new_book("Finnegans Wake", "James Joyce")
     book_id = lib.books.first.id
