63e000045c4ddd9d22128616370a8a63eeea6072
David Colby <davidlcolby@gmail.com>
2014-04-24 17:09:37 -0500

    Clean up library.rb comments

--- library.rb
+++ library.rb
@@ -48,6 +48,7 @@ class Library
   end
 
   def register_new_book(book)
+    # Assigns a random id to Book when it is added to the library. This is currently unused.
     book.id = rand()
     @books << book
   end
@@ -61,15 +62,17 @@ class Library
       end
     end
 
-    # If less than 2 books have been checked out by the borrower and the book is available, book can be checked out
-    if check_out_count < 2
-      if (book.status == 'available') && (borrower.has_overdue_books == false)
+    # If less than 2 books have been checked out by the borrower, the book is available and the borrower does not have any overdue books, check out the book
+    # If the book is unavailable and the borrower can checkout a book, add them to the queued_borrowers hash
+
+    if (check_out_count) < 2 && (borrower.has_overdue_books == false)
+      if (book.status == 'available')
         book.status = 'checked_out'
         @borrowers[book] = borrower
         book.due_date = Time.now + (86400*7)
         checked_out_books << book
         return book
-      elsif (book.status == 'checked_out') && (borrower.has_overdue_books == false)
+      elsif (book.status == 'checked_out')
         @queued_books << book
         @queued_borrowers[book] = borrower
         return 0
@@ -83,11 +86,15 @@ class Library
   end
 
   def check_in_book(book)
-    # Remove book from borrowers hash and change book status to available
+    # Remove book from borrowers hash and change book status to available.
+    # Remove borrower from has_overdue_books status - this should probably check to make sure they don't have any other overdue books before doing so
+    @borrowers[book].has_overdue_books = false
     @borrowers.delete(book)
     @checked_out_books.delete(book)
     book.status = 'available'
-    book.overdue = false
+    if book.overdue
+      book.overdue = false
+    end
 
     # If a book is queued for checkout when it is checked in, it should be checked out to the queued borrower
     if @queued_borrowers[book]
@@ -107,12 +114,13 @@ class Library
   end
 
   def overdue_books(book, now=Time.now)
+    # Now is an optional argument created to make sure tests pass correctly.
     now = now
     if book.due_date < now
       book.overdue = true
     end
 
-    # Find the borrower of the overdue book and set mark them as having overdue books
+    # Find the borrower of the overdue book and set them as having overdue books, to prevent additional borrowing
     od_borrower = @borrowers.select{|book| book}
     od_borrower[book].has_overdue_books = true
   end
