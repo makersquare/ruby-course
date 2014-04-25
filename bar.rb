1b7ebf3b6d4e2b1897c0721fd00737bd50924203
Benjamin Poon <benjamintpoon@gmail.com>
2014-04-25 15:09:21 -0500

    create custom getters / setters for Bar happy_discount

--- bar.rb
+++ bar.rb
@@ -1,4 +1,47 @@
 require 'time' # you're gonna need it
 
 class Bar
+
+  attr_reader :name, :menu_items
+  attr_accessor :happy_discount
+
+  def initialize(name)
+    @name = name
+    @menu_items = []
+    @happy_discount = 0
+  end
+
+  def add_menu_item(name, price)
+    item = Item.new(name, price)
+    @menu_items << item
+  end
+
+  def happy_discount
+    happy_hour? ? @happy_discount: 0
+  end
+
+  def happy_discount=(new_discount)
+    if new_discount > 1
+      @happy_discount = 1
+    elsif new_discount < 0
+      @happy_discount = 0
+    else
+      @happy_discount = new_discount
+    end
+  end
+
+  def happy_hour?
+  end
 end
+
+
+class Item
+
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+end
+
