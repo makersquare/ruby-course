cede3cbf4002d1f67709154c029c24edacd8b1e7
Ashley McKemie <a.mckemie@utexas.edu>
2014-04-25 15:40:36 -0500

    Created working happy_hour? method

--- bar.rb
+++ bar.rb
@@ -10,6 +10,11 @@ class Bar
   end
 
   def happy_hour?
+    if Time.now >= Time.parse("3pm") && Time.now <= Time.parse("4pm")
+      return true
+    else
+      return false
+    end
   end
 
   def add_menu_item(item, cost)
