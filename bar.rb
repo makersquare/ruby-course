e6d8ecfef50d7d2af1ce5e5290a4e2ed1f395171
David Colby <davidlcolby@gmail.com>
2014-04-25 15:06:51 -0500

    Implement happy hour method

--- bar.rb
+++ bar.rb
@@ -9,11 +9,13 @@ class Bar
     @happy_discount = 0
   end
 
+  # Take in a name and price and create a new Item instance based on these args. Add new Item to @menu_items
   def add_menu_item(name,price)
     item = Item.new(name,price)
     @menu_items << item
   end
 
+  # Custom happy hour discount setter - always between 0 and 1
   def happy_discount=(discount)
     if discount >= 1
       @happy_discount = 1
@@ -33,6 +35,12 @@ class Bar
   end
 
   def happy_hour?
+    now = Time.now
+    if now > Time.parse("3pm") && now < Time.parse("4pm")
+      true
+    else
+      false
+    end
   end
 
 end
