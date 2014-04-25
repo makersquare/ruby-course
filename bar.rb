22065d761e39348c0e1a5014d9913049e50ca634
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-04-25 14:46:12 -0500

    Has empty menu. -8 failures

--- bar.rb
+++ bar.rb
@@ -2,7 +2,13 @@ require 'time' # you're gonna need it
 
 class Bar
   attr_reader :name
+  attr_accessor :array
+
   def initialize(name)
     @name = name
+  end
+
+  def menu_items
+    @array = []
   end
 end
