40719cc82748fbc02735ccd112838fac80f00a43
Bobby Smith <bobbysmith24@gmail.com>
2014-04-25 15:44:58 -0500

    passed set dicsount method

--- bar.rb
+++ bar.rb
@@ -1,13 +1,31 @@
 require 'time' # you're gonna need it
 
+class Item
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+end
+
 class Bar
   attr_reader :name
-  attr_accessor :menu_items
+  attr_accessor :menu_items, :happy_discount
 
   def initialize(name)
     @name = name
     @menu_items = []
+    @happy_discount = 0
   end
+
+  def add_menu_item(name, price)
+    @menu_items << Item.new(name, price)
+  end
+
+  # def happy_discount
+  #   @discount = 0
+  # end
 end
 
 
