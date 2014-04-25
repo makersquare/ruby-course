815f8571c63147c7ae19c5b9bd21830f791ac6bc
Ashley McKemie <a.mckemie@utexas.edu>
2014-04-25 15:20:42 -0500

    Created custom happy_discount getter

--- bar.rb
+++ bar.rb
@@ -2,15 +2,27 @@ require 'time' # you're gonna need it
 
 class Bar
   attr_reader :name, :menu_items
-  attr_accessor :happy_discount
+  attr_writer :happy_discount
+
   def initialize(name)
     @name = name
     @menu_items = []
     @happy_discount = 0
   end
 
+  def happy_hour?
+  end
+
   def add_menu_item(item, cost)
     @menu_items << Item.new(item, cost)
+  end
+
+  def happy_discount
+    if happy_hour?
+      @happy_discount
+    else
+      0
+    end
   end
 
 end
