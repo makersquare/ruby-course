93411da6117e74709166a5818971d01ff6bf0c6c
Ashley McKemie <a.mckemie@utexas.edu>
2014-04-25 14:57:28 -0500

    Created Item class, modified add_menu_items method to add newly created Item object to array

--- bar.rb
+++ bar.rb
@@ -8,6 +8,16 @@ class Bar
   end
 
   def add_menu_item(item, cost)
-    @menu_items << [item, cost]
+    @menu_items << Item.new(item, cost)
+  end
+
+end
+
+class Item
+  attr_reader :name, :price
+  def initialize(name, price)
+    @name = name
+    @price = price
   end
 end
+
