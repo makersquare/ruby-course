2e9272c3ad1b1373e88350a7b7472402b5ecb8b1
Caresa Huddleston <caresa.huddleston@gmail.com>
2014-04-26 12:11:47 -0500

    added tests and methods to test for happy hour

--- bar.rb
+++ bar.rb
@@ -1,4 +1,54 @@
 require 'time' # you're gonna need it
 
 class Bar
+ attr_reader :name
+ attr_accessor :menu_items, :happy_discount, :start_hr, :finish_hr
+
+ def initialize(name, menu_items=0)
+  @name = name
+  @menu_items = []
+  @happy_discount = 0
+ end
+
+ def add_menu_item(name, price)
+  item = MenuItems.new(name, price)
+  @menu_items << item
+ end
+
+ def happy_hour?
+  if Time.now >= Time.parse("3pm") && Time.now <= Time.parse("4pm")
+    true
+  else
+    false
+  end
+ end
+
+ def happy_discount
+  if happy_hour?
+    @happy_discount
+  else
+    0
+  end
+ end
+
+ def happy_discount=(discount)
+    if discount >= 0 && discount <= 1
+      @happy_discount = discount
+    elsif discount > 1
+      @happy_discount = 1
+    else
+      @happy_discount = 0
+    end
+ end
+end
+
+class MenuItems
+
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
 end
