20d128707cd4b70e117d7a2bb2db51ea457bcbf9
Michael Ornellas <mike@themakersquare.com>
2014-04-27 15:39:50 -0500

    Refactor initialize to use .generate_id instead, refactor tests

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,12 +1,16 @@
 
 class TM::Project
   attr_reader :id
-  @@id_counter = 0
 
   def initialize(name)
     @name = name
-    @id = @@id_counter
-    @@id_counter += 1
+    @id = self.class.generate_id
+  end
+
+  def self.generate_id
+    tmp = @@id_counter ||= 0
+    @@id_counter +=1
+    tmp
   end
 
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -8,6 +8,10 @@ describe 'Project' do
 
   let(:project) { TM::Project.new('Moving') }
 
+  before(:each) do
+    reset_class_variables(TM::Project)
+  end
+
   describe '.initialize' do
     it "requires a name" do
       expect { TM::Project.new }.to raise_error(ArgumentError)
@@ -15,8 +19,16 @@ describe 'Project' do
     end
 
     it "assigns a unique id" do
-      expect(project.id).to eq(1)
-      expect(TM::Project.new('Second').id).to eq(2)
+      expect(project.id).to eq(0)
+      expect(TM::Project.new('Second').id).to eq(1)
+    end
+  end
+
+  describe '.generate_id' do
+    it 'generates a unique id' do
+      expect(TM::Project.generate_id).to eq(0)
+      expect(TM::Project.generate_id).to eq(1)
+      expect(TM::Project.generate_id).to eq(2)
     end
   end
 end
