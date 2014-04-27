68e8b2e8920e111f3aca4bc82f0c9f7bd68670d3
Michael Ornellas <mike@themakersquare.com>
2014-04-27 15:27:24 -0500

    Add unique id to TM::Project

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,6 +1,12 @@
 
 class TM::Project
+  attr_reader :id
+  @@id_counter = 0
+
   def initialize(name)
     @name = name
+    @id = @@id_counter
+    @@id_counter += 1
   end
+
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,14 +1,22 @@
 require 'spec_helper'
+require 'pry'
 
 describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
 
+  let(:project) { TM::Project.new('Moving') }
+
   describe '.initialize' do
     it "requires a name" do
       expect { TM::Project.new }.to raise_error(ArgumentError)
-      expect { TM::Project.new('new project') }.not_to raise_error
+      expect { project }.not_to raise_error
+    end
+
+    it "assigns a unique id" do
+      expect(project.id).to eq(1)
+      expect(TM::Project.new('Second').id).to eq(2)
     end
   end
 end
