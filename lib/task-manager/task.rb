76ad6b1b1c67954e989b6051e42f4dc5b9af3690
Michael Ornellas <mike@themakersquare.com>
2014-04-27 15:44:15 -0500

    Add attributes to TM::Task

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -1,3 +1,8 @@
 
 class TM::Task
+  def initialize(project_id, description, priority_number)
+    @project_id = project_id
+    @description = description
+    @priority_number = priority_number
+  end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -4,4 +4,11 @@ describe 'Task' do
   it "exists" do
     expect(TM::Task).to be_a(Class)
   end
+
+  describe '.initialize' do
+    it "requires a description, project_id and priority_number" do
+      expect { TM::Task.new }.to raise_error(ArgumentError)
+      expect { TM::Task.new("A new task", 0, 1) }.not_to raise_error
+    end
+  end
 end
