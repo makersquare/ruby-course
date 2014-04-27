ac3691d704a41b04f01b74a8c8be4f655904dbbf
Michael Ornellas <mike@themakersquare.com>
2014-04-27 15:50:51 -0500

    Add mark_complete and complete? methods to TM::Task

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -4,5 +4,14 @@ class TM::Task
     @project_id = project_id
     @description = description
     @priority_number = priority_number
+    @complete = false
+  end
+
+  def complete?
+    @complete
+  end
+
+  def mark_complete
+    @complete = true
   end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe 'Task' do
+
+  let(:task) { TM::Task.new("A new task", 0, 1) }
+
   it "exists" do
     expect(TM::Task).to be_a(Class)
   end
@@ -8,7 +11,15 @@ describe 'Task' do
   describe '.initialize' do
     it "requires a description, project_id and priority_number" do
       expect { TM::Task.new }.to raise_error(ArgumentError)
-      expect { TM::Task.new("A new task", 0, 1) }.not_to raise_error
+      expect { task }.not_to raise_error
+    end
+  end
+
+  describe '#mark_complete' do
+    it "marks a task complete" do
+      expect(task.complete?).to eq(false)
+      expect(task.mark_complete).to eq(true)
+      expect(task.complete?).to eq(true)
     end
   end
 end
