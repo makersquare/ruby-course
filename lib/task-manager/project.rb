42f601f16b26a1542e351a12639fd7192c43deff
Michael Ornellas <mike@themakersquare.com>
2014-04-27 15:16:28 -0500

    Add name to TM::Project
    Add name parameter to TM::Project .initialize method that stores in @name.

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,6 @@
 
 class TM::Project
+  def initialize(name)
+    @name = name
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,4 +4,11 @@ describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
+  describe '.initialize' do
+    it "requires a name" do
+      expect { TM::Project.new }.to raise_error(ArgumentError)
+      expect { TM::Project.new('new project') }.not_to raise_error
+    end
+  end
 end
