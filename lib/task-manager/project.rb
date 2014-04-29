d9710abceecf9feab6b70d43ded82803e7461356
Tony Fazio <antoniojfazio@gmail.com>
2014-04-29 14:37:14 -0500

    added initialize method to TM::Project to allow optional name but automatically generate unique id

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,13 @@
 
 class TM::Project
+  attr_reader :id
+  attr_accessor :name
+
+  @@class_id = 1
+
+  def initialize(name=nil)
+    @name = name
+    @id = @@class_id
+    @@class_id +=1
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,4 +4,10 @@ describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
+  it "allows project to be created with name optional but autmatically given unique id" do
+    project = TM::Project.new("hello")
+    expect(project.id).to eq(1)
+    expect(project.name).to eq("hello")
+  end
 end
