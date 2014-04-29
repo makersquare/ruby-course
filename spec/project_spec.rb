b548eb927baaa71ecb5af6b3c24808edab76470a
Benjamin Poon <benjamintpoon@gmail.com>
2014-04-29 15:46:18 -0500

    Wrote a buncha tests, need more

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,7 +1,54 @@
 require 'spec_helper'
 
 describe 'Project' do
-  it "exists" do
-    expect(TM::Project).to be_a(Class)
+
+  describe '.initialize' do
+    it "should be created with a name" do
+      project = TM::Project.new('Project Mayhem')
+      expect(project.name).to eq "Project Mayhem"
+      expect(TM::Project.new()).to raise_error
+    end
+
+    it "must have a unique id" do
+      project1 = TM::Project.new('Unoriginally Named Project')
+      project2 = TM::Project.new('Unoriginally Named Project')
+      expect(project1.id).to_not eq project2.id
+    end
   end
+
+  describe '.add_task' do
+    let (:project) {
+      my_project = Project.new('Some Project')
+      my_project.add_task(task1)
+      my_project.add_task(task2)
+    }
+
+    let (:task1) { task1 = Task.new(my_project.id, 'do some stuff', 1) }
+    let (:task2) { task2 = Task.new(my_project.id, 'do other stuff', 2) }
+
+    it "stores a list of task objects" do
+      my_project.add_task(task1)
+      my_project.add_task(task2)
+      expect(my_project.tasks).to eq [task1, task2]
+    end
+
+    it "has a timestamp of when it is created" do
+      allow(Time).to receive(:now).and_return(Time.parse("2014-04-29 12:00PM"))
+      fresh_task = Task.new(my_project.id, 'do stuff NOW', 3)
+      expect(fresh_task.timestamp).to eq (Time.parse("2014-04-29 12:00PM"))
+    end
+  end
+
+  describe '.completed_tasks' do
+    it "retrieves a list of all completed tasks, sorted by creation date" do
+
+    end
+
+    it "retrieves a list of all incomplete tasks, sorted by priority" do
+    end
+
+    it "knows older tasks get priority over newer ones with the same priority" do
+    end
+  end
+
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,7 +1,37 @@
 require 'spec_helper'
 
 describe 'Task' do
-  it "exists" do
-    expect(TM::Task).to be_a(Class)
+
+  let(:myproject) { project = TM::Project.new('MyProject') }
+  let(:task1) { task1 = TM::Task.new(project_id, 'take out the trash', 1) }
+  let(:task2) { task2 = TM::Task.new(project_id, 'clean up your room', 2) }
+
+  describe '.initialize' do
+    it "should have a unique id" do
+      expect(task1.id).to_not eq task2.id
+    end
+
+    it "should have a description string" do
+      expect(trash_task.description).to eq 'take out the trash'
+    end
+
+    it "should have a priority from 1 to 3" do
+      expect(task.priority).to be_within(2).of(3)
+      expect(task.priority).to be_within(1).of(3)
+    end
+  end
+
+  describe '.mark_complete' do
+
+    it "can be marked as complete, returning true if successful" do
+      expect(task1.mark_complete).to eq true
+      expect(task1.status).to eq 'complete'
+    end
+
+    it "shouldn't mark completed tasks as complete, returning nil" do
+      task1.mark_complete
+      expect(task1.mark_complete).to eq nil
+      expect(task1.status).to eq 'complete'
+    end
   end
 end
