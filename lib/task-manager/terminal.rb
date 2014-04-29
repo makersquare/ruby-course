4775165fcc9d87112666518144ebf26c986170f2
Canaan Davis <canaandavis@canaans-mbp.att.net>
2014-04-29 17:21:50 -0500

    Add create project for terminal class

--- lib/task-manager/terminal.rb
+++ lib/task-manager/terminal.rb
@@ -1,2 +1,13 @@
 class TM::TerminalClient
+
+  attr_reader :projects
+
+  def initialize
+    @projects = []
+  end
+
+  def create_project(name)
+    @projects << TM::Project.new(name)
+  end
+
 end
--- spec/terminal_spec.rb
+++ spec/terminal_spec.rb
@@ -5,4 +5,22 @@ describe 'TerminalClient' do
   it "exists" do
     expect(TM::TerminalClient).to be_a(Class)
   end
+
+  context 'a terminal client is initialized' do
+    let(:terminal) { TM::TerminalClient.new }
+
+    it 'has an array for projects' do
+      expect(terminal.projects).to eq([])
+    end
+  end
+
+  context 'a new project is created' do
+    let(:terminal) { TM::TerminalClient.new }
+
+    it 'can create a new project' do
+      terminal.create_project("Project 1")
+      expect(terminal.projects.count).to eq(1)
+    end
+
+  end
 end
