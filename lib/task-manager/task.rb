59242b8b96b0b47be471bb1fa744f4ac9e238e67
Ashley McKemie <a.mckemie@utexas.edu>
2014-04-29 15:13:01 -0500

    Created failing tests for TM::Task initialize method and implemented method

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -1,4 +1,13 @@
 
 class TM::Task
+  @@counter = 1
+  attr_reader :description, :project_id, :priority, :task_id
 
+  def initialize(description, project_id, priority)
+    @description = description
+    @project_id = project_id
+    @priority = priority
+    @task_id = @@counter
+    @@counter += 1
+  end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -4,4 +4,31 @@ describe 'Task' do
   it "exists" do
     expect(TM::Task).to be_a(Class)
   end
+
+  describe 'initialize' do
+
+    it "Creates a task that has a description" do
+      project1 = TM::Project.new("Grades")
+      task = TM::Task.new("Create gradebook", project1.id, 1)
+      expect(task.description).to eq("Create gradebook")
+    end
+
+    it "Creates a task that assigns the project's id to it" do
+      project1 = TM::Project.new("Grades")
+      task = TM::Task.new("Create gradebook", project1.id, 1)
+      expect(task.project_id).to eq(project1.id)
+    end
+
+    it "Creates a task that has a priority number" do
+      project1 = TM::Project.new("Grades")
+      task = TM::Task.new("Create gradebook", project1.id, 1)
+      expect(task.priority).to eq(1)
+    end
+
+    it "Creates a task that has it's own id number" do
+      project1 = TM::Project.new("Grades")
+      task = TM::Task.new("Create gradebook", project1.id, 1)
+      expect(task.task_id).to eq(4)
+    end
+  end
 end
