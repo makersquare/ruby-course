c9be4023c1f42f5cac33fcb2240877dd864f9cb9
Greg Cosgrove <gregcosgrove@gmail.com>
2014-04-29 16:02:41 -0500

    Add test for project instance initalization to equal empty array and pass

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -2,16 +2,16 @@
 class TM::Project
 
 attr_reader :created, :project_id, :add_task
-attr_accessor :name, :task
+attr_accessor :name, :task, :project
 
 @@project_count = 0
 
-  def initialize(name)
+  def initialize(name="New Project")
+    @project = []
     @name = name
     @created = Time.now
     @project_id = @@project_count
     @@project_count += 1
-    @task = []
   end
 
   def add_task(task)
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -5,4 +5,9 @@ describe 'Project' do
     expect(TM::Project).to be_a(Class)
   end
 
+  it "creates an empty array at initalization" do
+    new_project = TM::Project.new
+    expect(new_project.project).to eq([])
+  end
+
 end
