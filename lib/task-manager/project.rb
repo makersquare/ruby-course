284bc924a79c5c6e2f9d7a3c1b374c257c9212d4
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-04-29 15:11:39 -0500

    Fails a new project can be created with a name.

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,12 @@
 
 class TM::Project
+
+    attr_accessor :name, :pid
+
+    @pid = 0
+    def initialize(name)
+      @name = name
+      @pid += 1
+    end
+
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,7 +1,29 @@
 require 'spec_helper'
 
+ describe 'Project' do
+    it "exists" do
+      expect(TM::Project).to be_a(Class)
+    end
+
+    it "allows a new project to be created with a name" do
+      shopping = TM::Project.new("Shopping")
+      expect(shopping.name).to eq("Shopping")
+      expect(shopping.pid).to eq(1)
+    end
+end
+
+
 describe 'Task' do
   it "exists" do
     expect(TM::Task).to be_a(Class)
   end
+
+  xit "A new task can be created with a project id, description, and priority number." do
+    buy_shoes = TM::Task.new(1,"Buy a new pair of shoes",1)
+    expect(buy_shoes[:pid]).to eq(1)
+    expect(buy_shoes[:description]).to eq("Buy a new pair of shoes")
+    expect(buy_shoes[:pn]).to eq(1)
+  end
+
+  # xit "A new task can be complete, specified by id" do
 end
