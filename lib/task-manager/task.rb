9663fb4a6cd47380027af5d57d602efd961b6799
Tony Fazio <antoniojfazio@gmail.com>
2014-04-29 14:44:03 -0500

    allows task to be created with project id, description, and priority number

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -1,3 +1,10 @@
 
 class TM::Task
+  attr_reader :project_id
+  attr_accessor :description, :priority_number
+  def initialize(project_id, description, priority_number)
+    @project_id = project_id
+    @description = description
+    @priority_number = priority_number
+  end
 end
