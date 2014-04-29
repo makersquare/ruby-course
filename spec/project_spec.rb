d4a2597306ac0c26d72dea5461d28c19b8772aaf
Said Maadan <saidmaadan@gmail.com>
2014-04-29 15:05:58 -0500

    Adding first test to project class initialize method

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,4 +4,16 @@ describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
+  it "has project name" do
+  project1 = TM::Project.new("NYC Project")
+  project1.name.should == "NYC Project"
+end
+
+ it "has a string representaion" do
+  project1 = TM::Project.new("NYC Project")
+  project1.should == "Welcome to Project Manager Pro®. What can I do for you today?"
+ end
+
+
 end
