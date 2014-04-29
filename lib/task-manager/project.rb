e01e7ffe01851bb1616eb9bc33b3dad44d4c6a69
Ashley McKemie <a.mckemie@utexas.edu>
2014-04-29 14:57:51 -0500

    Created failing tests for initialize method and implemented method in TM::Project class

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,11 @@
 
 class TM::Project
+  @@counter = 1
+  attr_accessor :name
+  attr_reader :id
+  def initialize(name)
+    @name = name
+    @id = @@counter
+    @@counter += 1
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,4 +4,17 @@ describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
+  describe 'initialize' do
+    it "creates a new project with a name" do
+      project1 = TM::Project.new("Grades")
+
+      expect(project1.name).to eq("Grades")
+    end
+
+    it "gives a new project a unique id" do
+      project1 = TM::Project.new("Grades")
+      expect(project1.id).to_not raise_error()
+    end
+  end
 end
