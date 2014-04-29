1ae271f738361f0779531c5173e8e4e8bb3f344b
Christian Catalan <crcatala@gmail.com>
2014-04-29 15:15:00 -0500

    started writing rsepc tests

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,7 +1,71 @@
 require 'spec_helper'
 
 describe 'Project' do
-  it "exists" do
-    expect(TM::Project).to be_a(Class)
+  # it "exists" do
+  #   expect(TM::Project).to be_a(Class)
+  # end
+
+  describe "initialize method" do
+  	it "exists" do
+  		expect(TM::Project).to be_a(Class)
+  	end
+
+  	it "can be initialized with a name" do
+
+  	end
+
+  	it "has a default name of 'default' if name not specified" do
+  		
+  	end
+
+  	it "can be initialized with a name" do
+
+  	end
+
+  	it "creates a unique ID is created starting with id=1" do
+
+  	end
+
+  	it "creates multiple unique IDS in numerical order" do
+
+  	end
+
+  	it "initialiezes with a empty tasks array" do
+
+  	end
+
   end
+
+  describe "retrieve tasks method" do
+  	# ex retrieve_tasks(by_date, by_priority, by_completed)
+
+  	it "returns an array of task objects" do
+
+  	end
+
+  	it "can return tasks ordered by date created" do
+
+  	end
+
+  	it "can return tasks ordered by priority" do
+
+  	end
+
+  	it "can return just completed tasks" do
+
+  	end
+
+
+  	it "can return just incompleted tasks" do
+
+  	end
+  	
+
+
+  end
+
+  describe "sort by date method" do
+  	
+  end
+
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,7 +1,58 @@
 require 'spec_helper'
 
 describe 'Task' do
-  it "exists" do
-    expect(TM::Task).to be_a(Class)
-  end
+	describe "initialize method" do
+		it "exists" do
+			expect(TM::Task).to be_a(Class)
+		end
+
+		it "can be initialized with a name" do
+
+		end
+
+		it "has a default name of 'default' if name not specified" do
+
+		end
+
+		it "creates a unique ID is created starting with id=1" do
+
+		end
+
+		it "creates multiple unique IDS in numerical order" do
+
+		end
+
+		it "can be initialized with a description" do
+
+		end
+
+		it "has default description of 'none'" do
+
+		end
+
+		it "has a priority number" do
+
+		end
+
+		it "has a default priority number of 1" do
+
+		end
+
+		it "has a creation date" do
+
+		end
+
+		it "creation date works with Time.now" do
+
+		end
+
+		it "has a completed boolean variable with default value false" do
+
+		end
+
+		it "has a completed date with default value nil" do 
+		end
+
+
+	end
 end
