79d04d26877f6f7b27f6e140f709fb93661d9526
David Colby <davidlcolby@gmail.com>
2014-04-30 17:26:17 -0500

    Prompt user for new command after each command executes

--- lib/task-manager.rb
+++ lib/task-manager.rb
@@ -35,21 +35,25 @@ class TMTerminal
       puts "\tadd PID PRIORITY DESC - Add a new task to project with id=PID"
       puts "\tmark TID - Mark task with id=TID as complete"
       puts "\texit with exit"
+      print "Enter a command: "
     when "list"
       puts "Project ID\tProject Name"
       TM::Project.list_projects.each do |project|
         puts"#{project.pid}\t\t#{project.name}"
       end
+      print "Enter a command: "
     when "create"
       print "Enter the project's name: "
       name = gets.chomp()
       new_project = TM::Project.new(name)
+      print "Enter a command: "
     when "show"
       project = TM::Project.get_project(1)
       puts "Priority\tID\tDescription"
       project.incomplete_tasks.each do |task|
         puts "#{task.priority}\t#{task.task_id}\t#{task.description}"
       end
+      print "Enter a command: "
     when "history"
       print "Enter the project id: "
       id = gets.chomp().to_i
@@ -57,6 +61,7 @@ class TMTerminal
       project.completed_tasks.each do |task|
         puts "#{task.task_id} | #{task.description}"
       end
+      print "Enter a command: "
     when "add"
       print "Enter the project id: "
       project = gets.chomp().to_i
@@ -66,10 +71,12 @@ class TMTerminal
       priority = gets.chomp().to_i
 
       TM::Task.new(project,description,priority)
+      print "Enter a command: "
     when "mark"
       print "Enter the task id: "
       id = gets.chomp().to_i
       TM::Task.complete_task(id)
+      print "Enter a command: "
     end
   end while choice != "exit" # This isn't necessary, exit will always exit the program
 
