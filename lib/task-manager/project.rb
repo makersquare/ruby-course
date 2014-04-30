9f4f3d98ec7e120fd42b22c3a4fcea03f4ae2ef3
Zachary Spector <zjspector@gmail.com>
2014-04-30 11:14:23 -0500

    Create tests and implement code for Project initialize method

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,16 @@
 
 class TM::Project
+  attr_reader :name, :id
+  @@project_id = 0
+
+  def initialize(name)
+    @name = name
+    @@project_id += 1
+    @id = @@project_id
+    @time = Time.now
+  end
+
+  def self.reset_class_variables
+    @@project_id = 0
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,7 +1,23 @@
 require 'spec_helper'
 
 describe 'Project' do
+  before(:each) do
+    @p1 = TM::Project.new("My First Project")
+    @p2 = TM::Project.new("My Second Project")
+    TM::Project.reset_class_variables
+  end
   it "exists" do
     expect(TM::Project).to be_a(Class)
+  end
+
+  describe ".initialize" do
+    it "creates a new project with name" do
+      expect(@p1.name).to eq("My First Project")
+    end
+
+    it "automatically assigns a project ID to new project" do
+      expect(@p1.id).to eq(1)
+      expect(@p2.id).to eq(2)
+    end
   end
 end
