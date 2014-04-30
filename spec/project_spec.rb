32b7122c26cf58e2088cd51e36a1c808448e3458
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-04-30 09:13:42 -0500

    Separated name and unique id tests in rspec.

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -7,7 +7,14 @@ describe 'Project' do
 
   it "allows a new project to be created with a name" do
       shopping = TM::Project.new("Shopping")
+      expect(shopping).to be_a(TM::Project)
       expect(shopping.name).to eq("Shopping")
-      expect(shopping.pid).to eq(1)
+  end
+
+  it "initializes with a unique id" do
+  	p1 = TM::Project.new("Project 1")
+  	p2 = TM::Project.new("Project 2")
+  	expect(p1.pid).to eq(2)
+  	expect(p2.pid).to eq(3)
   end
 end
