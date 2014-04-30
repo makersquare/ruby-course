4bc79b5cab4572def055522990ab1ac29089b32b
Benjamin Poon <benjamintpoon@gmail.com>
2014-04-30 10:45:47 -0500

    terminal_spec.rb wasn't staged in last commit

---
+++ spec/terminal_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'Terminal' do
+  before(:each) do
+  end
+
+  describe '.initialize' do
+  end
+
+  describe '.get_input' do
+
+  end
+
+  describe '.parse_command' do
+  end
+
+  describe 'execute_command' do
+  end
+end
