ec6911f28585ea6999c4c52b4f7ccb2b3367c199
Zachary Spector <zjspector@gmail.com>
2014-04-30 11:15:51 -0500

    Write test and implement code for task initialize method

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -1,3 +1,20 @@
 
 class TM::Task
+  attr_reader :description, :priority, :tid, :completed
+
+  @@task_id = 0
+  def initialize(description, priority)
+    @description = description
+    @priority = priority
+    @@task_id += 1
+    @tid = @@task_id
+    @completed = false
+    @time = Time.now
+  end
+
+
+
+  def self.reset_class_variables
+    @@task_id = 0
+  end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -4,4 +4,39 @@ describe 'Task' do
   it "exists" do
     expect(TM::Task).to be_a(Class)
   end
+
+  before(:each) do
+    @t1 = TM::Task.new("create a new task", 1)
+    @t2 = TM::Task.new("eat lunch", 2)
+    TM::Task.reset_class_variables
+  end
+
+  describe '.initialize' do
+    it "sets a task ID number for new tasks" do
+      expect(@t1.tid).to eq(1)
+      expect(@t2.tid).to eq(2)
+    end
+
+    it "sets a description for new tasks" do
+      expect(@t1.description).to eq("create a new task")
+    end
+
+    it "sets a priority number for new tasks" do
+      expect(@t1.priority).to eq(1)
+    end
+
+    it "initializes with task set as incomplete" do
+      expect(@t1.completed).to eq(false)
+    end
+  end
+
+  describe '.set_complete' do
+    before(:each) do
+      @t1.set_complete
+    end
+    it "marks a task as completed" do
+      expect(@t1.completed).to eq(true)
+      expect(@t2.completed).to eq(false)
+    end
+  end
 end
