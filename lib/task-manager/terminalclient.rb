acc1f89cdc13b96ee555d218e5c54e7ed6f86857
David Colby <davidlcolby@gmail.com>
2014-05-01 09:01:16 -0500

    Take user input from console on one line

--- lib/task-manager/terminalclient.rb
+++ lib/task-manager/terminalclient.rb
@@ -24,7 +24,7 @@ class TMTerminal
       puts "\tcreate NAME - Create a new project with name=NAME"
       puts "\tshow PID - Show remaining tasks for project with id=PID"
       puts "\thistory PID - Show completed tasks for project with id=PID"
-      puts "\tadd PID PRIORITY DESC - Add a new task to project with id=PID"
+      puts "\tadd PID DESC PRIORITY - Add a new task to project with id=PID"
       puts "\tmark TID - Mark task with id=TID as complete"
       puts "\texit with exit"
       print "Enter a command: "
@@ -34,40 +34,49 @@ class TMTerminal
         puts"#{project.pid}\t\t#{project.name}"
       end
       print "Enter a command: "
-    when "create"
-      print "Enter the project's name: "
-      name = gets.chomp()
+    when /create ./
+      choice = choice.split(" ",2)
+      name = choice[1]
+
       new_project = TM::Project.new(name)
+      puts "Project created."
       print "Enter a command: "
-    when "show"
-      project = TM::Project.get_project(1)
+    when /show ./
+      choice = choice.split(" ")
+      pid = choice[1].to_i
+      project = TM::Project.get_project(pid)
+
       puts "Priority\tID\tDescription"
       project.incomplete_tasks.each do |task|
-        puts "#{task.priority}\t#{task.task_id}\t#{task.description}"
+        puts "#{task.priority}\t\t#{task.task_id}\t#{task.description}"
       end
       print "Enter a command: "
-    when "history"
-      print "Enter the project id: "
-      id = gets.chomp().to_i
-      project = TM::Project.get_project(id)
+    when /history ./
+      choice = choice.split(" ")
+      pid = choice[1].to_i
+
+      project = TM::Project.get_project(pid)
+      puts "Completed Tasks"
+      puts "Task ID\tTask Description"
       project.completed_tasks.each do |task|
         puts "#{task.task_id} | #{task.description}"
       end
       print "Enter a command: "
-    when "add"
-      print "Enter the project id: "
-      project = gets.chomp().to_i
-      print "Enter the task description: "
-      description = gets.chomp()
-      print "Enter the task priority: "
-      priority = gets.chomp().to_i
+    when /add ./
+      choice = choice.split(" ",4)
+      pid = choice[1].to_i
+      priority = choice[2].to_i
+      description = choice[3]
 
-      TM::Task.new(project,description,priority)
+      TM::Task.new(pid,description,priority)
+      puts "Task created"
       print "Enter a command: "
-    when "mark"
-      print "Enter the task id: "
-      id = gets.chomp().to_i
+    when /mark ./
+      choice = choice.split(" ")
+      id = choice[1].to_i
+
       TM::Task.complete_task(id)
+      puts "Task completed"
       print "Enter a command: "
     end
   end while choice != "exit" # This isn't necessary, exit will always exit the program
