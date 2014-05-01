c6ae2db8f2a14e98dbf2cff9a595f72244651afc
Christian Catalan <crcatala@gmail.com>
2014-05-01 11:17:56 -0500

    added due_date to Task class, and refactored print formatting method so multiple lines in code

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -1,5 +1,5 @@
 class TM::Task
-	attr_reader :date_created, :date_completed, :completed
+	attr_reader :date_created, :date_completed, :completed, :date_due
 	attr_accessor :id, :name, :priority, :description
 
 	@@task_count = 0
@@ -13,6 +13,7 @@ class TM::Task
 		@priority = priority
 		@date_created= Time.now
 		@date_completed = nil
+		@date_due = Time.now + (7*24*60*60) #1 week from now
 		@completed = false
 	end
 
@@ -26,13 +27,34 @@ class TM::Task
 		@date_completed = nil
 	end
 
+	def overdue?
+		Time.now > @date_due
+	end
 
 	def self.print_header
-		printf "%-10s %-4s %-12s %-15s %-21s %-12s %-15s\n", "[Priority]", "[ID]", "[Name]", "[Description]", "[Date Created]", "[Completed?]", "[Date Completed]"
+		printf "%-10s %-4s %-12s %-15s %-21s %-15s %-21s %-15s %-21s\n",
+		"[Priority]",
+		"[ID]",
+		"[Name]",
+		"[Description]",
+		"[Date Created]",
+		"[Completed?]",
+		"[Date Completed]",
+		"[Overdue?]",
+		"[Date Due]" 
 	end
 
 	def print_details
-		printf "%-10s %-4s %-12s %-15s %-21s %-12s %-15s\n", "#{@priority}", "#{@id}", "#{@name}", "#{@description}", "#{@date_created.strftime '%m/%d/%Y %I:%M%p'}", "#{@completed ? 'yes' : 'no'}", "#{@date_completed == nil ? "" : (@date_completed.strftime '%m/%d/%Y %I:%M%p')}"
+		printf "%-10s %-4s %-12s %-15s %-21s %-15s %-21s %-15s %-21s\n",
+		"#{@priority}",
+		"#{@id}",
+		"#{@name}",
+		"#{@description}",
+		"#{@date_created.strftime '%m/%d/%Y %I:%M%p'}",
+		"#{@completed ? 'yes' : 'no'}",
+		"#{@date_completed == nil ? "" : (@date_completed.strftime '%m/%d/%Y %I:%M%p')}",
+		"#{overdue? ? 'yes' : 'no'}",
+		"#{@date_due.strftime '%m/%d/%Y %I:%M%p'}"
 	end	
 
 	def print_details_old
