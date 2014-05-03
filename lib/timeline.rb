ff7ee829419dc3242d58ae92f02d2fc2e37fdffb
Brian Will <brian.thomas.will@gmail.com>
2014-05-03 02:49:36 -0700

    expand into rails app

--- lib/timeline.rb
+++ lib/timeline.rb
@@ -3,7 +3,7 @@ require 'active_model'
 module Timeline
 end
 
-require 'timeline/entity.rb'
+require_relative 'timeline/entity.rb'
 
 Gem.find_files("timeline/database/*.rb").each { |path| require path }
 Gem.find_files("timeline/entities/*.rb").each { |path| require path }
