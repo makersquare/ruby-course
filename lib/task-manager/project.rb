734299db55216616a5dee9401aa7e527365dca88
Amy Bakameyer <bymak87@gmail.com>
2014-05-04 19:50:47 -0500

    terminal option added

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,21 +1,55 @@
 
 class TM::Project
 
-  @@id_count = 0
   attr_reader :name, :id
+  attr_accessor :tasks
+
+  @@id_count = 0
+  @@projects = []
 
   def initialize(name)
-
 
     @name = name
     @@id_count += 1
     @id = @@id_count
+    @tasks = []
+    @@projects << self
 
+  end
+
+  def add_task(task)
+    @tasks << task
+  end
+
+  def self.projects
+    @@projects
+  end
+
+  def self.projects=(var)
+    @@projects = arg
+  end
+
+  def self.find_pid(pid)
+    @@projects.select {|p| p.id == pid}.first
   end
 
   def self.reset_class_variables
     @@id_count = 0
+    @@projects =[]
   end
 
+  def completed_task_list
+    t = @tasks.select {|task| task.complete}
+    t.sort_by {|task| task.creation_date}.reverse
+  end
+
+  def incompleted_task_list
+    t = @tasks.select {|task| !task.complete?}
+    t.sort_by {|task| [task.creation_date, task.priority_number]}
+  end
+
+  def view
+    "#{@id}: #{@name}"
+  end
 
 end
