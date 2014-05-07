616312b3e75f206ebc09f432334358effe7babbd
Ashley McKemie <a.mckemie@utexas.edu>
2014-05-07 06:41:14 -0500

    Created list_all method in project manager

--- spec/projectmanager_spec.rb
+++ spec/projectmanager_spec.rb
@@ -15,49 +15,54 @@ describe 'ProjectManager' do
     end
   end
 
-  describe 'add_project' do
-    it "adds a project to the @projects array" do
-      @pm.add_project(@project1)
-      expect(@pm.projects.first).to eq(@project1)
-    end
-  end
-
-  describe 'get_project' do
-    it "returns the project that matches the given id" do
+  describe 'methods' do
+    before (:each) do
       @pm.add_project(@project1)
       @pm.add_project(@project2)
-      expect(@pm.get_project(2)).to eq(@project2)
     end
 
-    it "returns nil if given a project id that doesn't match any project" do
-      expect(@pm.get_project(5)).to eq(nil)
+    describe 'add_project' do
+      it "adds a project to the @projects array" do
+        expect(@pm.projects.first).to eq(@project1)
+      end
+    end
+
+    describe 'get_project' do
+      it "returns the project that matches the given id" do
+        expect(@pm.get_project(2)).to eq(@project2)
+      end
+
+      it "returns nil if given a project id that doesn't match any project" do
+        expect(@pm.get_project(5)).to eq(nil)
+      end
+    end
+
+    describe 'list all' do
+      it "lists all projects" do
+          task = TM::Task.new("Create gradebook", @project1.id, 1)
+          task2 = TM::Task.new("Add students", @project1.id, 2)
+          task3 = TM::Task.new("Create gradebook", @project2.id, 1)
+          task4 = TM::Task.new("Add students", @project2.id, 2)
+
+          @project1.add_task(task)
+          @project1.add_task(task2)
+          @project2.add_task(task3)
+          @project2.add_task(task4)
+
+          task.due_date = "1 Feb 2014"
+          task2.due_date = "1 June 2012"
+          task3.due_date = "3 Feb 2015"
+          task4.due_date = "1 May 2020"
+
+          TM::Task.mark_complete(1)
+          STDOUT.should_receive(:puts).with("Name: Grades - ID: 1")
+          STDOUT.should_receive(:puts).with("Percentage Finished - 50.0%")
+          STDOUT.should_receive(:puts).with("Percentage Tasks Overdue - 50.0%")
+          STDOUT.should_receive(:puts).with("Name: Tests - ID: 2")
+          STDOUT.should_receive(:puts).with("Percentage Finished - 0%")
+          STDOUT.should_receive(:puts).with("Percentage Tasks Overdue - 0%")
+          @pm.list_all
+        end
     end
   end
-
-  # describe 'list all' do
-  #   it "lists all projects" do
-  #       task = TM::Task.new("Create gradebook", @project1.id, 1)
-  #       task2 = TM::Task.new("Add students", @project1.id, 2)
-  #       task3 = TM::Task.new("Create gradebook", @project2.id, 1)
-  #       task4 = TM::Task.new("Add students", @project2.id, 2)
-
-  #       @project1.add_task(task)
-  #       @project1.add_task(task2)
-  #       @project2.add_task(task3)
-  #       @project2.add_task(task4)
-
-  #       task.due_date = "1 Feb 2014"
-  #       task2.due_date = "1 June 2012"
-  #       task3.due_date = "3 Feb 2015"
-  #       task4.due_date = "1 May 2020"
-
-  #       TM::Task.mark_complete(1)
-  #       STDOUT.should_receive(:puts).with("Name: Grades - ID: 1")
-  #       STDOUT.should_receive(:puts).with("Percentage Finished - 50.0%")
-  #       STDOUT.should_receive(:puts).with("Percentage Tasks Overdue - 50.0%")
-  #       STDOUT.should_receive(:puts).with("Name: Tests - ID: 2")
-  #       STDOUT.should_receive(:puts).with("Percentage Finished - 0%")
-  #       STDOUT.should_receive(:puts).with("Percentage Tasks Overdue - 0%")
-  #       @pm
-  # end
 end
