6b98e88814c82dd78ad0655b9a6f623f5041800a
Canaan Davis <canaandavis@canaans-mbp.att.net>
2014-05-09 16:26:55 -0500

    update with create_project id

--- lib/task-manager.rb
+++ lib/task-manager.rb
@@ -2,6 +2,26 @@
 # Create our module. This is so other files can start using it immediately
 module TM
   class Database
+
+    attr_reader :projects, :task, :projects_counter, :task_counter
+
+    def initialize
+      @projects = {}
+      @task = {}
+      @projects_counter = 0
+      @task_counter = 0
+    end
+
+    def create_project(data)
+      data[:id] = @projects_counter
+      @projects[@projects_counter] = data
+      @projects_counter += 1
+    end
+
+  end
+
+  def self.db
+    @__db_instance ||= Database.new
   end
 end
 
--- spec/task-manager_spec.rb
+++ spec/task-manager_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 require 'pry'
 
-describe "Datebase" do
+describe "TaskManager::Database" do
+
   before(:each) do
-    @db = TM::Database.new
+    @database = TM::Database.new
+    @db = TM.db
   end
 
   it 'exist' do
@@ -11,9 +13,37 @@ describe "Datebase" do
   end
 
   context 'it is initialized' do
+
     it 'creates an empty projects hash' do
+
       expect(@db.projects).to eq({})
     end
+
+    it 'creates an empty task hash' do
+      expect(@db.task).to eq({})
+    end
+
+    it 'has a projects counter' do
+      expect(@db.projects_counter).to eq(0)
+    end
+
+    it 'has a task counter' do
+      expect(@db.task_counter).to eq(0)
+    end
+
+  end
+
+  context 'it can add a new project to the datebase' do
+
+    it 'can add new projects' do
+      @db.create_project({:name => "Pizza Party Planning"})
+      expect(@db.projects).to eq({0 => {:name => "Pizza Party Planning", :id => 0}})
+      expect(@db.projects_counter).to eq(1)
+    end
+
+  end
+
+  context 'it can return a new project from the database' do
   end
 
 end
