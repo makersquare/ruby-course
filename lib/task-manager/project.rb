8f4b549c0c65f26199cce5847fe7d6825e44a933
Shehzan Devani <shehzandevani@gmail.com>
2014-05-09 13:41:17 -0500

    Ripped out gut of project to show students

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -6,12 +6,4 @@ class TM::Project
     @name = name
     @id = id
   end
-
-  def complete_tasks
-    TM::Task.retrieve_tasks(@id, true)
-  end
-
-  def incomplete_tasks
-    TM::Task.retrieve_tasks(@id, false)
-  end
 end
--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -10,29 +10,4 @@ class TM::Task
     @complete = complete
     @timestamp = timestamp
   end
-
-  def mark_complete
-    @complete = true
-    # db.update_task(self.id, :complete => true)
-  end
-
-  # def self.retrieve_tasks(p_id, complete)
-  #   res = @@all_tasks.values.select do |task|
-  #     task.complete == complete &&
-  #       task.project_id == p_id
-  #   end
-  #   res.sort do |p1, p2|
-  #     if p1.priority < p2.priority
-  #       x = -1
-  #     elsif p2.priority < p1.priority
-  #       x =  1
-  #     else
-  #       if p1.timestamp < p2.timestamp
-  #         x = -1
-  #       else
-  #         x = 1
-  #       end
-  #     end
-  #   end
-  # end
 end
