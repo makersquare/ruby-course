308c814ea426e3be7c0be26541442f2d6d4f1dd5
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-05-11 22:56:44 -0500

    Added empty tests for database file.

---
+++ spec/database_spec.rb
@@ -0,0 +1,67 @@
+require 'spec_helper'
+
+describe 'Database' do
+
+  describe '#build_project' do
+    it 'should be able to return an instance of the TM::Task class' do
+    end
+  end
+
+  describe '#create_project' do
+    it 'should create a project in a project hash' do
+    end
+
+    it 'should make an instance of the TM::Project class' do
+    end
+  end
+
+  describe '#get_project' do
+    it 'should return an instance of the TM::Project class with to project id' do
+    end
+  end
+
+  describe '#update_project' do
+    it 'should update the projects name in the projects hash given the id of the project' do
+    end
+
+    it 'should return an instance of the TM::Project class with the updated data' do
+    end
+  end
+
+  describe '#destroy_project' do
+    it 'should delete a project from the projects has given the id of the project' do
+    end
+  end
+
+  describe '#build_task' do
+    it 'should be able to return an instance of the TM::Task class' do
+    end
+  end
+
+  describe '#create_task' do
+    it 'should make a new task in the tasks hash' do
+    end
+
+    it 'should return an instance of the TM::Task class' do
+    end
+  end
+
+  describe '#get_task' do
+    it 'should return an instance of TM::Task class given the task id' do
+    end
+  end
+
+  describe '#update_task' do
+    it 'should update the given data in the correct task in the task hash' do
+    end
+
+    it 'should return an instance of the TM::Task class with the updated data' do
+    end
+  end
+
+  describe '#destroy_task' do
+    it 'should delete a task from the task hash' do
+    end
+  end
+
+end
