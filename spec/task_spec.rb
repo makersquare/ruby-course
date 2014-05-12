3bd252b94321704ed54a4325e8b99804a99329c1
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-05-12 14:01:15 -0500

    Edited tests for task class to pass.

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -3,23 +3,30 @@ require 'pry-debugger'
 
 describe 'Task' do
 
-  before(:each) do
-    TM::Task.reset_class_variables
-    @p1 = TM::Project.new("Project 1")
-    @t1 = TM::Task.new(1, 'Task 1', 1, '2014 1 31')
-    @t2 = TM::Task.new(1, 'Task 2', 2, '2014 5 6')
-  end
+  let(:t1) { TM::Task.new(1, 1, 'Task 1', 1, '2014 1 1', '2014 5 12', false)}
+  let(:t2) { TM::Task.new(1, 2, 'Task 2', 3, '2014 6 6', '2014 5 12', false)}
 
-  xit "exists" do
+  it "exists" do
     expect(TM::Task).to be_a(Class)
   end
 
   describe '#initialize' do
-    xit "A new task can be created with a project id, description, priority number, and due date(y m d)." do
-      expect(@t1.tid).to eq(1)
-      expect(@t1.description).to eq("Task 1")
-      expect(@t1.pnum).to eq(1)
-      expect(@t1.duedate).to eq('2014 1 31')
+    it "A new task can be created with a project id, description, priority number, and due date(y m d)." do
+      expect(t1.pid).to eq(1)
+      expect(t1.tid).to eq(1)
+      expect(t1.desc).to eq("Task 1")
+      expect(t1.pnum).to eq(1)
+      expect(t1.duedate).to eq('2014 1 1')
+      expect(t1.date).to eq('2014 5 12')
+      expect(t1.complete).to eq(false)
+
+      expect(t2.pid).to eq(1)
+      expect(t2.tid).to eq(2)
+      expect(t2.desc).to eq("Task 2")
+      expect(t2.pnum).to eq(3)
+      expect(t2.duedate).to eq('2014 6 6')
+      expect(t2.date).to eq('2014 5 12')
+      expect(t2.complete).to eq(false)
     end
   end
 
