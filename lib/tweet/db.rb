4e9d8faeb4ca276d7100bb9bb9e2887e345da04a
Taylor Smith <twsmith89@gmail.com>
2014-05-14 15:44:04 -0500

    solve dem tests cash dem checks

--- lib/tweet/db.rb
+++ lib/tweet/db.rb
@@ -1,5 +1,5 @@
 class Tweet::DB
-  attr_reader :text_tweets, :tags, :text_tweet_tags
+  attr_reader :text_tweets, :tags, :text_tweet_tags, :pic_tweets, :pic_tweet_tags
 
   def initialize
     @text_tweets = {}
@@ -8,6 +8,10 @@ class Tweet::DB
     @next_tag_id = 0
     @text_tweet_tags = {}
     @text_tweet_tags_counter = 0
+    @pic_tweets = {}
+    @pic_tweet_counter = 0
+    @pic_tweet_tags = {}
+    @pic_tweet_tags_counter = 0
   end
 
   def create_text_tweet(data)
@@ -28,9 +32,7 @@ class Tweet::DB
   end
 
   def get_or_create_tag(data)
-    # binding.pry
     existing_tag = tags.select {|id, tag| tag[:tag] == data[:tag]}.first
-    # binding.pry
     if existing_tag
       data = existing_tag.last
     else
@@ -59,7 +61,44 @@ class Tweet::DB
     @text_tweet_tags_counter += 1
     return id
   end
+
+  # Methods related to PicTweets
+  def create_pic_tweet(data)
+    data[:id] = @pic_tweet_counter
+    @pic_tweet_counter += 1
+
+    data[:tags].each do |tag|
+      tag_obj = get_or_create_tag({tag: tag})
+      create_pic_tweet_tag(data[:id], tag_obj.id)
+    end
+
+    PicTweet.new(data[:content], data[:pic_url], data[:tags], data[:id])
+  end
+
+  def create_pic_tweet_tag(pt_id, tag_id)
+    id = @pic_tweet_tags_counter
+    data = {pt_id: pt_id, tag_id: tag_id, id: id}
+    @pic_tweet_tags[id] = data
+    @pic_tweet_tags_counter += 1
+    return id
+  end
+
+  # Input: a tag entity
+  # Output: an array of PicTweets with the given tag
+  def get_pic_tweets_from_tag(tag)
+    new_array = @pic_tweet_tags.values.select { |tag_hash| tag_hash[:tag_id] == tag.id }
+    new_array.map { |data| Tweet.db.build_pic_tweet(data) }
+  end
+
+  # Build a PicTweet out of a data hash
+  # Input: A hash
+  # Output: A PicTweet object
+  def build_pic_tweet(data)
+    PicTweet.new(data[:content], data[:pic_url], data[:tags], data[:id])
+  end
 end
+
+
 
 module Tweet
   def self.db
