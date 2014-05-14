7b232509dcb720d68c9a46f9c44cc1441a8e2e1e
Taylor Smith <twsmith89@gmail.com>
2014-05-14 15:43:50 -0500

    testing pictweets

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -109,4 +109,100 @@ describe "db" do
       end
     end
   end
+
+  describe 'pic_tweets in db' do
+    let(:pt) do
+      data = {
+        content: "My pic",
+        pic_url: "my_pic.jpg",
+        tags: ["my", "pic"]
+      }
+      Tweet.db.create_pic_tweet(data)
+    end
+
+    let(:pt2) do
+      data = {
+        content: "My pic2",
+        pic_url: "my_pic2.jpg",
+        tags: ["my", "pic"]
+      }
+      Tweet.db.create_pic_tweet(data)
+    end
+
+    let(:pt3) do
+      data = {
+        content: "My pic3",
+        pic_url: "my_pic3.jpg",
+        tags: ["my"]
+      }
+      Tweet.db.create_pic_tweet(data)
+    end
+
+    let(:tag1) { Tweet.db.get_or_create_tag(tag:"my") }
+    let(:tag2) { Tweet.db.get_or_create_tag(tag:"pic") }
+
+    describe ".create_pic_tweet" do
+      it "returns a PicTweet" do
+        expect(pt).to be_a(PicTweet)
+        expect(pt.content).to eq("My pic")
+        expect(pt.pic_url).to eq("my_pic.jpg")
+        expect(pt.id).to be_a(Fixnum)
+        # TODO: test pt.tags
+      end
+
+      it "also calls get_or_create_tag" do
+        # Tweet.db.get_or_create_tag({tag: "tag1"})
+        expect(Tweet.db).to receive(:get_or_create_tag).with({tag: "my"}).
+        and_call_original
+        expect(Tweet.db).to receive(:get_or_create_tag).with({tag: "pic"}).
+        and_call_original
+        # We have to call tt after the tests so that the expectations 
+        # are fulfilled. Rspec remembers the expectations and then checks
+        # them when tt is run.
+        # If we call it before the tests, rspec forgets that it did stuff.
+        
+        pt
+      end
+
+      it "creates a relationship between pic_tweet and tag" do
+        pt
+
+        # TODO: We need some sort of way to grab the text_tweet_tag ID instead
+        # of hard-coding it in as 0 and x1
+        expect(Tweet::db.pic_tweet_tags.values).to include(
+          {pt_id:pt.id, tag_id:tag1.id, id:0})
+        expect(Tweet::db.pic_tweet_tags.values).to include(
+          {pt_id:pt.id, tag_id:tag2.id, id:1})
+      end
+    end
+
+    describe ".get_pic_tweets_from_tag" do
+      it "returns array of PicTweets linked to the tag" do
+        # Calling all our let block stuff
+        pt
+        pt2
+        pt3
+
+        tag = Tweet.db.get_or_create_tag({tag:"pic"})
+        result = Tweet.db.get_pic_tweets_from_tag(tag)
+        expect(result).to be_a(Array)
+        expect(result.length).to eq(2)
+        expect(result.first).to be_a(PicTweet)
+      end
+    end
+    
+    describe ".build_pic_tweet" do
+      let(:data) do 
+        {
+          content: "My Pic",
+          tags: ["my", "pic"],
+          pic_url: "my_pic.jpg"
+        }
+      end
+      it "returns a PicTweet" do
+        result = Tweet.db.build_pic_tweet(data)
+        expect(result).to be_a(PicTweet)
+      end
+    end
+  end
 end
