4927a9c469cca2398702bc953d52edaf3489cad1
Gilbert <gilbertbgarza@gmail.com>
2014-05-28 11:46:15 -0500

    Project setup and first tests

---
+++ README.md
@@ -0,0 +1,15 @@
+# Double Dog PoS
+
+A Point of Sales system built for Double Dog.
+
+----
+## User Stories
+
+- As a manager, I want to sign into Double Dog PoS Admin system
+  - This only requires a simple password (no account for an admin)
+- As a manager, I want to create items for order
+- As a manager, I want to see all orders
+- As a manager, I want to see all orders created by a given employee.
+- As an employee, I want to sign into Double Dog PoS
+- As an employee, I want to create orders
+  - Each order will have at least one item
---
+++ spec/database/in_memory_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe DoubleDog::Database::InMemory do
+  let(:db) { described_class.new }
+
+  it "creates an item" do
+    item = db.create_item(:name => 'hot dog', :price => 5)
+    expect(item.id).to_not be_nil
+    expect(item.name).to eq 'hot dog'
+    expect(item.price).to eq 5
+  end
+
+  it "retrieves an item" do
+    item = db.create_item(:name => 'hot dog', :price => 5)
+    retrieved_item = db.get_item(item.id)
+    expect(retrieved_item.name).to eq 'hot dog'
+    expect(retrieved_item.price).to eq 5
+  end
+
+  it "grabs all items" do
+    db.create_item(:name => 'fries', :price => 3)
+    db.create_item(:name => 'pickle', :price => 4)
+    db.create_item(:name => 'potato', :price => 8)
+
+    items = db.all_items
+    expect(items.count).to eq 3
+    expect(items.map &:name).to include('fries', 'pickle', 'potato')
+    expect(items.map &:price).to include(3, 4, 8)
+  end
+end
