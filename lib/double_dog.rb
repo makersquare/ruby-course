8494341926d7648c9e8a03455b1518bc4a85e704
Gilbert <gilbertbgarza@gmail.com>
2014-05-29 22:49:30 -0500

    Add database singleton getter

--- lib/double_dog.rb
+++ lib/double_dog.rb
@@ -1,8 +1,13 @@
 
 module DoubleDog
+  def self.db
+    @__db_instance ||= Database::InMemory.new
+  end
 end
 
 require_relative 'double_dog/entities/item.rb'
 require_relative 'double_dog/entities/user.rb'
 
 require_relative 'double_dog/database/in_memory.rb'
+require_relative 'double_dog/use_cases/create_item.rb'
+require_relative 'double_dog/use_cases/sign_in.rb'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,12 @@ require 'double_dog'
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+  # Reset database singleton before every test
+  config.before do
+    DoubleDog.instance_variable_set(:@__db_instance, nil)
+  end
+
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
