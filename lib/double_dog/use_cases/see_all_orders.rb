965daded48c92eebfabd58e5c2cefbf634fe8b48
Gene Le <gene.gkl@gmail.com>
2014-05-30 20:35:27 -0500

    SeeAllOrders use case

---
+++ lib/double_dog/use_cases/see_all_orders.rb
@@ -0,0 +1,25 @@
+module DoubleDog
+  class SeeAllOrders
+    def run(params)
+      return failure(:not_admin) unless admin_session?(params[:admin_session])
+
+      orders = DoubleDog.db.all_orders
+      return success(orders: orders)
+    end
+
+    def admin_session?(session_id)
+      user = DoubleDog.db.get_user_by_session_id(session_id)
+      user && user.admin?
+    end
+
+  private
+
+    def failure(error_name)
+      return :success? => false, :error => error_name
+    end
+
+    def success(data)
+      return data.merge(:success? => true)
+    end
+  end
+end
---
+++ spec/use_cases/see_all_orders_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe DoubleDog::SeeAllOrders do
+
+  describe "Validation" do
+    it "requires the user to be an admin" do
+      use_case = DoubleDog::SeeAllOrders.new
+      expect(use_case).to receive(:admin_session?).and_return(false)
+
+      result = use_case.run(admin_session: 'stubbed')
+
+      expect(result[:success?]).to eq(false)
+      expect(result[:error]).to eq(:not_admin)
+    end
+  end
+
+  it "returns all orders" do
+    item_1 = DoubleDog.db.create_item(name: 'hot dog', price: 5)
+    item_2 = DoubleDog.db.create_item(name: 'fries', price: 3)
+    order_1 = DoubleDog.db.create_order(session_id: 'stubbed', items: [item_1, item_2])
+    order_2 = DoubleDog.db.create_order(session_id: 'stubbed', items: [item_2])
+
+    use_case = DoubleDog::SeeAllOrders.new
+    expect(use_case).to receive(:admin_session?).and_return(true)
+
+    result = use_case.run(admin_session: 'stubbed')
+
+    expect(result[:success?]).to eq(true)
+
+    orders = result[:orders]
+
+    expect(orders.count).to be >= 2
+    item_names = orders.map { |order| order.items.map &:name }.flatten
+    expect(item_names).to include('hot dog', 'fries')
+    expect(item_names.count).to eq(3)
+  end
+end
