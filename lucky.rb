7e429b51b480adb75b3ea4600307553ad78caf0c
Nick McDonnough <nickmcd@gmail.com>
2014-06-06 09:36:44 -0500

    Create lucky.rb

---
+++ lucky.rb
@@ -0,0 +1,20 @@
+# In some countries of former Soviet Union there was
+# a belief about lucky tickets. A transport ticket
+# of any sort was believed to posess luck if sum of
+# digits on the left half of its number was equal to
+# the sum of digits on the right half. Here are
+# examples of such numbers:
+#
+# 003111    #         3 = 1 + 1 + 1
+# 813372    # 8 + 1 + 3 = 3 + 7 + 2
+# 17935     #     1 + 7 = 3 + 5
+# 56328116  # ???
+#
+# Such tickets were either eaten after being used
+# or collected for bragging rights.
+#
+# Read the tests carefully and ake them pass.
+#
+# Credit to SundaySalsa on codewars.com for coming
+# up with this problem.
+
