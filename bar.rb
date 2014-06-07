2af3e86ebbb19494ae9492e4d17c56c7e3849e15
Andy Schetter <andyschetter@gmail.com>
2014-06-07 14:59:48 -0500

    Add happy hour setter method and passed test

--- bar.rb
+++ bar.rb
@@ -1,16 +1,21 @@
 require 'time' # you're gonna need it
 
 class Bar
-  attr_reader :name, :menu_items
+  attr_reader :name, :menu_items, :happy_discount
 
   def initialize(name)
     @name = name
     @menu_items = []
+    @happy_discount = 0
   end
 
   def add_menu_item(name, price)
     item = Menu_item.new(name, price)
     @menu_items << item
+  end
+
+  def happy_discount=(discount)
+    @happy_discount = discount
   end
 end
 
--- spec/bar_spec.rb
+++ spec/bar_spec.rb
@@ -37,11 +37,11 @@ describe Bar do
     expect(item.price).to eq 9.95
   end
 
-  xit "has a default happy hour discount of zero" do
+  it "has a default happy hour discount of zero" do
     expect(@bar.happy_discount).to eq 0
   end
 
-  xit "can set its happy hour discount" do
+  it "can set its happy hour discount" do
     expect { @bar.happy_discount = 0.5 }.to_not raise_error
   end
 
