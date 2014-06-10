d47ece1861bcad472db0f0988c4292a5ecb1eb1b
Jered McCullough <mathias.armstrong@gmail.com>
2014-06-09 17:44:19 -0700

    I wrote a test that confirmed that when a project is created it has a name, given by an input and a id number.  the ID number is the total number of Projects that had bee started

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,10 @@
 
 class TM::Project
+  attr_reader :name, :id
+  def initialize(name)
+  @@ids={}
+  @name=name
+  @id=@@ids.count
+  @@ids[@id]=self
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 
 describe 'Project' do
+  let(:test){TM::Project.new('first')}
   it "exists" do
     expect(TM::Project).to be_a(Class)
+  end
+  context 'project is started with a name' do
+    it 'has a name' do
+      expect(test.name).to eq('first')
+    end
+  end
+  context 'starting a new project' do
+    it 'has an id number' do
+      expect(test.id).to be_a(Fixnum)
+    end
   end
 end
