a062a51837be429fc75260638ca393686eae0393
Jonathan Katz <jonathan.a.katz@gmail.com>
2014-06-09 22:32:29 -0700

    added initialize method for Project class and passed tests

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,17 @@
 
 class TM::Project
+  attr_reader :id, :name, :task_list
+
+  @@project_list = []
+
+  def initialize(name)
+    @name = name
+    @@project_list << self
+    @id = @@project_list.count
+    @task_list = []
+  end
+
+  def self.project_list
+    @@project_list
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -5,18 +5,19 @@ describe 'Project' do
     expect(TM::Project).to be_a(Class)
   end
 
-  it "stores all project instances in an array" do
-    expect(Project.project_list).to be_an_instance_of(Array)
+  it "adds all project instances to an array" do
+    expect(TM::Project.project_list).to be_an_instance_of(Array)
   end
 
   it "is initialized with a name, a unique ID, a default task list of 0, and adds itself to the project list array" do
-    p = Project.new(name)
-    expect(p.name).to eq(name)
-    expect(p.id).to eq(Project.project_list.size)
-    expect(p.task_list).to eq(0)
+    p = TM::Project.new("Odin")
+    expect(p.name).to eq("Odin")
+    expect(p.id).to eq(TM::Project.project_list.size)
+    expect(p.task_list.size).to eq(0)
 
-    p2 = Project.new(name2)
-    expect(Project.project_list.size).to eq(2)
+    p2 = TM::Project.new("Euler")
+    expect(TM::Project.project_list.size).to eq(2)
     expect(p2.id).to eq(2)
+    expect(p2.task_list.size).to eq(0)
   end
 end
