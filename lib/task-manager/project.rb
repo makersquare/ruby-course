344bc3fdaf806c4f880606883733f7002a15fd1b
Paul Dombkowski <pdombkowski@gmail.com>
2014-06-10 15:00:06 -0500

    Initial commit of project class and tests

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,32 @@
+#require 'task'
+require 'pry-debugger'
 
 class TM::Project
+  attr_reader :name, :id
+
+  @@counter = 0
+  @@project_list = []
+
+  def initialize(name)
+    @name = name
+    @@counter += 1
+    @id = @@counter
+    @@project_list << self
+  end
+
+  def add_task(desc, priority)
+    TM::Task.new(@id,desc,priority)
+  end
+
+  def get_complete_tasks
+    project_tasks = TM::Task.task_list.values.select { |t| t.project == @id && t.complete == true }
+    project_tasks.sort_by { |t| t.created }
+  end
+
+  def get_incomplete_tasks
+    project_tasks = TM::Task.task_list.values.select { |t| t.project == @id && t.complete == false }
+    project_tasks.sort_by { |t| [(t.priority * -1), t.created] }
+
+  end
+
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,7 +1,51 @@
 require 'spec_helper'
+require 'pry-debugger'
 
 describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
+  before(:all) do
+    @project = TM::Project.new("To-Do List") 
+  end
+
+  describe '.initialize' do
+    it "must have a name" do 
+      expect(@project.name).to eq("To-Do List")
+    end
+
+    it "has a project id" do
+      expect(@project.id).to eq(1)
+    end
+  end
+
+  describe '.add_task' do
+    it 'can add a task' do
+      @project.add_task("Get Milk", 3)
+      value = TM::Task.task_list.values.find { |t| t.project == 1 }
+      
+      expect(value.description).to eq("Get Milk")
+    end
+  end
+  
+  describe '.get_incomplete_tasks' do
+    it 'can display all incomplete tasks in order of priority, then date' do
+      project=TM::Project.new("Task List")
+
+      project.add_task("Get Milk", 3)
+      project.add_task("Take insulin", 10)
+      project.add_task("Pay mortgage", 7)
+      project.add_task("Get eggs", 3)
+
+      tasks = project.get_incomplete_tasks
+
+      expect(tasks.map { |t| t.description }).to eq(["Take insulin", "Pay mortgage", "Get Milk", "Get eggs"])
+    end
+  end
+      
+      
+
 end
+
+  
