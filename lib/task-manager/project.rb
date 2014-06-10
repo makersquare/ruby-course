e2bc4809750f01e7a5a76a4d1443bd8ada4d2a98
Andy Schetter <andyschetter@gmail.com>
2014-06-10 10:00:25 -0500

    Add project class with name and id assignment and rspec tests

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,17 @@
 
 class TM::Project
+  attr_reader :name, :id
+  @@counter = 0
+
+  def initialize name
+    @name = name
+    @id = @@counter
+    @@counter += 1
+  end
+
+  def retrieve_completed_tasks
+  end
+
+  def retrieve_incomplete_tasks
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,4 +4,20 @@ describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
+  it "must have a name" do
+    project = TM::Project.new("Project 1")
+    expect(project.name).to eq("Project 1")
+  end
+
+  it "has a project id" do
+    project = TM::Project.new("Project 1")
+    expect(project.id).to eq("Project 1")
+  end
+
+  xit "can retrieve a list of completed tasks"
+  end
+
+  xit "can retrieve a list of incomplete tasks"
+  end
 end
