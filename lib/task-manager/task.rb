4600a504cfac2dac22b4143069b90ab87655aa74
Brittney Kistner <bkistner01@gmail.com>
2014-06-10 15:18:11 -0700

    Completed initialization and priority check for task.rb

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -1,3 +1,22 @@
 
 class TM::Task
+
+  attr_accessor :name, :status, :creation_date, :priority_number, :description
+
+  def initialize(name, priority_number,description=nil)
+    @name = name
+    @status = "available"
+    @creation_date = Time.now
+    @priority_number = priority_number
+    @description = description
+  end
+
+  def priority_check(priority_number)
+    if priority_number > 5 || priority_number < 1
+      return nil
+    else
+      @priority_number
+    end
+  end
+
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -4,4 +4,38 @@ describe 'Task' do
   it "exists" do
     expect(TM::Task).to be_a(Class)
   end
+
+  context 'when initialized' do
+    # before(:each) do
+    #   task1 = TM::Task.new("task1",5)
+    # end
+    it "has a name" do
+      task1 = TM::Task.new("task1",5)
+      expect(task1.name).to eq("task1")
+    end
+
+    it "has a creation date" do
+      task1 = TM::Task.new("task1",5)
+    end
+
+    it "has a priority number" do
+      task1 = TM::Task.new("task1",5)
+      expect(task1.priority_number).to eq(5)
+    end
+
+    it "has a status of available" do
+      task1 = TM::Task.new("task1",5)
+      expect(task1.status).to eq("available")
+    end
+
+    it "will return a nil description if nothing provided" do
+      task1 = TM::Task.new("task1",5)
+      expect(task1.description).to eq(nil)
+    end
+
+    xit "will return nil if the priority number is not inclusive of 1..5" do
+      task1 = TM::Task.new("task1",6)
+      expect(task1.priority_number).to eq(nil)
+    end
+  end
 end
