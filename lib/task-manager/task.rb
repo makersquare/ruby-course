72ad5e1e5e23e9f0a0a2a1a0dc5454e0ea109a85
Loren Hale <loren.hale@gmail.com>
2014-06-10 10:56:31 -0500

    add :id and :created_at to tasks

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -1,12 +1,16 @@
 
 class TM::Task
-  attr_reader :description, :priority, :project_id
+  attr_reader :id, :description, :priority, :project_id, :created_at
+
+  @@counter = 0
 
   def initialize description, priority, project_id
+    @id          = @@counter += 1
     @description = description
     @priority    = priority
     @project_id  = project_id
-    @complete   = false
+    @complete    = false
+    @created_at  = Time.now
   end
 
   def complete?
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -17,6 +17,15 @@ describe 'Task' do
     expect(task.project_id).to eq(project_id)
   end
 
+  it "is given a default ID and created_at time" do
+    time = Time.new(2014,01,01,15,10)
+    expect(Time).to receive(:now).and_return(time)
+    expect(task).to receive(:id).and_return(1)
+
+    expect(task.id).to eq(1)
+    expect(task.created_at).to eq(time)
+  end
+
   it "can be set to :complete by ID" do
     task.complete
     expect(task.complete?).to eq(true)
