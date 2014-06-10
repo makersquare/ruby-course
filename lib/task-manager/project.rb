6eb4c451ea90d6f83968e0d5cb94da86d0b95f84
Loren Hale <loren.hale@gmail.com>
2014-06-10 10:49:04 -0500

    Project can be created

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,19 @@
 
 class TM::Project
+  attr_reader :name, :id
+
+  @@counter = 0
+
+  def initialize name
+    @name = name
+    @id   = @@counter += 1
+  end
+
+  def completed
+    [ ]
+  end
+
+  def incompleted
+    [ ]
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,7 +1,57 @@
 require 'spec_helper'
 
 describe 'Project' do
+  let(:klass    ) { TM::Project }
+  let(:name     ) { 'Blah' }
+  let(:project  ) { klass.new(name) }
+
+  # let(:task) do
+  #   task = mock_model(TM::Task)
+  #   task.stub!(:status)
+  #   task.stub!(:priority)
+  # end
+
+  # let(:task1) do
+  #   task.status = :complete
+  #   task.priority = 1
+  #   task
+  # end
+
+  # let(:completed) { [task, task, task] }
+
   it "exists" do
-    expect(TM::Project).to be_a(Class)
+    expect(klass).to be_a(Class)
+  end
+
+  it "must have a name" do
+    expect(project.name).to eq(name)
+  end
+
+  it "must have an ID" do
+    expect(project.id).to eq(2)
+  end
+
+  it "retrieves a list of complete tasks sorted by priority" do
+    task1 = double('Task', :status => :complete, :priority => 1)
+    task2 = double('Task', :status => :complete, :priority => 2)
+    completed = [task1, task2]
+    allow(project).to receive(:completed).and_return(completed)
+
+    expect(project.completed).to eq(completed)
+  end
+
+  describe "it retrieves a list of incomplete tasks sorted by priortiy" do
+    it "must" do
+      task1 = double('Task', :status => :incomplete, :priority => 1)
+      task2 = double('Task', :status => :incomplete, :priority => 2)
+      incompleted = [task1, task2]
+      allow(project).to receive(:incompleted).and_return(incompleted)
+
+      expect(project.incompleted).to eq(incompleted)
+    end
+
+    context "when two tasks have the same priority number" do
+      it "the older task gets priority"
+    end
   end
 end
