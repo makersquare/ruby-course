472bbfccbc437a38883055dfe7e4c9efa58ebe81
Brittney Kistner <bkistner01@gmail.com>
2014-06-10 14:40:16 -0700

    Completed initialization check test for project.rb

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,3 +1,26 @@
 
 class TM::Project
+
+  @@id_hello = 0
+
+  attr_reader :name
+  attr_accessor :id
+
+  def initialize(name)
+    @name = name
+    @id = @@id_hello +=1
+  end
+
+
+  def sort_tasks_priority
+  end
+
+  def sort_tasks_date
+  end
+
+  def complete_tasks
+  end
+
+  def incomplete_tasks
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,4 +4,36 @@ describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
+
+  it 'initializes with a name and id' do
+    project1 = TM::Project.new("project1")
+    expect(project1.name).to eq("project1")
+    expect(project1.id).to eq(1)
+
+    project2 = TM::Project.new("project2")
+    expect(project2.name).to eq("project2")
+    expect(project2.id).to eq(2)
+  end
+
+  it 'sorts tasks by priority number' do
+  end
+
+  it 'sorts tasks by completion date' do
+  end
+
+  it 'retrieves a list of all completed tasks' do
+  end
+
+  it 'retrives a list of all incomplete tasks' do
+  end
+
+  it 'sorts the tasks in an incomplete list by priorty number' do
+  end
+
+  it 'sorts tasks in an incomplete list by creation date if the priority number is the same' do
+  end
+
 end
+
+
+
