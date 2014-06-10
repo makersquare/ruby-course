f93b724530517ab8aeb700e4efaf7fa0801ccc19
Loren Hale <loren.hale@gmail.com>
2014-06-10 10:48:43 -0500

    Task can be created and set to complete

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -1,3 +1,19 @@
 
 class TM::Task
+  attr_reader :description, :priority, :project_id
+
+  def initialize description, priority, project_id
+    @description = description
+    @priority    = priority
+    @project_id  = project_id
+    @complete   = false
+  end
+
+  def complete?
+    @complete
+  end
+
+  def complete
+    @complete = true
+  end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,7 +1,24 @@
 require 'spec_helper'
 
 describe 'Task' do
+  let(:klass      ) { TM::Task }
+  let(:project_id ) { 1 }
+  let(:description) { 'My Task' }
+  let(:priority   ) { 1 }
+  let(:task       ) { klass.new(description, priority, project_id) }
+
   it "exists" do
-    expect(TM::Task).to be_a(Class)
+    expect(klass).to be_a(Class)
+  end
+
+  it "can be created with a project_id, description, and priority" do
+    expect(task.description).to eq(description)
+    expect(task.priority).to eq(priority)
+    expect(task.project_id).to eq(project_id)
+  end
+
+  it "can be set to :complete by ID" do
+    task.complete
+    expect(task.complete?).to eq(true)
   end
 end
