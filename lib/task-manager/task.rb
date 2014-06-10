a19312c448953add774ed4a7b2ed1a5b3261324c
Paul Dombkowski <pdombkowski@gmail.com>
2014-06-10 14:59:19 -0500

    Initial commit of task class and tests

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -1,3 +1,27 @@
 
 class TM::Task
+  attr_reader :project, :description, :priority, :created, :complete, :id
+
+  @@counter=0
+  @@task_list={}
+
+  def self.task_list
+    @@task_list
+  end
+
+  def initialize(project_id, description, priority=5)
+    @@counter += 1
+    @id = "P#{project_id}_T#{@@counter}".to_sym
+    @project = project_id
+    @priority = priority
+    @description = description
+    @created = Time.now()
+    @complete = false
+    @@task_list[@id] = self
+  end
+
+  def done
+    @complete = true
+  end
+
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,7 +1,53 @@
 require 'spec_helper'
+require 'timecop'
 
 describe 'Task' do
   it "exists" do
     expect(TM::Task).to be_a(Class)
+  end
+
+  before(:all) do
+    @task = TM::Task.new(25, 'Pay credit card', 10)
+  end
+
+  describe '.initialize' do
+    it 'has a project id' do
+      expect(@task.project).to eq(25)
+    end
+
+    it 'has a priority' do
+      expect(@task.priority).to eq(10)
+    end
+
+    it 'has a description' do
+      expect(@task.description).to eq('Pay credit card')
+    end
+
+    it 'is not complete by default' do
+      expect(@task.complete).to eq(false)
+    end
+
+    it 'has a creation date' do
+      Timecop.freeze(Time.now)
+      task = TM::Task.new(1,1,"nothing")
+
+      expect(task.created).to eq(Time.now)
+    end
+
+    it 'has a unique id' do
+      expect(@task.id).to eq(:P25_T1)
+    end
+
+    it 'is a member of the task hash' do
+      expect(TM::Task.task_list[@task.id].description).to eq(@task.description)
+    end
+  end
+
+  describe '.mark_complete' do
+    it 'can mark a task as done' do
+      @task.done()
+
+      expect(@task.complete).to eq(true)
+    end
   end
 end
