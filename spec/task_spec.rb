7309ee08b5fc0c2829d7be699292c62b1c233a72
Gabe Polk <gabe.a.polk@gmail.com>
2014-06-10 16:54:07 -0700

    Finished testing for Task initializer

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -5,16 +5,29 @@ describe 'Task' do
     expect(TM::Task).to be_a(Class)
   end
 
+  let(:task) {TM::Task.new("Design a wireframe", 5, 123456789)}
+
   describe '#initialize' do
-    it "is a Task"
-    it "inherits the project id" #class variable?
-    it "has a description"
-    it "has a priority number"
-    it "has a default complete status of 'false'"
-    it "has a creation date"
+    it "is a Task" do
+      expect(task).to be_a(TM::Task)
+    end
+    it "inherits the project id" do
+      expect(task.project_id).to eq(123456789)
+    end
+    it "has a description" do
+      expect(task.description).to eq("Design a wireframe")
+    end
+    it "has a priority number" do
+      expect(task.priority).to eq(5)
+    end
+    it "has a default complete status of 'false'" do
+      expect(task.complete).to eq(false)
+    end
+    it "has a creation date" do
+      expect(task.creation_time).not_to eq(nil)
+    end
   end
 
   describe '#change_complete_status'
     it "marks the task as incomplete if it's complete"
-    # it "marks the task as complete if it's incomplete"
 end
