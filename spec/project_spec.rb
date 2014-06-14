ff7c5ecf9ab447e1e688bdde49fe624d942c9bbd
DJ Daniels <dj.daniels90@gmail.com>
2014-06-14 11:45:11 -0700

    nothing done

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,19 +4,8 @@ describe 'Project' do
   it "exists" do
     expect(TM::Project).to be_a(Class)
   end
-  it "contains classwide population variable used for PID assignment, starting at 0" do
-    expect(TM::Project.task_nums).to eql(0)
-  end
+
   describe "#initialize" do
-    let(:proj) { TM::Project.new("project name") }
-
-    it "throws errors if initialized without a name" do
-      expect{TM::Project.new}.to raise_exception(ArgumentError)
-    end
-    it "initializes with a publicaly readable name" do
-      expect(proj.name).to eql("project name")
-    end
-
 
 
   end
