969183a9978a54edfc3553d51db224f279633615
DJ Daniels <dj.daniels90@gmail.com>
2014-06-15 16:14:12 -0700

    created necessary class methods and instance methods for the project class

--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,11 +1,45 @@
 
 class TM::Project
-  attr_reader :name
+  attr_reader :name, :pid, :project_tasks
 
+  @@project_index = 0
+  @@master_project_list = {}
 
   def initialize(name)
     @name = name
-
+    @pid = @@project_index
+      @@project_index += 1
+    @project_tasks = []
+    @@master_project_list[@pid] = self
   end
 
+  def self.get_project(pid)
+    @@master_project_list[pid]
+  end
+
+  def self.get_projects
+    @@master_project_list.values
+  end
+
+  def self.destroy_all_projects(bool)
+    if bool
+      @@project_index = 0
+      @@master_project_list = {}
+    end
+  end
+
+  def add_task(desc, priority)
+    new_task = TM::Task.new(@pid, desc, priority)
+    @project_tasks << new_task
+  end
+
+  def completed_tasks
+    completed = @tasks.select {|tsk| tsk.complete}
+    completed.sort {|a, b| b.creation_time <=> a.creation_time}
+  end
+
+  def incomplete_tasks
+    incomplete = @tasks.select {|ts| !tsk.complete}
+    incomplete.sort {|a,b| (b.priority <=> a.priority) == 0 ? (b.creation_time <=> a.creation_time) : (b.priority <=> a.priority)}
+  end
 end
