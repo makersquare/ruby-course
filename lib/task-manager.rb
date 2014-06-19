5e074c8f89a7affe4588a8a7a102297a72c7cb63
Catherine Noll <noll.catherine@gmail.com>
2014-06-18 18:48:00 -0700

    Add database methods for projects, tasks, and employees, and update project.rb to call DB methods

--- lib/task-manager.rb
+++ lib/task-manager.rb
@@ -5,4 +5,6 @@ end
 # Require all of our project files
 require_relative 'task-manager/task.rb'
 require_relative 'task-manager/project.rb'
+require_relative 'task-manager/db.rb'
+require_relative 'task-manager/employee.rb'
 # require 'pry-debugger'
--- lib/task-manager/project.rb
+++ lib/task-manager/project.rb
@@ -1,58 +1,50 @@
 require 'time'
 require 'pg'
 
-@@db = PG.connect(host: 'localhost', dbname: 'task-manager')
-
 class TM::Project
   attr_accessor :name, :pid, :tasks
 
-  @@projects = [] ##add to projects table of db
+  @@projects = []
 
   def initialize(name=nil)
     @name = name
-    @pid = @@projects.size #change to return of result
     @tasks = [] ##add to tasks table of db
     @@projects << self
-    result = @@db.exec("INSERT INTO projects(name) VALUES ('#{@name}, #{@creation_date}') RETURNING *;")
-  end
-
-  def self.create_tables
-    @@db.exec("CREATE TABLE projects(pid SERIAL, name TEXT, creation_date TIMESTAMP, PRIMARY KEY(pid));")
-    @@db.exec("CREATE TABLE tasks(tid SERIAL, priority INTEGER, description TEXT, pid INTEGER REFERENCES projects(pid), status TEXT, creation_date TIMESTAMP, PRIMARY KEY(tid));")
   end
 
   def add_task(priority, description)
     task = TM::Task.new(priority, description)
     @tasks << task
-    result = @@db.exec("INSERT INTO tasks(description) VALUES('#{@priority}, #{@description}, #{@pid}, #{@status}, #{@creation_date}') RETURNING *;")
-    result.values
+    TM.db.add_task(priority, description, pid, eid=nil, status='incomplete', creation_date=Time.now)
+  end
+
+  def list_employee_tasks
+    TM.db.list_employee_tasks(eid)
+  end
+
+  def list_employee_projects(eid)
+    TM.db.list_employee_tasks(eid)
   end
 
   def list_all_tasks
-    result = @@db.exec("SELECT * FROM tasks;")
-    result.values
+    TM.db.list_all_tasks
     # puts @tasks.map { |i| puts "TID #{i.tid}: Priority #{i.priority}, #{i.description}" }
   end
 
   def list_completed_tasks
-   # result = @@db.exec("SELECT * FROM tasks WHERE status ='complete' AND #{task.pid}=@pid;")
-    result = @@db.exec("PREPARE tasks(priority, description) AS SELECT * FROM tasks WHERE status ='complete' AND #{task.pid}=@pid;")
-
-    result.values
+    TM.db.list_completed_tasks
     # @tasks.sort_by! { |i| i.creation_date }
     # @tasks.select { |i| puts "TID #{i.tid}: #{i.description}" if i.status == 'complete' }
   end
 
   def list_incomplete_tasks
-    result = @@db.exec("SELECT * FROM tasks WHERE status ='incomplete';")
-    result.values
+    TM.db.list_incomplete_tasks
     # @tasks.sort_by! { |i| [i.priority, i.creation_date] }
     # @tasks.select { |i| puts "TID #{i.tid}: Priority #{i.priority}, #{i.description}" if i.status == 'incomplete' }
   end
 
-  def self.list_projs
-    result = @@db.exec("SELECT * FROM projects;")
-    result.values
+  def self.list_projects
+    TM.db.list_projects
     # @@projects.each_with_index { |i,idx| puts "PID #{idx}: #{i.name}" }
   end
 
