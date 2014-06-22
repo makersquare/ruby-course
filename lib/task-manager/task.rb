aceec752d3a1599705d97ffe9de3ed8c0c36b61d
Loren Hale <loren.hale@gmail.com>
2014-06-22 10:24:47 -0500

    refactor task and specs

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -1,35 +1,6 @@
-
 class TM::Task
   attr_reader :id, :description, :priority,
               :project_id, :employee_id, :completed, :created_at
-
-  # @@tasks   = [ ]
-  # @@counter = 0
-
-  # def self.all
-  #   @@tasks
-  # end
-
-  # def self.find(task_id)
-  #   @@tasks.find {|t| t.id == task_id}
-  # end
-
-  # def self.tasks_for(project_id, completed = false)
-  #   all.select do |task|
-  #     task.complete? == completed && task.project_id == project_id
-  #   end.sort_by do |task|
-  #     if completed
-  #       task.created_at
-  #     else
-  #       [task.priority, task.created_at]
-  #     end
-  #   end
-  # end
-
-  # def self.set_complete(task_id)
-  #   task = find(task_id)
-  #   task.mark_complete
-  # end
 
   def initialize(args)
     @id          = args[:id]
@@ -40,26 +11,4 @@ class TM::Task
     @completed   = args[:completed]
     @created_at  = args[:created_at]
   end
-
-  # def save!
-  #   TM.db.create('tasks', create_args)
-  # end
-
-  # def create
-  #   args = TM.db.create_task( [ @priority, @description, @project_id, @employee_id, @completed ] )
-  #   @id         = args[:id]
-  #   @created_at = args[:created_at]
-  #   self
-  # end
-
-  # def complete?
-  #   @completed
-  # end
-
-  # def mark_complete
-  #   @completed = true
-  #   self
-  # end
-
-
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,70 +1,31 @@
 require 'spec_helper'
 
 describe 'Task' do
-  # let(:project    ) { TM::Project.new('Test Project') }
-  # let(:description) { 'My Task' }
-  # let(:priority   ) { 1 }
-  # let(:task       ) { klass.new(description, priority, project.id) }
-
   let(:klass) { TM::Task }
 
-  before(:each) do
-    klass.class_variable_set :@@counter, 0
-    klass.class_variable_set :@@tasks, [ ]
+  let(:args) { {
+    :id          => 1,
+    :description => 'Test Task',
+    :priority    => 1,
+    :project_id  => 1,
+    :employee_id => 1,
+    :completed   => false,
+    :created_at  => Time.now
+  } }
 
-    @project = TM::Project.new("Test Project")
-    @task1 = klass.new(@project.id, 1, "Task 1").mark_complete
-    @task2 = klass.new(@project.id, 2, "Task 2").mark_complete
-    @task3 = klass.new(@project.id, 3, "Task 3")
-    @task4 = klass.new(@project.id, 4, "Task 4")
-  end
+  let(:task) { klass.new(args) }
 
   it "exists" do
     expect(klass).to be_a(Class)
   end
 
-  it "responds to find given a task_id by returning the task" do
-    expect(klass.find(@task1.id)).to eq(@task1)
-  end
-
-  it "responds to all by returning all tasks" do
-    expect(klass.all.first).to be_a(klass)
-    expect(klass.all.length).to eq(4)
-  end
-
-  context "responds to tasks_for given a project_id" do
-    it "when incomplete" do
-      expect(klass.tasks_for(@project.id).length).to eq(2)
-      expect(klass.tasks_for(@project.id).first).to eq(@task3)
-    end
-    it "when complete by passing an optional parameter 'true'" do
-      expect(klass.tasks_for(@project.id, true).length).to eq(2)
-      expect(klass.tasks_for(@project.id, true).first).to eq(@task1)
-    end
-  end
-
-  it "can be created with a project_id, priority, description" do
-    expect(@task1.project_id).to eq(@project.id)
-    expect(@task1.priority).to eq(1)
-    expect(@task1.description).to eq('Task 1')
-  end
-
-  it "is given a default ID and created_at time" do
-    # time = Time.new(2014,01,01,15,10)
-    # expect(Time).to receive(:now).and_return(time)
-    # expect(@task1).to receive(:id).and_return(1)
-
-    expect(@task1.id).to eq(1)
-    expect(@task1.created_at).to be_a(Time)
-  end
-
-  it "Class responds to set_complete given a task_id by setting the task to completed" do
-    expect( @task3.complete? ).to eq(false)
-    expect(klass.set_complete(@task3.id).complete?).to eq(true)
-  end
-
-  it "instance responds to mark_complete by setting the task to completed" do
-    expect( @task3.complete? ).to eq(false)
-    expect(@task3.mark_complete.complete?).to eq(true)
+  it "initialize accepts an arg hash with id, description, priority, project_id, employee_id, completed, created_at" do
+    expect(task.id).to eq(1)
+    expect(task.description).to eq('Test Task')
+    expect(task.priority).to eq(1)
+    expect(task.project_id).to eq(1)
+    expect(task.employee_id).to eq(1)
+    expect(task.completed).to eq(false)
+    expect(task.created_at).to be_a(Time)
   end
 end
