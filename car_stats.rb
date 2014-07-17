42bad41fd51bba4945cf21d75d0c9858a62078be
Catherine O'Neill <cathzozo@gmail.com>
2014-07-16 22:09:23 -0500

    refactored to make create_hash method

--- car_stats.rb
+++ car_stats.rb
@@ -1,16 +1,19 @@
 class CarStats
   def self.calc_top_color(car_array)
+    color_stats = CarStats.create_hash(car_array) 
+    color_stats.key(color_stats.values.max)
+  end
+
+  def self.create_hash(car_array)
     color_of_cars = car_array.map { |x| x.color }
     colors = color_of_cars.uniq
     
     color_stats = {}
+    
     colors.each do |x| 
       y = color_of_cars.count(x)  
       color_stats[x] = y
     end
-    
-    color_stats.key(color_stats.values.max)
-
-  
+    color_stats
   end
 end
