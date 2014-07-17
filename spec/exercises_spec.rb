24b701ccd546a217a0ea4ac7d48b6c0ff55b8a0b
Emma Follender <efollender@gmail.com>
2014-07-17 14:46:27 -0500

    Added tests to check whether ex1 returns the number of elements in a given array, whether ex2 returns the second element in a given array, and whether ex3 returns the sum of a given array of numbers

--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -3,6 +3,9 @@ require "./exercises.rb"
 
 
 describe Exercises do
+  before(:each) do
+    @arr = [1,2,3]
+  end
 
   it 'triples a given string' do
     Exercises.ex0('blah').should eq('blahblahblah')
@@ -10,5 +13,17 @@ describe Exercises do
 
   it 'returns nope if the string is wishes' do
     Exercises.ex0('wishes').should eq('nope')
+  end
+
+  it 'returns the number of elements in an array' do
+    Exercises.ex1(@arr).should eq(3)
+  end
+
+  it 'returns the second element of an array' do
+    Exercises.ex2(@arr).should eq(2)
+  end
+
+  it 'returns the sum of the given array of numbers' do
+    Exercises.ex3(@arr).should eq(6)
   end
 end
