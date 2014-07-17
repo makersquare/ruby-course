e890dc6a998cc2cdd0251072c6eb33893c28f518
Daniel Cox <draycox@gmail.com>
2014-07-17 16:25:10 -0500

    Exercise 1 Complete, Added in RSPEC file structure

--- exercises.rb
+++ exercises.rb
@@ -12,8 +12,9 @@ module Exercises
 
   # Exercise 1
   #  - Returns the number of elements in the array
-  def self.ex1(array)
-    # TODO
+  
+  def self.ex1(array)  
+    array.size
   end
 
   # Exercise 2
--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -1,16 +1,83 @@
 require 'pry-byebug'
 require './exercises.rb'
 
-describe 'Exercise 0' do
-  it "triples the length of a string" do
-    result = Exercises.ex0("ha")
-    expect(result).to eq("hahaha")
+describe 'Exercises' do
+
+  context "Exercise 0" do
+    it "triples the length of a string" do
+      result = Exercises.ex0("ha")
+      expect(result).to eq("hahaha")
+    end
+
+    it "returns 'nope' if the string is 'wishes'" do
+      result = Exercises.ex0("wishes")
+      expect(result).to eq("nope")
+    end
+  end
+    
+  context "Exercise 1" do  
+    it "returns the number of elements in the array" do
+      result = Exercises.ex1([1,2,3])
+      expect(result).to be(3)
+    end
   end
 
-  it "returns 'nope' if the string is 'wishes'" do
-    result = Exercises.ex0("wishes")
-    expect(result).to eq("nope")
+  context "Exercise 2" do  
+    xit "returns the number of elements in the array" do
+      result = Exercises.ex1(array)
+      expect(result).to be(array.count)
+    end
   end
+
+  context "Exercise 3" do  
+    xit "returns the number of elements in the array" do
+      result = Exercises.ex1(array)
+      expect(result).to be(array.count)
+    end
+  end
+
+  context "Exercise 4" do  
+    xit "returns the number of elements in the array" do
+      result = Exercises.ex1(array)
+      expect(result).to be(array.count)
+    end
+  end
+
+  context "Exercise 5" do  
+    xit "returns the number of elements in the array" do
+      result = Exercises.ex1(array)
+      expect(result).to be(array.count)
+    end
+  end
+
+  context "Exercise 6" do  
+    xit "returns the number of elements in the array" do
+      result = Exercises.ex1(array)
+      expect(result).to be(array.count)
+    end
+  end
+
+  context "Exercise 7" do  
+    xit "returns the number of elements in the array" do
+      result = Exercises.ex1(array)
+      expect(result).to be(array.count)
+    end
+  end
+
+  context "Exercise 8" do  
+    xit "returns the number of elements in the array" do
+      result = Exercises.ex1(array)
+      expect(result).to be(array.count)
+    end
+  end
+
+  context "Exercise 9" do  
+    xit "returns the number of elements in the array" do
+      result = Exercises.ex1(array)
+      expect(result).to be(array.count)
+    end
+  end
+
 
 
 end
