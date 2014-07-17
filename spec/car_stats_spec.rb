227d4b85e0cd87ae55113e423dd7f189b90df7d6
Joey G <digitaljedi@outlook.com>
2014-07-17 11:25:14 -0500

    completed cars assignment

--- spec/car_stats_spec.rb
+++ spec/car_stats_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 
-describe CarStats, :pending => true do
+describe CarStats do
 
   before do
     @cars = []
     3.times { @cars << Car.new('black') }
     5.times { @cars << Car.new('silver') }
     2.times { @cars << Car.new('white') }
+    #puts @cars
   end
 
   it "calculates the most popular car color" do
+    #binding.pry
     pop_color = CarStats.calc_top_color(@cars)
     expect(pop_color).to eq 'silver'
   end
