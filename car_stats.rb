da37bcbde5318f65557f5fe7769c38180d1439af
daniel <demise4u@gmail.com>
2014-07-16 22:20:41 -0500

    completed cars lesson

--- car_stats.rb
+++ car_stats.rb
@@ -1,3 +1,14 @@
 class CarStats
-  # TODO
+  def self.calc_top_color(input)
+    frequency = Hash.new(0)
+    input.each {|x| frequency[x.color] +=1}
+    top_color = frequency.sort_by {|k,v| v}
+    top_color[-1][0]
+  end
+  def self.calc_bottom_color(input)
+    frequency = Hash.new(0)
+    input.each {|x| frequency[x.color] +=1}
+    bottom_color = frequency.sort_by {|k,v| v}
+    bottom_color[0][0]
+  end
 end
