4115df02b32b707aa55096a90fec55d87219e989
Catherine O'Neill <cathzozo@gmail.com>
2014-07-16 22:16:06 -0500

    pass calc_bottom_color

--- car_stats.rb
+++ car_stats.rb
@@ -4,6 +4,11 @@ class CarStats
     color_stats.key(color_stats.values.max)
   end
 
+  def self.calc_bottom_color(car_array)
+    color_stats = CarStats.create_hash(car_array)
+    color_stats.key(color_stats.values.min)
+  end
+
   def self.create_hash(car_array)
     color_of_cars = car_array.map { |x| x.color }
     colors = color_of_cars.uniq
