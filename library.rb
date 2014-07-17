9efcd2a15a358c91ab437735caa54db835e72f14
Derek Ellis <dkellis42@gmail.com>
2014-07-16 20:48:23 -0500

    Does not allow a borrower to check out more than two books completed

--- library.rb
+++ library.rb
@@ -1,6 +1,6 @@
 
 class Book
-  attr_accessor :title, :author, :id, :status
+  attr_reader :title, :author, :id, :status
 
   def initialize(title, author, id=nil)
     @title = title
@@ -12,7 +12,7 @@ class Book
   def check_out
     if @status == 'available'
       @status = 'checked_out'
-      true
+      return true
     else
       return false
     end
@@ -21,7 +21,7 @@ class Book
   def check_in
     if @status == 'checked_out'
       @status = 'available'
-      true
+      return true
     else
       return false
     end
@@ -29,75 +29,60 @@ class Book
 end
 
 class Borrower
-  attr_accessor :name
-
-
+  attr_reader :name
   def initialize(name)
     @name = name
-
   end
 end
 
+
 class Library
-  attr_accessor :books, :name, :id
-  
+  attr_reader :books, :name, :id
   def initialize
-    @books = []
     @id_tab = 0
+    @books = []
     @borrowers = {}
     @books_checked = {}
     @names = []
-
-
   end
-# @@ people << { name: name, id: @@person_counter }
-  # def books
-  # end
+
   def register_new_book(title, author, id=@id_tab)
     @books << Book.new(title, author, id)
     @id_tab += 1
   end
 
-
-  def add_book(title, author)
-
-  end
-
   def check_out_book(book_id, borrower, count=0)
-    if @books[book_id].status == 'checked_out'
-      return nil
+    pointer = 0
+    @names.select { |name| pointer += 1 if name == borrower.name }
+    book = @books.find { |book| book.id == book_id }
+    if(book.status == 'available' && pointer < 2)
+      @borrowers[borrower.name] = book
+      @names << borrower.name
+      book.check_out
+      book
     else
-      @names << @books_checked.values #setting array equal to values in @books_checked {  }
-      @pointer = 0
-      @names.select do |name|
-        if name == borrower.name
-          @pointer += 1
-        end
-      end
-      if @pointer < 2
-        @books[book_id].check_out
-        @books_checked[book_id] = borrower
-        @borrowers[borrower.name] = @books[book_id]
-      else
-        return nil
-      end
-
+      return nil
     end
-    #id => borrower
-    #sam => id
-    # books_checked = 0
-    # @books.each do |x|
-    #   if x.status == 'checked_out'
-    #     return nil
-    #   elsif x.borrower
-    #     books_checked += 1
-    #       return nil if books_checked >= 2
-    #   else
-    #     x.check_out(borrower)
-    #     return x
-    #   end
-    # end
   end
+
+
+
+
+
+    # return nil if @books[book_id].status == 'checked_out'
+    # @pointer = 0
+    # @names.select do |name|
+    #   @pointer += 1 if name == borrower.name
+    # end
+    # if @pointer < 2
+      
+    #   @books[book_id].check_out
+    #   @books_checked[book_id] = borrower.name
+    #   @borrowers[borrower.name] = @books[book_id]
+    # else
+    #   return nil
+    # end
+
 
   def get_borrower(book_id)
     @borrowers.key(@books[book_id])
