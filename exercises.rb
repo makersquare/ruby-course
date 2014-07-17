35078a73ab179e0389d4c4dd4aaf45012632117a
Catherine O'Neill <cathzozo@gmail.com>
2014-07-17 16:14:40 -0500

    exercise 0 passed

--- exercises.rb
+++ exercises.rb
@@ -4,13 +4,17 @@ module Exercises
   #  - Triples a given string `str`
   #  - Returns "nope" if `str` is "wishes"
   def self.ex0(str)
-    # TODO
+    if str == "wishes"
+      'nope'
+    else
+      "#{str}#{str}#{str}"
+    end
   end
 
   # Exercise 1
   #  - Returns the number of elements in the array
   def self.ex1(array)
-    # TODO
+    #TODO
   end
 
   # Exercise 2
