a3e3b72f193f5b81201395223f2039e4758ab9ea
RandyDavis <randy_davis@outlook.com>
2014-07-17 11:41:24 -0500

    Update menu_items method, add Food class, and add menu item method works to pass test.

--- bar.rb
+++ bar.rb
@@ -2,10 +2,25 @@ require 'time' # you're gonna need it
 
 class Bar
 
-  attr_reader :name
+  attr_reader :name, :menu_items
 
   def initialize(name="The Irish Yodel")
     @name = name
+    @menu_items = []
   end
 
+  def add_menu_item(item, price)
+    newfood = Food.new(item, price)
+    @menu_items.push(newfood)
+  end
+end
+
+class Food
+
+  attr_reader :item, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
 end
