6588458df0e330618fae508e5395d24750d74862
Austin Sprawls <austinsprawls@gmail.com>
2014-07-17 11:08:13 -0500

    DRYed CarStat class by pulling out car_colors_array class method

--- car_stats.rb
+++ car_stats.rb
@@ -1,12 +1,16 @@
 
 class CarStats
   def self.calc_top_color(cars)
-    car_colors = cars.map {|car| car.color}
+    car_colors = car_colors_array(cars)
     car_colors.sort_by {|color| car_colors.count(color)}.pop
   end
 
+  def self.car_colors_array(cars)
+    cars.map {|car| car.color}
+  end
+
   def self.calc_bottom_color(cars)
-    car_colors = cars.map {|car| car.color}
+    car_colors = car_colors_array(cars)
     car_colors.sort_by {|color| car_colors.count(color)}.shift
   end
 end
