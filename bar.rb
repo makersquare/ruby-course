3d50ff5bea587f0e6cd83c6a7e2f8e143fd87380
Allison Zadrozny <allison@zadrozny.com>
2014-07-17 15:12:39 -0500

    Completed Bar up to '  has a default happy hour discount of zero'

--- bar.rb
+++ bar.rb
@@ -3,13 +3,15 @@ require 'time' # you're gonna need it
 class Bar
 
   attr_reader :name, :menu_items
+  attr_accessor :happy_discount
 
-  def initialize(name)
+  def initialize(name, happy_discount=0)
     @name = name
     # because the add_menu item method has two arguments, 
     # I think a hash is best to keep track of them: @bar.add_menu_item('Cosmo', 5.40)
     # but then I initialized them as objects, so now it's an array of objects with attributes name and price.
     @menu_items = []
+    @happy_discount = happy_discount
   end
 
   def add_menu_item(item, price)
@@ -17,6 +19,14 @@ class Bar
     new_item = MenuItem.new(item, price)
     @menu_items << new_item
   end
+
+  def happy_hour?
+    # the following two lines is my janky code before majorly improved.
+    # @happy_hour = Time.now
+    # return true if Time.parse('3 pm') <= @happy_hour && @happy_hour <= Time.parse('4 pm')
+    Time.now.hour == 15
+  end
+
 end
 
 #Since we can retreive menu items and call methods on them, they have attributes. Thus, a Class is born!
@@ -27,3 +37,4 @@ class MenuItem
     @price = price
   end
 end
+
--- spec/bar_spec.rb
+++ spec/bar_spec.rb
@@ -37,7 +37,7 @@ describe Bar do
     expect(item.price).to eq 9.95
   end
 
-  xit "has a default happy hour discount of zero" do
+  it "has a default happy hour discount of zero" do
     expect(@bar.happy_discount).to eq 0
   end
 
@@ -81,14 +81,18 @@ describe Bar do
   # DO NOT CHANGE SPECS ABOVE THIS LINE #
 # # # # # # # # # # # # # # # # # # # # # #
 
-  describe '#happy_hour?', :pending => true do
+  describe '#happy_hour?' do
     it "knows when it is happy hour (3:00pm to 4:00pm)" do
       # TODO: CONTROL TIME
+      captured_time = Time.parse('3:30 pm')
+      Time.stub(:now).and_return(captured_time)
       expect(@bar.happy_hour?).to eq(true)
     end
 
     it "is not happy hour otherwise" do
       # TODO: CONTROL TIME
+      late_time = Time.parse('7 PM')
+      Time.stub(:now).and_return(late_time)
       expect(@bar.happy_hour?).to eq(false)
     end
   end
