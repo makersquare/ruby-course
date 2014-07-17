373412aea91f743a037ac5b379f2ec3991b1aaeb
RandyDavis <randy_davis@outlook.com>
2014-07-17 08:53:13 -0500

    Complete CarStats to pass all tests.

--- car_stats.rb
+++ car_stats.rb
@@ -1,3 +1,18 @@
 class CarStats
   # TODO
+  def self.calc_top_color(cars)
+    top=Hash.new(0) # Create hash to hold all car colors
+    cars.each do |car| # Iterate over each cars key to iterate by 1
+      top[car.color] += 1 
+    end
+    top.sort_by{ |color, freq| freq }.last.first
+  end
+
+  def self.calc_bottom_color(cars)
+    bottom=Hash.new(0) 
+    cars.each do |car|
+      bottom[car.color] += 1
+    end
+    bottom.sort_by{ |color, freq| freq }.first.first
+  end
 end
