9409cdd5c93159059c022a6194392fb6ffe18fc2
Emma Follender <efollender@gmail.com>
2014-07-17 10:53:54 -0500

    Created the Bar class which initializes with a name, empty menu_items array, and default happy_discount of 0. Created a method to add items to the menu_item array. Created the MenuItem class which initializes with a name and price.

--- bar.rb
+++ bar.rb
@@ -1,4 +1,27 @@
 require 'time' # you're gonna need it
 
 class Bar
+  attr_reader :name, :menu_items
+  attr_accessor :happy_discount
+
+  def initialize(name)
+    @name = name
+    @menu_items = []
+    @happy_discount = 0
+  end
+
+  def add_menu_item(name, price)
+    new_item = MenuItem.new(name, price)
+    @menu_items.push(new_item)
+  end
+
+end
+
+class MenuItem
+  attr_reader :name, :price, :details
+  def initialize(name, price)
+    @name = name
+    @price = price
+    @details = {name: name, price: price}
+  end
 end
