7d89cc66005ef0d6a7db3144e023aea505811885
Justin Beuck <jbeuck@yahoo.com>
2014-07-17 11:45:43 -0500

    completed car.rb and car_stats.rb along with tests

--- car_stats.rb
+++ car_stats.rb
@@ -1,3 +1,20 @@
 class CarStats
-  # TODO
+
+	def self.calc_top_color cars
+		best = Hash.new(0)
+		cars.each {|v| best[v.color] +=1} 
+		best.max_by{|k,v| v}.first
+	end
+
+	def self.calc_bottom_color cars
+		best = Hash.new(0)
+		cars.each {|v| best[v.color] +=1} 
+		best.min_by{|k,v| v}.first
+	end
 end
+
+# class Car
+#	def initialize(color)
+#		@color = color
+#	end
+#end
