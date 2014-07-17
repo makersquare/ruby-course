65d5daa41895ce0a9fad9792acf4353e877ceb67
Aaron Case <aacase@gmail.com>
2014-07-17 09:45:29 -0500

    Create initialize, add_menu_item, rspec up to third test

--- bar.rb
+++ bar.rb
@@ -1,4 +1,16 @@
 require 'time' # you're gonna need it
 
 class Bar
+  attr_reader :name, :menu_items
+  def initialize(name)
+    @name=name
+    @menu_items=[]
+  end
+  def add_menu_item(item, price)
+    @item=item
+    @price=price
+    menhash=Hash.new{}
+    menhash[item]=price
+    @menu_items.push(menhash)
+  end
 end
