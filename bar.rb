871d9c35c1168d4d8d3eb4ec86e059e160a32251
Emma Follender <efollender@gmail.com>
2014-07-18 15:10:12 -0500

    Added custom_discount method

--- bar.rb
+++ bar.rb
@@ -10,6 +10,7 @@ class Bar
     @discount = 0
     @exempt = []
     @purchases = {}
+    @custom_discounts = {}
   end
 
   def add_menu_item(name, price)
@@ -47,33 +48,21 @@ class Bar
   end
   def get_price(item_name)
     item = @menu_items.find {|item| item.name == item_name}
+    discount = @discount
+    if @custom_discounts.keys.include? item_name
+      discount = @custom_discounts[item_name]
+    end
     if (happy_hour? && (Time.now.monday? || Time.now.wednesday?) && !(@exempt.include? item))
-      (item.price - item.price * @discount).round(2)
+      (item.price - item.price * discount).round(2)
     elsif (happy_hour? && !(@exempt.include? item))
-      (item.price - item.price * (@discount/2)).round(2)
+      (item.price - item.price * (discount/2)).round(2)
     else
       item.price
     end
   end
 
   def purchase
-    choice = show_menu
-    item = @menu_items[(choice.to_i - 1)].name
-    puts "\nHere's your #{item}!\n\n"
-    if @purchases.keys.include? item
-      @purchases["#{item}"] += 1
-    else
-      @purchases["#{item}"] = 1
-    end
-  end
-
-  def show_menu
-    @count = 1 
-    @menu_items.each do |x|
-      price = get_price(x.name)
-      puts "#{@count}. #{x.name} $%.2f" % x.price
-      @count += 1
-    end
+    show_menu
     puts "Which would you like? (use number)"
     @choice = gets.chomp
     items = @menu_items.count
@@ -81,11 +70,34 @@ class Bar
       puts "Invalid choice. Try again."
       @choice = gets.chomp
     end
-    @choice
+    item = @menu_items[(@choice.to_i - 1)].name
+    puts "\nHere's your #{item}!\n\n"
+    if @purchases.keys.include? item
+      @purchases["#{item}"] += 1
+    else
+      @purchases["#{item}"] = 1
+    end
+    puts "Would you like to make another purchase? Y/N"
+    repeat = gets.chomp.upcase
+    purchase if repeat == 'Y'
+  end
+
+  def show_menu
+    @count = 1 
+    @menu_items.each do |x|
+      price = get_price(x.name)
+
+      puts "#{@count}. #{x.name} $%.2f" % x.price
+      @count += 1
+    end
   end
 
   def most_popular
     puts "The most popular drink is the #{@purchases.key(@purchases.values.max)} which has been purchased #{@purchases.values.max} times."
+  end
+
+  def custom_discount(item_name,discount)
+    @custom_discounts[item_name] = discount
   end
 end
 
@@ -97,13 +109,5 @@ class MenuItem
   end
 end
 
-bar = Bar.new("Videology")
-bar.add_menu_item('Cosmo', 5.40)
-bar.add_menu_item('Salty Dog', 7.80)
-bar.add_menu_item('Gin and Tonic', 6)
-bar.add_menu_item('Whiskey Sour', 8)
-bar.purchase
-bar.purchase
-bar.purchase
-puts bar.purchases
-puts bar.most_popular
+
+
