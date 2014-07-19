259d17e0701c86ba703f34f3659a54bf0432ab20
RandyDavis <randy_davis@outlook.com>
2014-07-19 17:08:17 -0500

    Write test for ex 1 that successfully fails.

--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -11,3 +11,12 @@ describe 'Exercise 0' do
     expect(result).to eq("nope")
   end
 end
+
+describe 'Exercise 1' do
+
+  it "returns the number of elements in the array" do
+    result = [1, 2, "cats", 5.1]
+    expect(result).to eq 4
+  end
+
+end
