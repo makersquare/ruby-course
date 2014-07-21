c6aa78b35d05e8a383a11d800a604358453c4212
Emma Follender <efollender@gmail.com>
2014-07-20 22:22:40 -0500

    Added check for Monday or Wednesday and adjusted discount accordingly. Added ability to make drinks exempt from happy hour. Added purchase method which allows a patron to buy drinks from a menu using the show_menu method.

--- bar.rb
+++ bar.rb
@@ -8,14 +8,10 @@ class Bar
     @menu_items = []
     @happy_discount = 0
     @discount = 0
-    @exempt = []
-    @purchases = {}
-    @custom_discounts = {}
   end
 
   def add_menu_item(name, price)
-    new_item = MenuItem.new(name, price)
-    @menu_items.push(new_item)
+    @menu_items.push(MenuItem.new(name, price))
   end
 
   def happy_discount
@@ -43,18 +39,16 @@ class Bar
       @discount = 0
     end
   end
-  def exempt(drink)
-    @exempt << drink
+  def exempt(item_name)
+    @menu_items.find{|drink| drink.name == item_name}.exempt = true
+
   end
   def get_price(item_name)
-    item = @menu_items.find {|item| item.name == item_name}
-    discount = @discount
-    if @custom_discounts.keys.include? item_name
-      discount = @custom_discounts[item_name]
-    end
-    if (happy_hour? && (Time.now.monday? || Time.now.wednesday?) && !(@exempt.include? item))
+    item = @menu_items.find{|drink| drink.name == item_name}
+    item.custom_discount ? discount = item.custom_discount : discount = @discount
+    if (happy_hour? && (Time.now.monday? || Time.now.wednesday?) && !(item.exempt))
       (item.price - item.price * discount).round(2)
-    elsif (happy_hour? && !(@exempt.include? item))
+    elsif (happy_hour? && !(item.exempt))
       (item.price - item.price * (discount/2)).round(2)
     else
       item.price
@@ -63,51 +57,74 @@ class Bar
 
   def purchase
     show_menu
-    puts "Which would you like? (use number)"
-    @choice = gets.chomp
-    items = @menu_items.count
-    while !((1..items).to_a.include?(@choice.to_i))
+    puts "Which would you like?"
+    @choice = gets.chomp.to_i - 1
+    items = @menu_items.length
+    while !((1..items).to_a.include?(@choice))
       puts "Invalid choice. Try again."
-      @choice = gets.chomp
+      @choice = gets.chomp.to_i - 1
     end
-    item = @menu_items[(@choice.to_i - 1)].name
-    puts "\nHere's your #{item}!\n\n"
-    if @purchases.keys.include? item
-      @purchases["#{item}"] += 1
-    else
-      @purchases["#{item}"] = 1
-    end
+    puts "\nHere's your %s!\n\n" % @menu_items[@choice].name
+    happy_hour? ? @menu_items[@choice].purchases[:happy_hour] += 1 : @menu_items[@choice].purchases[:regular_hours] += 1
     puts "Would you like to make another purchase? Y/N"
     repeat = gets.chomp.upcase
     purchase if repeat == 'Y'
   end
 
   def show_menu
-    @count = 1 
-    @menu_items.each do |x|
-      price = get_price(x.name)
-
-      puts "#{@count}. #{x.name} $%.2f" % x.price
-      @count += 1
+    count = 1 
+    @menu_items.each do |k|
+      price = get_price(k.name)
+      puts "#{count}. #{k.name} $%.2f" % k.price
+      count += 1
     end
   end
 
-  def most_popular
-    puts "The most popular drink is the #{@purchases.key(@purchases.values.max)} which has been purchased #{@purchases.values.max} times."
-  end
+def most_popular
+  regular = @menu_items.max_by {|k| k.purchases[:regular_hours]}
+  happy = @menu_items.max_by {|k| k.purchases[:happy_hour]}
+  puts "The most popular drink during regular hours is the " + regular.name.to_s + " which has been purchased " + regular.purchases[:regular_hours].to_s + " times."
+  puts "The most popular drink during happy hour is the " + happy.name.to_s + " which has been purchased " + happy.purchases[:happy_hour].to_s + " times."
+end
 
   def custom_discount(item_name,discount)
-    @custom_discounts[item_name] = discount
+    @menu_items.find{|drink| drink.name == item_name}.custom_discount = discount
+  end
+
+  def get_purchase_data
+    happy_purchases = 0
+    regular_purchases = 0
+    @menu_items.each do |drink|
+      happy_purchases += drink.purchases[:happy_hour]
+      regular_purchases += drink.purchases[:regular_hours]
+    end
+    puts "Happy hour purchases: #{happy_purchases}"
+    puts "Regular hours purchases: #{regular_purchases}"
   end
 end
 
 class MenuItem
-  attr_reader :name, :price, :details
+  attr_reader :name, :price, :exempt, :purchases
+  attr_accessor :custom_discount
   def initialize(name, price)
     @name = name
     @price = price
+    @exempt = false
+    @purchases = {happy_hour: 0, regular_hours: 0}
+    @custom_discount = nil
   end
 end
 
-
+bar = Bar.new("Videology")
+bar.add_menu_item('G&T', 6)
+bar.add_menu_item('Rum and Coke', 5)
+bar.add_menu_item('Blue Moon', 3)
+bar.add_menu_item('Whiskey Sour', 7)
+bar.add_menu_item('Cosmo', 5.40)
+bar.add_menu_item('Salty Dog', 7.80)
+bar.custom_discount('Rum and Coke', 0.3)
+bar.happy_discount = 0.5
+bar.purchase
+bar.get_purchase_data
+bar.most_popular
 
