ac6f34da22c367e9042037754dec07a5267098bd
Daniel Cox <draycox@gmail.com>
2014-07-22 16:10:20 -0500

    Got basic functionality in place, missing tests

---
+++ puppybreeder.rb
@@ -0,0 +1,57 @@
+class Puppy
+
+  attr_reader :breed, :name, :age
+
+  def initialize(args)
+    @breed = args[:breed]
+    @name = args[:name]
+    @age = args[:age]
+  end
+end
+
+
+
+class Inventory
+
+  attr_reader :po
+
+  def initialize
+    @inventory = []
+    @po = []
+  end
+
+  def puppy_list
+    @inventory.map{ |x| x.breed }.inject(Hash.new(0)){ |sum,elem| sum[elem]+= 1; sum }
+  end
+
+  def intake(args)
+    @@inventory << Puppy.new(args)
+  end
+
+  def po_list
+    @po.map{|x| x}
+  end
+
+end
+
+
+
+class PurchaseOrder
+
+  attr_reader :customer,:breed
+
+  def initialize(args)
+    @customer = args[:customer]
+    @breed = args[:breed]
+  end
+
+  def add(inventory)
+    inventory.po << self
+  end
+
+  def review
+    "Order for #{@customer} ~~~~> Breed: #{@breed}"
+  end
+
+end
+
