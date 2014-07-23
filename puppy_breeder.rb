988b3c0472d59c22495ec09a54a2ad86549deac2
Christine Oen <oen.christine@gmail.com>
2014-07-23 11:29:00 -0500

    working code, before rspec testing

--- puppy_breeder.rb
+++ puppy_breeder.rb
@@ -1,12 +1,25 @@
 class PurchaseRequest
-  attr_reader :breed_wanted, :purchaser_name
-  attr_accessor :accepted
+  attr_reader :breed_wanted, :purchaser_name, :request_time
+  attr_accessor :status
 
   def initialize(purchaser_name, breed_wanted)
     @purchaser_name = purchaser_name
     @breed_wanted = breed_wanted
-    @accepted = false
-  end 
+    @request_time = Time.now
+    @status = "pending"
+  end
+
+  def accepted?
+    @status == "accepted"
+  end
+
+  def pending?
+    @status == "pending"
+  end
+
+  def on_hold?
+    @status == "on hold"
+  end
 
 end
 
@@ -15,41 +28,30 @@ class PurchaseRequestInventory
 
   def initialize
     @request_array = []
-    @on_hold_array = []
     # binding.pry
-
-  #   need an approval process between puppy being added and accepting request
-  # when a puppy is born, need to automatically make on hold puppies. time.now attribute for requests. sort by method for time.
-
-
-# @status pending, accepted, on hold
-#   def pending?
-#     @status == pending
-#   end
-
   end
 
   def add_purchase_request(purchase_request, puppy_inventory)
 
-    if puppy_inventory.puppy_hash[purchase_request.breed_wanted.to_sym][:list].map {|x| x.available == true}[0] == true
+    if puppy_inventory.puppy_hash.has_key?(purchase_request.breed_wanted.to_sym)
       @request_array << purchase_request
     else
-      @on_hold_array << purchase_request
+      purchase_request.status = "on hold"
     end
-    
+
   end
 
   def view_requests
-    @request_array.find {|request| request.accepted == false}
+    @request_array.select {|request| request.pending? == true}.sort_by {|request| request.request_time}
   end
 
   def accept_purchase_request(purchase_request, puppy)
-    purchase_request.accepted = true
+    purchase_request.status = "accepted"
     puppy.available = false
   end
 
   def view_completed_orders
-    @request_array.find {|request| request.accepted == true}
+    @request_array.find {|request| request.accepted? == true}
   end
 
 end
@@ -107,13 +109,21 @@ puppy_inventory.add_puppy_to_inventory(puppy3)
 request1 = PurchaseRequest.new("Chris", "boxer")
 request2 = PurchaseRequest.new("Zach", "boxer")
 request3 = PurchaseRequest.new("Aaron", "pit")
+request4 = PurchaseRequest.new("Emma", "lab")
 
 request_inventory.add_purchase_request(request1, puppy_inventory)
 request_inventory.add_purchase_request(request2, puppy_inventory)
 request_inventory.add_purchase_request(request3, puppy_inventory)
+request_inventory.add_purchase_request(request4, puppy_inventory)
 
 puppy_inventory.print_puppy_inventory
 
+request_inventory.view_requests
+
+
+  #   need an approval process between puppy being added and accepting request
+  # when a puppy is born, need to automatically make on hold puppies. 
+  # time.now attribute for requests. sort by method for time.
 
 
 
