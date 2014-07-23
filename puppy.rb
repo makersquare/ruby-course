1604219673f3de35eb6493d70171193b6d89a113
Austin Sprawls <austinsprawls@gmail.com>
2014-07-23 14:25:05 -0500

    added functionality to part II of puppy breeder project

--- puppy.rb
+++ puppy.rb
@@ -7,56 +7,111 @@ module PuppyMill
       @name = args[:name]
       @age = args[:age]
     end
+
   end
 
   class Puppies
     @all_pups = {}
 
-    def self.add_pup(puppy)
-      pup_breed = @all_pups[puppy.breed]
-      @all_pups.has_key?(puppy.breed) ? (pup_breed[:list] << puppy) : (pup_breed = {price: :unknown, list: [puppy]})
+    def self.add_pup(puppy, klass=PuppyMill::PurchaseOrders)
+      klass.modify_hold_orders(puppy)
+      @all_pups.has_key?(puppy.breed) ? (@all_pups[puppy.breed][:list] << puppy) : (@all_pups[puppy.breed] = {price: :unknown, list: [puppy]})
     end
 
     def self.all_pups
       @all_pups
     end
 
-    def self.check_breed(breed)
+    def self.breed_available?(breed)
       @all_pups.has_key?(breed)
     end
   end
 
   class PurchaseOrder
-    attr_reader :customer_name, :breed
+    attr_reader :customer_name, :breed, :id
+    attr_accessor :status
+    @@id = 0
 
     def initialize(args)
       @customer_name = args[:customer_name]
       @breed = args[:breed]
+      @status = :pending
+      @id = @@id+=1
     end
 
     def review
       "customer: #{customer_name}, puppy breed: #{breed}"
     end
-
-    # def satisfy_order?
-    #   PuppyMill::Puppies.check_breed(breed)
-    # end
-
-    def accept
-      PuppyMill::PurchaseOrders.add_purchase_order(self)
-    end
   end
 
   class PurchaseOrders
-    @all_purchase_orders = []
+    @orders = []
 
-    def self.add_purchase_order(order)
-      @all_purchase_orders << order
+    def self.orders
+      @orders
     end
 
-    def self.view_all
-      @all_purchase_orders
+    def self.fulfill_order?(klass=PuppyMill::Puppies, order)
+      return true if klass.breed_available?(order.breed)
+    end
+
+    def self.add_order(order)
+      order.status = :hold unless fulfill_order?(order)
+      @orders << order
+    end
+
+    def self.modify_hold_orders(puppy)
+      unless @orders.empty?
+        new_pending_orders = @orders.select {|order| puppy.breed == order.breed}.each {|o| o.status = :pending}
+        delete_old_orders(new_pending_orders)
+        prioritize_hold_to_front_of_pending(new_pending_orders)
+      end
+    end
+
+    def self.delete_old_orders(new_pending_orders)
+      @orders = @orders.delete_if {|order| new_pending_orders.include?(order)}
+    end
+
+    def self.prioritize_hold_to_front_of_pending(new_pending_orders)
+      @orders = (new_pending_orders << @orders).flatten
+    end
+
+    def self.accept(accepted_order)
+      @orders = @orders.each {|order| order.status = :accepted if order == accepted_order}
+    end
+
+    def self.deny(denied_order)
+      @orders = @orders.each {|order| order.status = :denied if order == denied_order}
+    end
+
+    def self.view_all_accepted_orders
+      @orders.select {|order| order.status == :accepted}
+    end
+
+    def self.view_all_pending_orders
+      @orders.select {|order| order.status == :pending}.sort_by {|order| order.id}
     end
   end
-
 end
+
+# spot = PuppyMill::Puppy.new(name: 'Spot', age: 100, breed: 'dalmation')
+# rex = PuppyMill::Puppy.new(name: 'Rex', age: 250, breed: 'great dane')
+#
+# order1 = PuppyMill::PurchaseOrder.new(customer_name: 'Austin', breed: 'dalmation')
+# order2 = PuppyMill::PurchaseOrder.new(customer_name: 'Joe', breed: 'great dane')
+#
+# PuppyMill::PurchaseOrders.add_order(order2)
+# PuppyMill::PurchaseOrders.add_order(order1)
+#
+# puts "order 1 status: #{order1.status}"
+#
+#
+# puts PuppyMill::PurchaseOrders.orders
+#
+# # PuppyMill::Puppies.add_pup(spot)
+# # PuppyMill::Puppies.add_pup(rex)
+# PuppyMill::PurchaseOrders.modify_hold_orders(rex)
+# PuppyMill::PurchaseOrders.modify_hold_orders(spot)
+# puts "order1 status: #{order1.status}"
+# puts "order2 status: #{order2.status}"
+# puts PuppyMill::PurchaseOrders.orders
--- spec/puppy_spec.rb
+++ spec/puppy_spec.rb
@@ -23,7 +23,23 @@ describe PuppyMill::Puppy do
   it "allows breeder to input new puppies" do
     @rex.should_not be_nil
   end
+end
 
+describe PuppyMill::Puppies do
+  before(:all) do
+    @spot = PuppyMill::Puppy.new(breed: 'dalmation', name: 'Spot', age: 100)
+    @rex = PuppyMill::Puppy.new(name: 'Rex', breed: 'great dane', age: 365)
+  end
+
+  it "allows breeder to add puppies to inventory" do
+    PuppyMill::Puppies.add_pup(@spot)
+    PuppyMill::Puppies.add_pup(@rex)
+    PuppyMill::Puppies.all_pups.size.should eq 2
+  end
+
+  it ".breed_available? returns true if breed is available" do
+    PuppyMill::Puppies.breed_available?(@rex.breed)
+  end
 end
 
 describe PuppyMill::PurchaseOrder do
@@ -32,6 +48,7 @@ describe PuppyMill::PurchaseOrder do
     @spot = PuppyMill::Puppy.new(breed: 'dalmation', name: 'Spot', age: 100)
     @fido = PuppyMill::Puppy.new(breed: 'poodle', name: 'Fido', age: 100)
     @purchase = PuppyMill::PurchaseOrder.new(customer_name: 'Joe Dirt', breed: 'dalmation' )
+    @purchase2 = PuppyMill::PurchaseOrder.new(customer_name: 'Joe Dirt', breed: 'poodle' )
   end
 
   it "allows breeder to create purchase orders" do
@@ -43,14 +60,57 @@ describe PuppyMill::PurchaseOrder do
     @purchase.review.should eq "customer: Joe Dirt, puppy breed: dalmation"
   end
 
-  it "allows breeder to accept purchase order" do
-    @purchase.accept
-    PuppyMill::PurchaseOrders.view_all.first.should be @purchase
+  it "initializes with a default :pending status" do
+    @purchase.status.should eq :pending
+  end
+end
+
+describe PuppyMill::PurchaseOrders do
+  before(:all) do
+    @rex = PuppyMill::Puppy.new(name: 'Rex', breed: 'great dane', age: 365)
+    @spot = PuppyMill::Puppy.new(breed: 'dalmation', name: 'Spot', age: 100)
+    @fido = PuppyMill::Puppy.new(breed: 'poodle', name: 'Fido', age: 100)
+    @purchase = PuppyMill::PurchaseOrder.new(customer_name: 'Joe Dirt', breed: 'dalmation' )
+    @purchase2 = PuppyMill::PurchaseOrder.new(customer_name: 'Billy Bob', breed: 'poodle' )
   end
 
+  it "allows breeder to add purchase orders to store" do
+    PuppyMill::PurchaseOrders.add_order(@purchase)
+    PuppyMill::PurchaseOrders.add_order(@purchase2)
+    PuppyMill::PurchaseOrders.orders.size.should eq 2
+  end
 
-  it "allows breeder to check if a purchase order can be satisfied" do
+  it ".fulfill_order? returns false if breed not available" do
+    PuppyMill::PurchaseOrders.fulfill_order?(@purchase)
+  end
+
+  it ".fulfill_order? returns true if breed is available" do
     PuppyMill::Puppies.add_pup(@spot)
-    @purchase.satisfy_order?.should be_true
+    PuppyMill::PurchaseOrders.fulfill_order?(@purchase2)
+  end
+
+  it ".modify_hold_orders changes order status if breed becomes available" do
+    # PuppyMill::PurchaseOrders.add_order(@purchase2)
+    @purchase2.status.should eq :hold
+
+    PuppyMill::PurchaseOrders.modify_hold_orders(@fido)
+    @purchase2.status.should eq :pending
+  end
+
+  it ".accept allows breeder to accept orders" do
+    PuppyMill::PurchaseOrders.accept(@purchase2)
+    @purchase2.status.should eq :accepted
+  end
+
+  it ".deny allows breeder to deny orders" do
+    PuppyMill::PurchaseOrders.deny(@purchase2)
+    @purchase2.status.should eq :denied
+  end
+
+  it ".view_all_accepted_orders displays all accepted orders" do
+    PuppyMill::PurchaseOrders.accept(@purchase2)
+    PuppyMill::PurchaseOrders.accept(@purchase)
+    PuppyMill::PurchaseOrders.view_all_accepted_orders.include?(@purchase).should be_true
+    PuppyMill::PurchaseOrders.view_all_accepted_orders.include?(@purchase2).should be_true
   end
 end
