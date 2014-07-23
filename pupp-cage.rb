fa2d7bf0f832401b33fac8de5ca228d0077e8a1e
justin-lilly <jlilly353@gmail.com>
2014-07-23 01:01:24 -0500

    Refactored last commit

--- pupp-cage.rb
+++ pupp-cage.rb
@@ -8,10 +8,11 @@ module PuppCage
   def PuppCage.add_puppy(*puppy)
     # Puppy.new(breed:breed, cost:PUPPY_LIST[breed][:cost], age:0)
     puppy.each do |pupp| 
-      if PuppCage.breed_contains_pupps?(pupp)
-        PUPPY_LIST[pupp.breed][:puppies] << pupp
-      else
+      if PUPPY_LIST[pupp.breed].nil? || PUPPY_LIST[pupp.breed][:puppies].nil?
+        PuppCage.alter_breed_price(pupp.breed)
         PUPPY_LIST[pupp.breed][:puppies] = [pupp]
+      else
+        PUPPY_LIST[pupp.breed][:puppies] << pupp
       end
     end
   end
@@ -28,26 +29,12 @@ module PuppCage
     REQUEST_LIST.delete(request)
   end
 
-  def PuppCage.alter_breed_price(breed, cost=0)
-    
-    if PuppCage.breed_type_exists?(breed)
-      PUPPY_LIST[breed][:cost]=cost
-    else
+  def PuppCage.alter_breed_price(breed, cost=0)    
+    if PUPPY_LIST[breed].nil?
       PUPPY_LIST[breed] = {cost:cost}
-    end
-  end
-
-  def PuppCage.breed_contains_pupps?(pupp)
-    if PuppCage.breed_type_exists?(pupp.breed)
-      PUPPY_LIST[pupp.breed][:puppies].nil? ? false : true
     else
-      PuppCage.alter_breed_price(pupp.breed)
-      false
+      PUPPY_LIST[breed][:cost]=cost
     end
-  end
-
-  def PuppCage.breed_type_exists?(breed)
-    PUPPY_LIST[breed].nil? ? false : true
   end
 
 end
