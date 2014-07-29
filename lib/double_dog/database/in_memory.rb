463d4abeb5073ace3ee8f729a6bcfc3b3e11a8ae
Gilbert <gilbertbgarza@gmail.com>
2014-07-29 11:27:39 -0500

    Refactor in memory db

--- lib/double_dog/database/in_memory.rb
+++ lib/double_dog/database/in_memory.rb
@@ -17,12 +17,12 @@ module DoubleDog
         new_id = (@item_id_counter += 1)
         @users[new_id] = attrs
         attrs[:id] = new_id
-        User.new(attrs[:id], attrs[:username], attrs[:password], attrs[:admin])
+        build_user(attrs)
       end
 
       def get_user(id)
         attrs = @users[id]
-        User.new(attrs[:id], attrs[:username], attrs[:password], attrs[:admin])
+        build_user(attrs)
       end
 
       def create_session(attrs)
@@ -35,30 +35,30 @@ module DoubleDog
         return nil if @sessions[sid].nil?
         user_id = @sessions[sid][:user_id]
         user_attrs = @users[user_id]
-        User.new(user_attrs[:id], user_attrs[:username], user_attrs[:password], user_attrs[:admin])
+        build_user(user_attrs)
       end
 
       def get_user_by_username(username)
         user_attrs = @users.values.find { |attrs| attrs[:username] == username }
         return nil if user_attrs.nil?
-        User.new(user_attrs[:id], user_attrs[:username], user_attrs[:password], user_attrs[:admin])
+        build_user(user_attrs)
       end
 
       def create_item(attrs)
         new_id = (@item_id_counter += 1)
         @items[new_id] = attrs
         attrs[:id] = new_id
-        Item.new(attrs[:id], attrs[:name], attrs[:price])
+        build_item(attrs)
       end
 
       def get_item(id)
         attrs = @items[id]
-        Item.new(attrs[:id], attrs[:name], attrs[:price])
+        build_item(attrs)
       end
 
       def all_items
         @items.values.map do |attrs|
-          Item.new(attrs[:id], attrs[:name], attrs[:price])
+          build_item(attrs)
         end
       end
 
@@ -66,19 +66,34 @@ module DoubleDog
         new_id = (@order_id_counter += 1)
         @orders[new_id] = attrs
         attrs[:id] = new_id
-        Order.new(attrs[:id], attrs[:employee_id], attrs[:items])
+        build_order(attrs)
       end
 
       def get_order(id)
         attrs = @orders[id]
-        Order.new(attrs[:id], attrs[:employee_id], attrs[:items])
+        build_order(attrs)
       end
 
       def all_orders
         @orders.values.map do |attrs|
-          Order.new(attrs[:id], attrs[:employee_id], attrs[:items])
+          build_order(attrs)
         end
       end
+
+    private
+
+      def build_user(attrs)
+        User.new(attrs[:id], attrs[:username], attrs[:password], attrs[:admin])
+      end
+
+      def build_item(attrs)
+        Item.new(attrs[:id], attrs[:name], attrs[:price])
+      end
+
+      def build_order(attrs)
+        Order.new(attrs[:id], attrs[:employee_id], attrs[:items])
+      end
+
     end
   end
 end
