43884a4b5c5ceea018b963de3f9bfc90df630907
Joey G <digitaljedi@outlook.com>
2014-08-21 10:38:02 -0500

    wrote transaction script and hashed passwords

--- lib/honkr/scripts/sign_in.rb
+++ lib/honkr/scripts/sign_in.rb
@@ -1,8 +1,24 @@
 module Honkr
   class SignIn
 
+  	attr_reader :result
+
     def self.run(params)
-      # TODO
+      @result = {}
+      @params = params
+      @username = params[:username]
+      @password = params[:password]
+      # Databases.InMemory.get_user(@params)
+      user = Honkr.db.get_user_by_username(@username)
+      if user && user.has_password?(@password)
+      	@result[:session_id] = Honkr.db.create_session(:user_id => user.id)
+      	@result[:success?] = true
+      	return @result
+      else
+      	@result[:error] = :invalid_password
+      	@result[:success?] = false
+      	@result
+      end
     end
 
   end
