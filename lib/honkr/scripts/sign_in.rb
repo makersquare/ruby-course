5862c98d4a90c79ea8556072beca875bacb93e42
Joey G <digitaljedi@outlook.com>
2014-08-21 11:11:07 -0500

    refactored with some better code

--- lib/honkr/scripts/sign_in.rb
+++ lib/honkr/scripts/sign_in.rb
@@ -4,20 +4,30 @@ module Honkr
   	attr_reader :result
 
     def self.run(params)
-      @result = {}
-      @params = params
-      @username = params[:username]
-      @password = params[:password]
+      user = Honkr.db.get_user_by_username(params[:username])
+      # @result = {}
+      # @params = params
+      # @username = params[:username]
+      # @password = params[:password]
       # Databases.InMemory.get_user(@params)
-      user = Honkr.db.get_user_by_username(@username)
-      if user && user.has_password?(@password)
-      	@result[:session_id] = Honkr.db.create_session(:user_id => user.id)
-      	@result[:success?] = true
-      	return @result
-      else
-      	@result[:error] = :invalid_password
-      	@result[:success?] = false
-      	@result
+      # user = Honkr.db.get_user_by_username(@username)
+      if !user.has_password?(params[:password])
+      	return {success?: false, error: :invalid_password}
+      	# @result[:session_id] = Honkr.db.create_session(:user_id => user.id)
+      	# @result[:success?] = true
+      	# return @result
+      # else
+  	  end
+
+  	  session = Honkr.db.create_session(:user_id => user.id)
+  	  {
+  	  	success: true,
+  	  	session_id: session)
+  	  }
+
+      	# @result[:error] = :invalid_password
+      	# @result[:success?] = false
+      	# @result
       end
     end
 
