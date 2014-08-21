fe79a9eb10f0c08d7513b50bf1eecdb033812daa
Aaron Case <aacase@gmail.com>
2014-08-21 10:31:59 -0500

    complete sign in and pass hex specs

--- lib/honkr.rb
+++ lib/honkr.rb
@@ -1,3 +1,4 @@
+require 'digest'
 
 module Honkr
   def self.db
--- lib/honkr/entities/user.rb
+++ lib/honkr/entities/user.rb
@@ -10,11 +10,14 @@ module Honkr
     end
 
     def update_password(password)
-      # TODO: Hash incoming password and save as password digest
+      @password = password
+      @password_digest = Digest::SHA1.hexdigest(password)
     end
 
     def has_password?(password)
-      # TODO: Hash incoming password and compare against own password_digest
+      if @password_digest == Digest::SHA1.hexdigest(password)
+        true
+      end
     end
   end
 end
