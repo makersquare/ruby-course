36d4e8dad3801e9638e6602214d886629d7a04dc
Alexandria A. Johnson <alexandria.abdallah@gmail.com>
2014-08-28 11:02:30 -0500

    Sets a class method to determine the most and least popular colors in an array of Car objects

--- car_stats.rb
+++ car_stats.rb
@@ -1,3 +1,27 @@
 class CarStats
-  # TODO
+  def self.calc_top_color(cars)
+  colors = {}
+  cars.each do |car|
+   if colors[car.color] == nil
+      colors[car.color] = 1
+    else 
+      colors[car.color] += 1
+    end
+  end
+     top = colors.max_by {|k,v| v }
+     top[0]
+end
+
+  def self.calc_bottom_color(cars)
+  colors = {}
+  cars.each do |car|
+   if colors[car.color] == nil
+      colors[car.color] = 1
+    else 
+      colors[car.color] += 1
+    end
+  end
+     bottom = colors.min_by {|k,v| v }
+     bottom[0]
+  end
 end
