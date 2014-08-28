be4e82a2b6ef35c33dd758a4887dd4426a39cddf
Matteo Wiley <matteowiley@gmail.com>
2014-08-28 10:58:39 -0500

    This is a day three lesson on Cars.

--- car.rb
+++ car.rb
@@ -12,4 +12,21 @@ class Car
 end
 
 class BigRig < Car
+  def initialize(color)
+    super
+    @wheel_count = 18
+  end
+  def honk
+    'BBBBBRRRRRRAAAAAWWWHHHHH'
+  end
+
 end
+
+class Motorcycle < Car
+  def initialize
+    @color = "red"
+    @wheel_count = 2
+  end
+end
+
+
--- car_stats.rb
+++ car_stats.rb
@@ -1,3 +1,30 @@
+
 class CarStats
-  # TODO
+  def self.calc_top_color(cars)
+    car_color_count = {}
+    cars.each do |x|
+      if car_color_count[x.color] == nil
+        car_color_count[x.color] = 1
+      else 
+        car_color_count[x.color] += 1
+      end
+    end
+    top = car_color_count.max_by {|x,y| y }
+    top[0]
+  end
+
+  def self.calc_bottom_color(cars)
+    car_color_count = {}
+    cars.each do |x|
+      if car_color_count[x.color] == nil
+        car_color_count[x.color] = 1
+      else 
+        car_color_count[x.color] += 1
+      end
+    end
+    bottom = car_color_count.min_by {|x,y| y }
+    bottom[0]
+  end
+
 end
+
