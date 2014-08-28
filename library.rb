1b37c7d880931b5f99f85f5e248b460017a6a59e
Bradley Anderson <bcaytonanderson@gmail.com>
2014-08-28 13:59:50 -0500

    Added register_new_book method to Library.

---
+++ library.rb
@@ -0,0 +1,65 @@
+
+class Book
+  attr_reader :author, :title
+  attr_accessor :id, :status
+
+  def initialize(title, author, id: nil, status: 'available')
+    @author = author
+    @title = title
+    @id = id
+    @status = status
+  end
+  def check_out
+    if @status == 'available'
+      @status = 'checked out'
+      return true
+    else
+      return false
+    end
+  end
+  def check_in
+    if @status == 'checked out'
+      @status = 'available'
+    end
+  end
+end
+
+class Borrower
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Library
+  attr_reader :name
+  attr_accessor :books
+
+  def initialize(name)
+    @name = name
+    @books = []
+  end
+
+  def register_new_book(title, author)
+    @books << Book.new(title, author)
+    @books.each { |x| x.id = rand(1000) }
+  end
+
+  def add_book(title, author)
+    id = rand(1000)
+    @books << Book.new(title, author, id)
+  end
+
+  def check_out_book(book_id, borrower)
+  end
+
+  def check_in_book(book)
+  end
+
+  def available_books
+  end
+
+  def borrowed_books
+  end
+end
