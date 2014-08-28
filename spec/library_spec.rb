fed64492ba4d52a976bfce710448a9791501b3c5
Nick Damiano <damiano.nick@gmail.com>
2014-08-28 12:34:34 -0500

    fixed setup by adding arguments to Book.new

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -5,15 +5,15 @@ describe Book do
   it "has a title and author, and nil id" do
     book = Book.new("The Stranger", "Albert Camus")
 
-    # binding.pry
+    #binding.pry
 
     expect(book.title).to eq "The Stranger"
     expect(book.author).to eq "Albert Camus"
-    expect(book.id).to be_nil
+    expect(book.id).to be nil
   end
 
-  xit "has a default status of available" do
-    book = Book.new
+  it "has a default status of available" do
+    book = Book.new("Zombie Mermen", "Nick Damiano")
     expect(book.status).to eq 'available'
   end
 
