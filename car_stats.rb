35249aef6bc3e588d50147ed02a9d323a5efee4f
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2014-08-28 11:02:39 -0500

    Refactor if statement in helper method color_count to simple if statement.

--- car_stats.rb
+++ car_stats.rb
@@ -3,20 +3,18 @@ class CarStats
 
   def self.calc_top_color(cars_array)
     color_count(cars_array)
-
     @top_color = @count.max_by { |k, v| v}[0]
   end
 
   def self.calc_bottom_color(cars_array)
     color_count(cars_array)
-
     @top_color = @count.min_by { |k, v| v}[0]
   end
 
   def self.color_count(cars_array)
     colors_array = cars_array.map {|x| x.color }
     @count = Hash.new
-    colors_array.each {|x| @count[x] == nil ? @count[x] = 1 : @count[x] += 1}
+    colors_array.each {|x| @count[x] ? @count[x] += 1 : @count[x] = 1 }
     @count
   end
 end
