a2518531aa9c8edf71ab290975b16489281d525a
Lili Serfaty <lili.serfaty@gmail.com>
2014-08-28 10:55:15 -0500

    refactor car_status -- removed class variable for @cars_hash

--- car_stats.rb
+++ car_stats.rb
@@ -1,30 +1,28 @@
 class CarStats
 
-  @@cars_hash = {}
-
   def self.cars_to_hash(cars)
-    @@cars_hash
+    @cars_hash = {}
 
     cars.each do |x|
-      if @@cars_hash[x.color]
-        @@cars_hash[x.color] += 1
+      if @cars_hash[x.color]
+        @cars_hash[x.color] += 1
       else
-        @@cars_hash[x.color] = 1
+        @cars_hash[x.color] = 1
       end
-      @@cars_hash
+      @cars_hash
     end
 
   end
 
   def self.calc_top_color(cars)
     cars_to_hash(cars)
-    @@cars_hash.max_by { |k, v| v}.first
+    @cars_hash.max_by { |k, v| v}.first
 
   end
 
   def self.calc_bottom_color(cars)
     cars_to_hash(cars)
-    @@cars_hash.min_by { |k, v| v}.first
+    @cars_hash.min_by { |k, v| v}.first
   end
 
 end
