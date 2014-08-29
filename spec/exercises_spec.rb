af0fcf48954a3b7e11e11a90fd3634147bb3b2c9
Bradley Anderson <bcaytonanderson@gmail.com>
2014-08-29 14:57:15 -0500

    Wrote tests fo Exercise 0 and added Exercise 0 method.

---
+++ spec/exercises_spec.rb
@@ -0,0 +1,15 @@
+require './exercises.rb'
+require 'pry-byebug'
+
+
+describe 'Exercise 0' do 
+  it "triples the length of a string" do
+    result = Exercises.ex0("ha")
+    expect(result).to eq("hahaha")
+  end
+
+  it "returns 'nope' if the string is 'wishes'" do
+    result = Exercises.ex0("wishes")
+    expect(result).to eq("nope")
+  end  
+end
