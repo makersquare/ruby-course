84efd9712bae482be18524a1f2b3e724246ef71d
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2014-09-02 15:32:06 -0500

    Add test to see if pending_purchase_orders method for Requests class only shows orders with a status of pending. Test passes.

--- spec/requests_spec.rb
+++ spec/requests_spec.rb
@@ -19,8 +19,17 @@ describe PuppyBreeder::Requests do
     it "removes a completed purchase order from the array"
   end
 
-  describe '#purchase_orders' do
-    it "returns the purchase orders array"
+  describe '#pending_purchase_orders' do
+    it "shows only purchase orders with a status of pending" do
+      request1 = PuppyBreeder::PurchaseRequest.new("Golden Retriever")
+      request2 = PuppyBreeder::PurchaseRequest.new("Pitbull")
+
+      request1.accept
+
+      result = PuppyBreeder::Requests.pending_purchase_orders
+
+      expect(result.size).to eq 1
+    end
   end
 
 end
