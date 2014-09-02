047c08ded53f26c5805e52f666a19d0431182a01
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2014-09-02 10:40:35 -0500

    Write failing test for Puppy class initialize method. Write initialize method for Puppy class to make failing test pass.

--- lib/puppy_breeder/puppy.rb
+++ lib/puppy_breeder/puppy.rb
@@ -1,6 +1,13 @@
 #Refer to this class as PuppyBreeder::Puppy
 module PuppyBreeder
   class Puppy
+    attr_reader :name, :age, :breed
+
+    def initialize(name, age, breed)
+      @name = name
+      @age = age
+      @breed = breed
+    end
 
   end
 end
--- spec/puppy_spec.rb
+++ spec/puppy_spec.rb
@@ -2,4 +2,12 @@ require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::Puppy do
 
+  it "creates a puppy with a name, age and breed" do
+    spot = PuppyBreeder::Puppy.new("Spot", 1, "Golden Retriever")
+
+    expect(spot.name).to eq("Spot")
+    expect(spot.age).to eq 1
+    expect(spot.breed).to eq("Golden Retriever")
+  end
+
 end
