c9dd9c11cf6c817b79fd5a032b394c4a8b773b33
Daniel Burnette <dcburnette999@gmail.com>
2014-09-02 11:47:40 -0500

    completed tests for PurchaseRequest class, passing

--- spec/purchase_request_spec.rb
+++ spec/purchase_request_spec.rb
@@ -2,4 +2,19 @@ require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::PurchaseRequest do
 
+  describe '.initialize' do
+
+    it 'sets the breed and price' do
+      request = PuppyBreeder::PurchaseRequest.new("golden retriever")
+      expect(request.breed).to eq("golden retriever")
+      expect(request.price).to eq(200)
+    end
+
+    it 'adds itself into the RequestManager open_requests' do
+      request = PuppyBreeder::PurchaseRequest.new("golden retriever")
+      expect(PuppyBreeder::RequestManager.view_open_requests.include?(request)).to eq(true)
+    end
+
+  end
+
 end
