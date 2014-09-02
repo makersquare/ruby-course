16c13d114a58947813f67480151d5433475cb32e
Penkar <jspenkar@gmail.com>
2014-09-02 12:22:37 -0500

    ability to fulfill requests has been added.

--- lib/puppy_breeder/purchase_request.rb
+++ lib/puppy_breeder/purchase_request.rb
@@ -16,13 +16,36 @@ module PuppyBreeder
 
   class RequestRepository
     @@counter = 1
-    @@list = Hash.new(0)
-    attr_accessor :list
+    @@list = Hash.new
+    @@closed_request=[]
+    @@pending_request = []
+    attr_accessor :number
 
-    # def self.add_request(breed='mix')
-    #   request = PurchaseRequest.new(breed)
-    # end
+    def self.add_request(breed='mix')
+      request = PurchaseRequest.new(breed)
+      @number = @@counter
+      @@counter += 1
+      @@list[@number] = request
+      @@pending_request<< request
+    end
 
+    def self.complete_request(request_id)
+      puts "Request #{@@list[request_id].request_id} has been filled."
+      @@closed_request << @@list[request_id]
+      @@pending_request.delete(@@list[request_id])
+    end
+
+    def self.list
+      @@list
+    end
+
+    def self.pending_list
+      @@pending_request
+    end
+
+    def self.completed_list
+      @@closed_request
+    end
 
   end
 end
