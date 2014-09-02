91cec9d48fe6ebd5cc329fa5d89e3cb0103bd398
Lili Serfaty <lili.serfaty@gmail.com>
2014-09-02 10:36:27 -0500

    write test for initialize method. write code in puppy.rb to pass test.

--- lib/puppy_breeder/puppy.rb
+++ lib/puppy_breeder/puppy.rb
@@ -1,6 +1,13 @@
 #Refer to this class as PuppyBreeder::Puppy
 module PuppyBreeder
   class Puppy
+    attr_accessor :name, :breed, :age
+
+    def initialize(name, breed, age)
+      @name = name
+      @breed = breed
+      @age = age
+    end
 
   end
 end
--- spec/puppy_spec.rb
+++ spec/puppy_spec.rb
@@ -1,5 +1,12 @@
 require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::Puppy do
+  it "initializes with name, breed and age" do
+
+    puppy = PuppyBreeder::Puppy.new("mav", "husky", 4)
+    expect(puppy.name).to eq ("mav")
+    expect(puppy.breed).to eq("husky")
+    expect(puppy.age).to eq(4)
+  end
 
 end
