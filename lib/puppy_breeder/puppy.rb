13beee97826af9b64cc915591515279d19411faa
Travis Hooper <thooper41@gmail.com>
2014-09-02 15:28:39 -0500

    Working puppy-breeder program with passing tests.

--- lib/puppy_breeder/puppy.rb
+++ lib/puppy_breeder/puppy.rb
@@ -1,6 +1,17 @@
 #Refer to this class as PuppyBreeder::Puppy
 module PuppyBreeder
   class Puppy
-
+    attr_reader :name, :breed, :age
+    attr_accessor :price, :status
+    def initialize(name, breed, age=0, status='available', price=nil)
+      @name = name
+      @breed = breed
+      @age = age
+      @status = status
+      @price = price
+    end
   end
+
 end
+
+
--- lib/puppy_breeder/purchase_request.rb
+++ lib/puppy_breeder/purchase_request.rb
@@ -1,6 +1,15 @@
 #Refer to this class as PuppyBreeder::PurchaseRequest
 module PuppyBreeder
   class PurchaseRequest
+    attr_reader :name
+    def initialize(name)
+      @name = name
+    end
+
+    def request_dog(dog, breeder)
+      breeder.pending(dog, self)
+    end
 
   end
 end
+
