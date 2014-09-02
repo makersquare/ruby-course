1a475126dfb922eda242695776db5adb0b34f020
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2014-09-02 11:11:49 -0500

    Add failing tests for initialze method to create unique id and for accept method to change status to completed. Create counter to generate id for purchase request and create accept method.

--- lib/puppy_breeder/purchase_request.rb
+++ lib/puppy_breeder/purchase_request.rb
@@ -1,11 +1,19 @@
 #Refer to this class as PuppyBreeder::PurchaseRequest
 module PuppyBreeder
   class PurchaseRequest
-    attr_reader :breed, :status
+    attr_reader :breed, :status, :id
+
+    @@counter = 1
 
     def initialize(breed)
       @breed = breed
       @status = :pending
+      @id = @@counter
+      @@counter += 1
     end
+
+    def accept
+      @status = :completed
+    end 
   end
 end
--- spec/purchase_request_spec.rb
+++ spec/purchase_request_spec.rb
@@ -1,11 +1,29 @@
 require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::PurchaseRequest do
-  it "creates a new purchase request for a breed with a status of pending" do
-    request = PuppyBreeder::PurchaseRequest.new("Golden Retriever")
+  describe '.initialize' do
+    it "creates a new purchase request for a breed with a status of pending" do
+      request = PuppyBreeder::PurchaseRequest.new("Golden Retriever")
 
-    expect(request.breed).to eq("Golden Retriever")
-    expect(request.status).to eq(:pending)
+      expect(request.breed).to eq("Golden Retriever")
+      expect(request.status).to eq(:pending)
+    end
+
+    it "creates a unique id number for each purchase request" do
+      request1 = PuppyBreeder::PurchaseRequest.new("Golden Retriever")
+      request2 = PuppyBreeder::PurchaseRequest.new("Pitbull")
+
+      request1.id.should_not eq(request2.id)
+    end
+  end
+
+  describe '.accept' do
+    it "changes the purchase request status from pending to completed" do
+      request = PuppyBreeder::PurchaseRequest.new("Golden Retriever")
+      request.accept
+
+      expect(request.status).to eq(:completed)
+    end
   end
 
 end
