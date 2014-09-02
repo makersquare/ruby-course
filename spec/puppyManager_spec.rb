7495115191b08c8e68800cfc0c126727ec2a7113
Daniel Burnette <dcburnette999@gmail.com>
2014-09-02 12:03:42 -0500

    completed add_puppy_for_sale test, passed

--- spec/puppyManager_spec.rb
+++ spec/puppyManager_spec.rb
@@ -2,4 +2,18 @@ require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::PuppyManager do
 
+  describe '.add_puppy_for_sale' do
+
+    it 'pushes the given puppy argument into the PuppyManager @@puppies_for_sale array' do
+
+      puppy = PuppyBreeder::Puppy.new("Spot", "golden retriever", 1)
+
+      PuppyBreeder::PuppyManager.add_puppy_for_sale(puppy)
+
+      expect(PuppyBreeder::PuppyManager.puppies_for_sale.include?(puppy)).to eq(true)
+    end
+
+
+  end
+
 end
