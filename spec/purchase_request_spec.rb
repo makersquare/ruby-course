569ab55316ffb3645056d35dd86f8d8ece6146cc
Yusef Ouda <yiouda92@gmail.com>
2014-09-02 10:44:49 -0500

    Added extra test case for initialize in PurchaseRequest class

--- spec/purchase_request_spec.rb
+++ spec/purchase_request_spec.rb
@@ -2,6 +2,11 @@ require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::PurchaseRequest do
   describe '#initialize' do
+    it 'creates a PuppyBreeder::PurchaseRequest object' do
+      purchase_req = PuppyBreeder::PurchaseRequest.new("Greyhound")
+
+      expect(purchase_req).to be_an_instance_of(PuppyBreeder::PurchaseRequest)
+    end
     it 'initializes a purchase request for a given breed' do
       purchase_req = PuppyBreeder::PurchaseRequest.new("Greyhound")
 
