23aba8e1581fda840d7cba40c3968536dc8efcc3
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2014-09-02 15:15:04 -0500

    The spec files

--- spec/purchase_request_spec.rb
+++ spec/purchase_request_spec.rb
@@ -1,5 +1,11 @@
 require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::PurchaseRequest do
+  it "creates a purchase request with a readable user and breed" do
+    test_request = PuppyBreeder::PurchaseRequest.new("James","boxer")
+    expect(test_request).to be_a(PuppyBreeder::PurchaseRequest)
+    expect(test_request.customer).to eq("James")
+    expect(test_request.breed).to eq("boxer")
+  end
 
 end
