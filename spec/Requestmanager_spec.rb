c00a0556a8b6e003d770a8503c7334acc1419432
Daniel Burnette <dcburnette999@gmail.com>
2014-09-02 12:31:24 -0500

    finish add_request test, passed

--- spec/Requestmanager_spec.rb
+++ spec/Requestmanager_spec.rb
@@ -2,4 +2,24 @@ require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::RequestManager do
 
+  describe '.add_request' do
+
+    it 'adds the given purchase_request argument to the RequestManagers @@open_requests array' do
+
+      request = PuppyBreeder::PurchaseRequest.new("afghan hound")
+
+      #Clear all requests for testing purposes
+      PuppyBreeder::RequestManager.clear_all_requests
+      PuppyBreeder::RequestManager.add_request(request)
+
+      expect(PuppyBreeder::RequestManager.view_open_requests).to eq([request])
+
+    end
+
+  end
+
+  describe ''
+
+
+
 end
