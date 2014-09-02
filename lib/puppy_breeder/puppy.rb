4a5469b20a2dcc963ab8ea0f060a923e2b6333e9
Jimmy Pocock <jimmypocock@yahoo.com>
2014-09-02 10:47:08 -0500

    Initialized Puppy class. All tests passed

--- lib/puppy_breeder/puppy.rb
+++ lib/puppy_breeder/puppy.rb
@@ -1,6 +1,26 @@
 #Refer to this class as PuppyBreeder::Puppy
 module PuppyBreeder
   class Puppy
+    attr_accessor :cost, :sold_status
+    attr_reader :breed, :name, :age, :puppy_id
+    @@puppy_counter = 0
+    @@breed_costs = {
+      "French Bulldog" => 1500,
+      "Shih Tzu" => 500,
+      "Boxer" => 750,
+      "Golden Retriever" => 2000,
+      "Dachshund" => 400
+    }
+
+    def initialize(breed, name, age, sold_status="Available")
+      @breed = breed
+      @name = name
+      @age = age
+      @cost = @@breed_costs[breed]
+      @sold_status = sold_status
+      @@puppy_counter += 1
+      @puppy_id = @@puppy_counter
+    end
 
   end
 end
--- spec/puppy_spec.rb
+++ spec/puppy_spec.rb
@@ -1,5 +1,28 @@
 require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::Puppy do
+  it "Creates an instance of Puppy Class. Creates instance variables for breed, cost, sold_status, and puppy_id." do
+
+    puppy = PuppyBreeder::Puppy.new("French Bulldog", "Murphy", 0.5)
+
+    expect(puppy.breed).to eq("French Bulldog")
+    expect(puppy.name).to eq("Murphy")
+    expect(puppy.age).to eq(0.5)
+    expect(puppy.cost).to eq(1500)
+    expect(puppy.sold_status).to eq("Available")
+    expect(puppy.puppy_id).to eq(1)
+    expect(puppy.class).to eq(PuppyBreeder::Puppy)
+
+    puppy2 = PuppyBreeder::Puppy.new("Boxer", "Sadie", 1)
+
+    expect(puppy2.breed).to eq("Boxer")
+    expect(puppy2.name).to eq("Sadie")
+    expect(puppy2.age).to eq(1)
+    expect(puppy2.cost).to eq(750)
+    expect(puppy2.sold_status).to eq("Available")
+    expect(puppy2.puppy_id).to eq(2)
+    expect(puppy2.class).to eq(PuppyBreeder::Puppy)
+
+  end
 
 end
