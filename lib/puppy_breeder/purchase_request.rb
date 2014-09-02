a7f1e1b85c3d5fc974d55625586fc404ee19bddd
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2014-09-02 10:37:15 -0500

    Basic purchase request

--- lib/puppy_breeder/purchase_request.rb
+++ lib/puppy_breeder/purchase_request.rb
@@ -1,6 +1,10 @@
 #Refer to this class as PuppyBreeder::PurchaseRequest
 module PuppyBreeder
   class PurchaseRequest
-
+    attr_reader :customer,:breed
+    def initialize(customer,breed)
+      @customer = customer
+      @breed = breed
+    end
   end
 end
