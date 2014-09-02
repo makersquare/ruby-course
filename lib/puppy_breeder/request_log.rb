bda75f613e91ec0e19d3b204ce4982609de6cf86
Nick Damiano <damiano.nick@gmail.com>
2014-09-02 14:35:45 -0500

    added request_log.rb additional method outlines for review requests, view all, view rejected, and purge. added the spec outlines for those with comment descriptions in the spec file.

--- lib/puppy_breeder/request_log.rb
+++ lib/puppy_breeder/request_log.rb
@@ -8,5 +8,21 @@ module PuppyBreeder
     def add_request(request)
       @request_log.push(request)
     end
+
+    def review_requests
+      #find and return the requests that have status of pending
+    end
+    def view_all
+      #return all requests in the log including pending, rejected,
+      #and approved
+
+    end
+    def view_rejected
+      #return only rejected requests from the log
+    end
+    def purge_log
+      #erase the contents of the log or possibly do this for only 
+      #rejected items
+    end
   end
 end
--- spec/request_log_spec.rb
+++ spec/request_log_spec.rb
@@ -15,4 +15,25 @@ describe PuppyBreeder::RequestLog do
     expect(log.request_log.first).to eq(request)
 
   end
+
+  xit 'gives the first pending request for approval' do
+    #sort through the request log array for pending status
+    #get the first one
+    #approves or rejects 
+    #changes the status of the request
+  end
+
+  xit 'gives a list of all requests in the log' do
+    #returns the request log array
+  end
+
+  xit 'gives the rejected requests from the log' do
+    #returns the rejected status requests
+  end
+
+  xit 'purges the log' do
+    #erases the log and empties the array or maybe it just archives it 
+    #so the current log gets pushed into a separate array with all the 
+    #other archived logs, and the request log returns to an empty state. 
+  end
 end
