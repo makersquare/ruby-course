b0de991baa34195761c6bea853a509ca8d7f8842
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2014-09-02 09:49:01 -0500

    Basic puppy object

--- lib/puppy_breeder/puppy.rb
+++ lib/puppy_breeder/puppy.rb
@@ -1,6 +1,13 @@
 #Refer to this class as PuppyBreeder::Puppy
 module PuppyBreeder
   class Puppy
+    attr_accessor :age_in_days
+    attr_reader :name, :breed 
 
+    def initialize(breed: nil,name: nil,age_in_days:nil)
+      @breed = breed
+      @name = name
+      @age_in_days = age_in_days
+    end
   end
 end
