d551354121223542241e7cd5935fd688b5003a27
Matteo Wiley <matteowiley@gmail.com>
2014-09-02 12:52:14 -0500

    This takes you you throught both puppy and puppycontainer class. working on other parts a bit though

--- lib/puppy_breeder/puppy.rb
+++ lib/puppy_breeder/puppy.rb
@@ -1,6 +1,39 @@
 #Refer to this class as PuppyBreeder::Puppy
 module PuppyBreeder
   class Puppy
+    attr_accessor :name, :breed, :age, :puppy_id
+    @@counter = 0
+    def initialize(name, breed, age)
+      @name = name
+      @breed = breed
+      @age = age
+      @puppy_id = @@counter += 1
+    end 
+
+  end
+
+  class Puppycontainer
+    
+    def initialize(puppy)
+      @puppy = puppy
+      @pound = Hash.new(0)
+    end
+    
+    def add_stats(breed, cost)
+      @pound[breed] = {
+          :cost => cost,
+          :available => []
+        }
+    end
+
+    def add_pup_pound(puppy)
+      @pound[puppy.breed][:available] << puppy
+    end
+    
+    def pound
+      @pound
+    end
 
   end
 end
+
--- lib/puppy_breeder/purchase_request.rb
+++ lib/puppy_breeder/purchase_request.rb
@@ -1,6 +1,19 @@
 #Refer to this class as PuppyBreeder::PurchaseRequest
 module PuppyBreeder
   class PurchaseRequest
+    attr_accessor :breed, :purchaser_id
+    def initialize(breed)
+      @counter = 0
+      @breed = breed
+      @purchaser_id = @counter += 1
+    end
+
+  end
+
+  class Purchasecontainer
+    def initialize(request_id)
+      @request_id = request_id
+    end
 
   end
 end
