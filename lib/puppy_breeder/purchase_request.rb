39a1acc0a152bea3bd4e6c9b589d94885bd42d69
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2014-09-02 10:48:29 -0500

    Create failing test to initialize PurchaseRequest class with a pending status and a breed. Write initialize method to make failing test pass.

--- lib/puppy_breeder/purchase_request.rb
+++ lib/puppy_breeder/purchase_request.rb
@@ -1,6 +1,11 @@
 #Refer to this class as PuppyBreeder::PurchaseRequest
 module PuppyBreeder
   class PurchaseRequest
+    attr_reader :breed, :status
 
+    def initialize(breed)
+      @breed = breed
+      @status = :pending
+    end
   end
 end
--- spec/purchase_request_spec.rb
+++ spec/purchase_request_spec.rb
@@ -1,5 +1,11 @@
 require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::PurchaseRequest do
+  it "creates a new purchase request for a breed with a status of pending" do
+    request = PuppyBreeder::PurchaseRequest.new("Golden Retriever")
+
+    expect(request.breed).to eq("Golden Retriever")
+    expect(request.status).to eq(:pending)
+  end
 
 end
