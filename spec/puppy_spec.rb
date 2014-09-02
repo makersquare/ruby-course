cd97df9c43664493bf0c22ebe170378785f88342
Daniel Burnette <dcburnette999@gmail.com>
2014-09-02 11:31:42 -0500

    complete puppy_spec tests, passing

--- spec/puppy_spec.rb
+++ spec/puppy_spec.rb
@@ -2,4 +2,17 @@ require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::Puppy do
 
+  describe '.initialize' do
+
+    it 'sets the name, breed, and age' do
+      puppy = PuppyBreeder::Puppy.new("Bowser", "golden retriever", 0.5)
+      expect(puppy.name).to eq("Bowser")
+      expect(puppy.breed).to eq("golden retriever")
+      expect(puppy.age).to eq(0.5)
+    end
+
+  end
+
+
+
 end
