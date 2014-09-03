5aab820f187eb69b67875fd9e77638770988d643
Nick Damiano <damiano.nick@gmail.com>
2014-09-03 15:05:05 -0500

    actually the last request changed the inventory and THIS one changes the request log to reflect class variables and methods.

--- lib/puppy_breeder/request_log.rb
+++ lib/puppy_breeder/request_log.rb
@@ -2,31 +2,31 @@ module PuppyBreeder
   class RequestLog
     attr_reader :request_log, :pending
     def initialize
-      @request_log = []
-      @hold_log
+      @@request_log = []
+      @@hold_log
     end
 
-    def add_request(request)
+    def self.add_request(request)
       if Inventory.puppies[request.breed][list.length] >= 1
-      @request_log.push(request)
+      @@request_log.push(request)
     else
-      @hold_log.push(request)
+      @@hold_log.push(request)
     end
   end
 
-    def review_pending
+    def self.eview_pending
       #prints out a list of the information regarding the request
-      pending = @request_log.select{|request| request.status=="pending"}
+      pending = @@request_log.select{|request| request.status=="pending"}
     end
 
-    def modify_request(request_id, decision) #takes the request id and 
+    def self.modify_request(request_id, decision) #takes the request id and 
       #whether the request is approved or denied
-      found_request = @request_log.select{|request| request.request_id == request_id}      
+      found_request = @@request_log.select{|request| request.request_id == request_id}      
       found_request.first.status = decision
     end
 
-    def view_completed
-      completed_requests = @request_log.select{|request| request.status !="pending"}
+    def self.view_completed
+      completed_requests = @@request_log.select{|request| request.status !="pending"}
     end
   end
 end
