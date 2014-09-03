82bb7aeb6624a160a06221a666c9b67a7b4c3fa4
Travis Hooper <thooper41@gmail.com>
2014-09-02 22:11:41 -0500

    updating Gemfile, and spec files that weren't included with last commit

--- spec/puppy_spec.rb
+++ spec/puppy_spec.rb
@@ -1,5 +1,38 @@
 require_relative 'spec_helper.rb'
 
-describe PuppyBreeder::Puppy do
+describe 'PuppyBreeder::Puppy' do
 
+  it "A new puppy can be created with name, breed and age" do
+    benson = PuppyBreeder::Puppy.new("Benson", "mut", 5)
+
+    #binding.pry
+
+    expect(benson.name).to eq("Benson")
+    expect(benson.age).to eq(5)
+    expect(benson.breed).to eq('mut')
+  end
+end
+
+describe 'PuppyBreeder::Breeder' do
+  it 'Allows a breeder to add a puppy and assign a price' do
+    benson = PuppyBreeder::Puppy.new("Benson", "mut", 5)
+    travis = PuppyBreeder::Breeder.new('Travis')
+    travis.new_puppy(benson)
+
+    expect(travis.name).to eq 'Travis'
+    expect(travis.puppies.first.name).to eq 'Benson'
+    expect(travis.puppies.first.price).to eq(50)
+  end
+
+  it 'Allows a breeder to add a price to a new breed of puppy and update the price on an existing breed' do
+    benson = PuppyBreeder::Puppy.new("Benson", "mut", 5)
+    travis = PuppyBreeder::Breeder.new('Travis')
+    travis.new_puppy(benson)
+    travis.update_price('black lab', 100)
+    travis.update_price('mut', 25)
+    molly = PuppyBreeder::Puppy.new('Molly', "black lab", 2)
+    travis.new_puppy(molly)
+    expect(benson.price).to eq(25)
+    expect(molly.price).to eq(100)
+  end
 end
--- spec/purchase_request_spec.rb
+++ spec/purchase_request_spec.rb
@@ -1,5 +1,40 @@
 require_relative 'spec_helper.rb'
 
-describe PuppyBreeder::PurchaseRequest do
+describe 'PuppyBreeder::PurchaseRequest' do
+  
+  it "allows a customer to submit a purchase request for a specific breed for review by breeder" do
+    benson = PuppyBreeder::Puppy.new("Benson", "mut", 5)
+    travis = PuppyBreeder::Breeder.new('Travis')
+    travis.new_puppy(benson)
+    john = PuppyBreeder::PurchaseRequest.new('John')
+    john.request_dog('mut', travis)
+    expect(travis.purchase_requests[john]).to eq(benson)
+  end
+
+
+  it "allows the breeder to a sell a dog to a customer with a pending purchase request" do
+    benson = PuppyBreeder::Puppy.new("Benson", "mut", 5)
+    travis = PuppyBreeder::Breeder.new('Travis')
+    travis.new_puppy(benson)
+    john = PuppyBreeder::PurchaseRequest.new('John Smith')
+    john.request_dog('mut', travis)
+    travis.sell_dog(benson)
+
+    expect(benson.status).to eq 'sold to John Smith'
+    expect(travis.bank_account).to eq(50)
+  end
+
+  it "allows the breeder to view all puppy sales" do
+    benson = PuppyBreeder::Puppy.new("Benson", "mut", 5)
+    travis = PuppyBreeder::Breeder.new('Travis')
+    travis.new_puppy(benson)
+    molly = PuppyBreeder::Puppy.new('Molly', "black lab", 2)
+    travis.new_puppy(molly)
+    john = PuppyBreeder::PurchaseRequest.new('John Smith')
+    john.request_dog('mut', travis)
+    travis.sell_dog(benson)
+
+    expect(travis.view_sales.first).to eq(benson)
+  end
 
 end
