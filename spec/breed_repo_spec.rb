035477befb7f72a55dda90b8b5d421b7194485e1
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2014-09-05 15:03:42 -0500

    Create test for add breed method.

--- spec/breed_repo_spec.rb
+++ spec/breed_repo_spec.rb
@@ -18,4 +18,25 @@ describe PuppyBreeder::Repos::Breeds do
       expect(result.first.class).to eq(PuppyBreeder::Breed)
     end 
   end
+
+  describe '.add_breed' do
+    it 'adds a breed into the database' do
+      golden = PuppyBreeder::Breed.new("Golden Retriever", 800)
+      pitbull = PuppyBreeder::Breed.new("Pitbull", 500)
+      breeds = PuppyBreeder::Repos::Breeds.new
+      breeds.add_breed(golden)
+      result = breeds.log
+
+      expect(result.size).to eq 1
+      expect(result.first.price).to eq "$800.00"
+      expect(result.first.breed).to eq "Golden Retriever"
+
+      breeds.add_breed(pitbull)
+      result = breeds.log
+
+      expect(result.size).to eq 2
+      expect(result.last.price).to eq "$500.00"
+      expect(result.last.breed).to eq "Pitbull"
+    end
+  end
 end
