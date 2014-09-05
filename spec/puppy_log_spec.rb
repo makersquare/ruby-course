926c57ce714e9875201cc9684a622933f4c89524
Yusef Ouda <yiouda92@gmail.com>
2014-09-05 13:07:42 -0500

    Changed tests so that the tables are dropped before each spec file is run

--- spec/puppy_log_spec.rb
+++ spec/puppy_log_spec.rb
@@ -2,11 +2,14 @@ require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::Repos::PuppyLog do
 
+  before(:all) do
+    PuppyBreeder.puppy_repo.drop_tables
+  end
+
   describe '#log' do
     it 'returns array of puppy objects' do
       puppy = PuppyBreeder::Puppy.new("Fido", "Poodle", 23)
       PuppyBreeder.puppy_repo.add_puppy(puppy)
-
       result = PuppyBreeder.puppy_repo.log.first
       expect(result).to be_an_instance_of(PuppyBreeder::Puppy)
     end
@@ -14,7 +17,6 @@ describe PuppyBreeder::Repos::PuppyLog do
 
   describe '#add_puppy' do
     it 'adds a puppy to the data structure' do
-      PuppyBreeder.puppy_repo.drop_tables
       puppy1 = PuppyBreeder::Puppy.new("Fido", "Greyhound", 30)
       puppy2 = PuppyBreeder::Puppy.new("Jack", "Great Dane", 25)
       puppy3 = PuppyBreeder::Puppy.new("Jill", "Husky", 50)
@@ -26,7 +28,7 @@ describe PuppyBreeder::Repos::PuppyLog do
       res = PuppyBreeder.puppy_repo.log.select { |x| x.name == "Fido" }.first.name
       expect(res).to eq(puppy1.name)
       result = PuppyBreeder.puppy_repo.log.size
-      expect(result).to eq(3)
+      expect(result).to eq(4)
     end
   end
 end
