1d8e797dc38867bfbdf6ee99dfb21623bf58dda2
Daniel Burnette <dcburnette999@gmail.com>
2014-09-05 09:39:00 -0500

    refactored puppymanager spec

--- spec/puppyManager_spec.rb
+++ spec/puppyManager_spec.rb
@@ -2,30 +2,33 @@ require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::PuppyManager do
 
+  request_manager = PuppyBreeder.request_repo
+  puppy_manager = PuppyBreeder.puppy_repo
+
   describe '.add_puppy_for_sale' do
 
     it 'pushes the given puppy argument into the PuppyManager @@puppies_for_sale array' do
 
       puppy = PuppyBreeder::Puppy.new("Spot", "golden retriever", 1)
 
-      PuppyBreeder::PuppyManager.add_puppy_for_sale(puppy)
+      puppy_manager.add_puppy_for_sale(puppy)
 
-      expect(PuppyBreeder::PuppyManager.puppies_for_sale.include?(puppy)).to eq(true)
+      expect(puppy_manager.puppies_for_sale.include?(puppy)).to eq(true)
     end
 
     it 'checks Request Managers @@held_requests automatically and approves that request if a matching puppy was found' do
 
-      PuppyBreeder::RequestManager.clear_all_requests
-      PuppyBreeder::PuppyManager.clear_puppies
+      request_manager.clear_all_requests
+      puppy_manager.clear_puppies
 
       request = PuppyBreeder::PurchaseRequest.new("golden retriever")
-      PuppyBreeder::RequestManager.approve_request(request)
+      request_manager.approve_request(request)
 
       puppy = PuppyBreeder::Puppy.new("Spot", "golden retriever", 1)
-      PuppyBreeder::PuppyManager.add_puppy_for_sale(puppy)
-      PuppyBreeder::RequestManager.approve_request(request)
+      puppy_manager.add_puppy_for_sale(puppy)
+      request_manager.approve_request(request)
 
-      expect(PuppyBreeder::PuppyManager.puppies_for_sale.include?(puppy)).to eq(false)
+      expect(puppy_manager.puppies_for_sale.include?(puppy)).to eq(false)
   end
 
   end
@@ -36,10 +39,10 @@ describe PuppyBreeder::PuppyManager do
 
       puppy = PuppyBreeder::Puppy.new("Spot", "golden retriever", 1)
 
-      PuppyBreeder::PuppyManager.add_puppy_for_sale(puppy)
-      PuppyBreeder::PuppyManager.remove_puppy_for_sale(puppy)
+      puppy_manager.add_puppy_for_sale(puppy)
+      puppy_manager.remove_puppy_for_sale(puppy)
 
-      expect(PuppyBreeder::PuppyManager.puppies_for_sale.include?(puppy)).to eq(false)
+      expect(puppy_manager.puppies_for_sale.include?(puppy)).to eq(false)
 
 
     end
@@ -55,12 +58,12 @@ describe PuppyBreeder::PuppyManager do
       puppy2 = PuppyBreeder::Puppy.new("Bowser", "golden retriever", 0.5)
 
       #Clear all remnant puppies for test accuracy
-      PuppyBreeder::PuppyManager.clear_puppies
+      puppy_manager.clear_puppies
 
-      PuppyBreeder::PuppyManager.add_puppy_for_sale(puppy1)
-      PuppyBreeder::PuppyManager.add_puppy_for_sale(puppy2)
+      puppy_manager.add_puppy_for_sale(puppy1)
+      puppy_manager.add_puppy_for_sale(puppy2)
 
-      expect(PuppyBreeder::PuppyManager.puppies_for_sale).to eq([puppy1, puppy2])
+      expect(puppy_manager.puppies_for_sale).to eq([puppy1, puppy2])
 
 
     end
@@ -74,11 +77,11 @@ describe PuppyBreeder::PuppyManager do
       puppy1 = PuppyBreeder::Puppy.new("Spot", "golden retriever", 1)
       puppy2 = PuppyBreeder::Puppy.new("Bowser", "golden retriever", 0.5)
 
-      PuppyBreeder::PuppyManager.add_puppy_for_sale(puppy1)
-      PuppyBreeder::PuppyManager.add_puppy_for_sale(puppy2)
+      puppy_manager.add_puppy_for_sale(puppy1)
+      puppy_manager.add_puppy_for_sale(puppy2)
 
-      PuppyBreeder::PuppyManager.clear_puppies
-      expect(PuppyBreeder::PuppyManager.puppies_for_sale).to eq([])
+      puppy_manager.clear_puppies
+      expect(puppy_manager.puppies_for_sale).to eq([])
       
 
     end
@@ -91,12 +94,12 @@ describe PuppyBreeder::PuppyManager do
       puppy1 = PuppyBreeder::Puppy.new("Spot", "golden retriever", 1)
       puppy2 = PuppyBreeder::Puppy.new("Bowser", "golden retriever", 0.5)
 
-      PuppyBreeder::PuppyManager.add_puppy_for_sale(puppy1)
-      PuppyBreeder::PuppyManager.add_puppy_for_sale(puppy2)
+      puppy_manager.add_puppy_for_sale(puppy1)
+      puppy_manager.add_puppy_for_sale(puppy2)
 
       request = PuppyBreeder::PurchaseRequest.new("golden retriever")
 
-      expect(PuppyBreeder::PuppyManager.find_match(request)).to eq(puppy1)
+      expect(puppy_manager.find_match(request)).to eq(puppy1)
     end
 
   end
