8a2650b06739b1112f89050593fc0c334cb01270
Gilbert <gilbertbgarza@gmail.com>
2014-09-07 23:02:53 -0500

    ItemsController: create and show

--- web/app/controllers/items_controller.rb
+++ web/app/controllers/items_controller.rb
@@ -6,9 +6,22 @@ class ItemsController < ApplicationController
   end
 
   def create
+    tx_params = item_params.merge :session_id => $session_id
+    tx_params[:price] = tx_params[:price].to_f
+    result = DoubleDog::CreateItem.new.run(tx_params)
+
+    if result[:success?]
+      redirect_to item_path(result[:item].id)
+    else
+      @items = DoubleDog.db.all_items
+      @item = DoubleDog::Item.new(nil, tx_params[:name], tx_params[:price])
+      @error = result[:error]
+      render 'index'
+    end
   end
 
   def show
+    @item = DoubleDog.db.get_item  params[:id].to_i
   end
 
   def edit
@@ -21,6 +34,10 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def item_params
+    params.require(:item).permit(:name, :price)
+  end
 
   def authenticate_admin!
     if current_user.nil? || !current_user.admin?
