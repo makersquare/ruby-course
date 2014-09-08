93642c41643502698ba75fddf14fb348e2ecc3ed
Gilbert <gilbertbgarza@gmail.com>
2014-09-07 22:45:08 -0500

    Link to items dashboard if admin

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    # TODO: Change this to session[:app_session_id]
+    if $session_id
+      @_current_user ||= DoubleDog.db.get_user_by_session_id($session_id)
+    end
+  end
+
+  helper_method :current_user
 end
