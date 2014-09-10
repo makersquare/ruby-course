3fe85ad558a0295ed2b18ed5e30d680d18c7bcb0
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2014-09-10 12:48:47 -0500

    Create new erb files for each endpoint. Write each get/post request and html in the erb files.

--- web/server.rb
+++ web/server.rb
@@ -9,10 +9,27 @@ class Songify::Server < Sinatra::Application
     erb :index
   end
 
-  post '/show' do
-    "Test"
+  get '/show' do
+    @songs = Songify.songs_repo.get_all_songs
+    erb :show
   end
 
+  get '/new' do
+    erb :new
+  end
+
+  post '/create' do
+    song = Songify::Song.new(params["title"], params["artist"], params["album"], params["genre"], params["length"])
+    Songify.songs_repo.save_song(song)
+    erb :create
+  end
 
   run! if __FILE__ == $0
 end
+
+# <% song = Songify.songs_repo.get_song(song_id) %>
+#   <%= song.title %>
+#   <%= song.artist %>
+#   <%= song.album %>
+#   <%= song.genre %>
+#   <%= song.length %>
