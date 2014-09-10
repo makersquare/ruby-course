b112006df73795079f8d452c59e55ba2750a5685
Jimmy Pocock <jimmypocock@yahoo.com>
2014-09-10 11:53:07 -0500

    Updated public folder. Added bootstrap files for sense of completion. Updated routes for css and images.

--- web/server.rb
+++ web/server.rb
@@ -1,8 +1,10 @@
-require_relative 'lib/songify.rb'
+require_relative '../lib/songify.rb'
 
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry-byebug'
+
+set :bind, '0.0.0.0'
 
 get '/' do
   erb :index
