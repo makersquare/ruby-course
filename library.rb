ad1a9ea944be2c399ce2272b415bf368c65fbf75
David Coleman <colemandavid55@gmail.com>
2014-12-02 13:26:47 -0600

    alter check_out method for book class

--- library.rb
+++ library.rb
@@ -9,7 +9,12 @@ class Book
   end
 
   def check_out
-    @status = "checked_out"
+    if @status == "available"
+      @status = "checked_out"
+      return true
+    else 
+      return false
+    end
   end
 end
 
