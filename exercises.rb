09dfd925af7ba30f938d5d82eacf3813c94b83fe
greg <gmikeska07@gmail.com>
2014-12-03 08:58:47 -0600

    implements and satisfies the test for exercise 7

--- exercises.rb
+++ exercises.rb
@@ -66,7 +66,10 @@ module Exercises
   #  - If the string `str` exists in the array,
   #    add `str` to the end of the array
   def self.ex7(array, str)
-    # TODO
+    if array.include? str
+      array.push(str)
+    end
+    return array
   end
 
   # Exercise 8
