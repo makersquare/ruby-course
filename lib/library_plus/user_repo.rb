13861e4e22569094dcac6468c48275aa89bde5c6
charles milam <charles.milam@gmail.com>
2014-12-05 16:25:58 -0600

    Modify save to add new user

--- lib/library_plus/user_repo.rb
+++ lib/library_plus/user_repo.rb
@@ -12,11 +12,32 @@ module Library
     end
 
     def self.save(db, user_data)
-      if user_data['id']
-        # TODO: Update SQL statement
+      if user_data["id"]
+        sql_users = %Q[
+          SELECT *
+          FROM users
+          WHERE id = user[:id]
+        ]
+        db.exec sql
       else
-        # TODO: Insert SQL statement
+        # prepared statement
+        statement_insert_user = %Q[
+          insert into users 
+          (name)
+          values ($1)
+        ]
+        db.prepare("insert_user", statement_insert_user)
+        # execute prepared statement
+        db.exec_prepared("insert_user", [user_data[:name]])
       end
+      # grab the user via query and return it
+      sql_users = %Q[
+          SELECT *
+          FROM users
+          WHERE name = $1
+        ]
+      users = db.exec(sql_users, [user_data[:name]])
+      users.first
     end
 
     def self.destroy(db, user_data)
