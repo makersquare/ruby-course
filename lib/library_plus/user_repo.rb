8edacf27e541309ac4de4f183588ba39e558b074
David Coleman <colemandavid55@gmail.com>
2014-12-05 11:10:45 -0600

    commit before new pull request to fix some test typos

--- lib/library_plus/user_repo.rb
+++ lib/library_plus/user_repo.rb
@@ -8,19 +8,28 @@ module Library
       db.exec("SELECT * FROM users").to_a
     end
 
-    def self.find(db, user_data)
+    def self.find(db, user_id)
       # TODO: Insert SQL statement
+      id = user_id['id']
+      db.exec("SELECT * FROM users WHERE id = $4", [id])
     end
 
     def self.save(db, user_data)
       if user_data['id']
         # TODO: Update SQL statement
+        name = user_data['name']
+        id = user_data['id']
+        db.exec("UPDATE movies SET name = $1 WHERE id = $2", [name, id])
+        new_data = Library::UserRepo.find(db, id)
+
       else
         # TODO: Insert SQL statement
+        name = user_data['name']
+        db.exec("INSERT into users (name) VALUES ($1)", [name])
       end
     end
 
-    def self.destroy(db, user_data)
+    def self.destroy(db, user_id)
       # TODO: Delete SQL statement
     end
 
