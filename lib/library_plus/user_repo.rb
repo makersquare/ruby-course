d3432c83d421f5dd42c469e507a3436a3fb8eee8
Spenser Filler <spenserfiller@gmail.com>
2014-12-05 14:53:37 -0600

    Implemnts search, create, and update user

--- lib/library_plus/user_repo.rb
+++ lib/library_plus/user_repo.rb
@@ -8,15 +8,23 @@ module Library
     end
 
     def self.find(db, user_id)
+      result = db.exec("SELECT * FROM users WHERE id = $1;", [user_id])
+      result.first
       # TODO: Insert SQL statement
     end
 
     def self.save(db, user_data)
       if user_data['id']
+        db.exec("UPDATE users SET name = $1 WHERE id = $2;",[user_data['name'], user_data['id']])
         # TODO: Update SQL statement
       else
+        db.exec("INSERT INTO users (name) VALUES ($1);", [user_data['name']])
+        user_data['id']
         # TODO: Insert SQL statement
       end
+      result = db.exec("SELECT * FROM users WHERE name = $1;", [user_data['name']])
+      result.first
+
     end
 
     def self.destroy(db, user_id)
--- server.rb
+++ server.rb
@@ -6,3 +6,16 @@ require './lib/library_plus'
 get '/' do
   erb :index
 end
+
+get '/books' do
+  erb :"books/index"
+end
+
+get '/books' do
+  #Create a new books
+  erb :"book/show"
+end
+
+get '/books/:id' do
+  erb :"books/show"
+end
