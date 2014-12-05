1b9703c99d3e754a7d2e70ded05e16750a4528dd
Jason Hatchett <jason@hatchett.net>
2014-12-05 11:17:22 -0600

    My changes

--- lib/library_plus/user_repo.rb
+++ lib/library_plus/user_repo.rb
@@ -14,8 +14,10 @@ module Library
     def self.save(db, user_data)
       if user_data['id']
         # TODO: Update SQL statement
+        db.exec_params("UPDATE users SET name = $2 WHERE id= $1 ",[user_data["id"],user_data[:name]])
       else
         # TODO: Insert SQL statement
+        db.exec_params("INSERT INTO users (name) VALUES ($1)",[user_data[:name]])
       end
     end
 
