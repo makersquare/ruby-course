1a675ca9c40c2933cb3c80311d70430426c4a732
Melissa Patterson <melissa.patterson.va@gmail.com>
2014-12-06 18:34:02 -0600

    add user post function

--- server.rb
+++ server.rb
@@ -1,8 +1,29 @@
 require 'sinatra'
 require './lib/library_plus'
 
-# set :bind, '0.0.0.0' # This is needed for Vagrant
+set :bind, '0.0.0.0' # This is needed for Vagrant
+
+
 
 get '/' do
   erb :index
 end
+
+get '/users' do
+  db = Library.create_db_connection('library_dev')
+  @users = Library::UserRepo.all(db)
+  erb :'users/index'
+end
+
+post '/users/create' do
+  db = Library.create_db_connection('library_dev')
+  Library::UserRepo.save(db, params)
+  redirect '/users'
+end
+
+get '/books' do
+  db = Library.create_db_connection('library_dev')
+  @books = Library::BookRepo.all(db)
+  erb :'books/index'
+end
+   
