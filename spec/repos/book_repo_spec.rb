ec3abfecc610e75a1aaa7ba2235614a02ecde297
charles milam <charles.milam@gmail.com>
2014-12-06 22:00:37 -0600

    Initial commit.

---
+++ spec/repos/book_repo_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+
+describe Library::BookRepo do
+
+  def user_count(db)
+    db.exec("SELECT COUNT(*) FROM users")[0]["count"].to_i
+  end
+
+  let(:db) { Library.create_db_connection('library_test') }
+
+  before(:each) do
+    Library.clear_db(db)
+  end
+
+  it "gets all users" do
+    db.exec("INSERT INTO users (name) VALUES ($1)", ["Alice"])
+    db.exec("INSERT INTO users (name) VALUES ($1)", ["Bob"])
+
+    users = Library::BookRepo.all(db)
+    expect(users).to be_a Array
+    expect(users.count).to eq 2
+
+    names = users.map {|u| u['name'] }
+    expect(names).to include "Alice", "Bob"
+  end
+
+  it "creates users" do
+    expect(user_count(db)).to eq 0
+
+    user = Library::BookRepo.save(db, :name => "Alice")
+    expect(user['id']).to_not be_nil
+    expect(user['name']).to eq "Alice"
+
+    # Check for persistence
+    expect(user_count(db)).to eq 1
+
+    user = db.exec("SELECT * FROM users")[0]
+    expect(user['name']).to eq "Alice"
+  end
+
+  it "finds users" do
+    user = Library::BookRepo.save(db, :name => "Alice")
+    retrieved_user = Library::BookRepo.find(db, user['id'])
+    expect(retrieved_user['name']).to eq "Alice"
+  end
+
+  it "updates users" do
+    user1 = Library::BookRepo.save(db, :name => "Alice")
+    user2 = Library::BookRepo.save(db, { :id => user1['id'], :name => "Alicia" })
+    
+    expect(user2['id']).to eq(user1['id'])
+    expect(user2['name']).to eq "Alicia"
+
+    # Check for persistence
+    user3 = Library::BookRepo.find(db, user1['id'])
+    expect(user3['name']).to eq "Alicia"
+  end
+
+  it "destroys users" do
+    user = Library::BookRepo.save(db, :name => "Alice")
+    expect(user_count(db)).to eq 1
+
+    Library::BookRepo.destroy(db, user['id'])
+    expect(user_count(db)).to eq 0
+  end
+end
