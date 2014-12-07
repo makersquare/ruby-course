5ee566c270f7927d2da41af167807e9789879ca5
Melissa Patterson <melissa.patterson.va@gmail.com>
2014-12-07 17:25:39 -0600

    add layout

--- server.rb
+++ server.rb
@@ -31,4 +31,36 @@ post '/books/create' do
   db = Library.create_db_connection('library_dev')
   Library::BookRepo.save(db, params)
   redirect '/books'
-end   
+end 
+
+get '/books/:id' do
+  db = Library.create_db_connection('library_dev')
+  book_data = Library::BookRepo.find(db, params[:id])
+  @title = book_data['title']
+  @author = book_data['author']
+  @status = book_data['status']
+  @id = book_data['id']
+  @users = Library::UserRepo.all(db)
+  puts book_data['borrower_id']
+  if book_data['borrower_id'] != nil
+    user = Library::UserRepo.find(db, book_data['borrower_id'])
+    puts user
+    @name = user['name']
+    @borrower_id = user['id']
+  end
+  erb  :'books/shell'
+end
+
+post '/books/:id/check-out' do
+  db = Library.create_db_connection('library_dev')
+  puts params
+  Library::BookRepo.check_out(db,params[:id],params[:user])
+  redirect "/books"
+end
+  
+post '/books/check-in' do
+  db = Library.create_db_connection('library_dev')
+  puts params
+  Library::BookRepo.check_in(db,params[:id].to_i)
+  redirect "/books"
+end  
