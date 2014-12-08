d690c02343233dd2557e4fbed592b9cc469325ce
Jason Hatchett <jason@hatchett.net>
2014-12-08 13:38:00 -0600

    End of Library Plus

--- lib/library_plus/book_repo.rb
+++ lib/library_plus/book_repo.rb
@@ -1,1 +1,46 @@
-# TODO
+module Library
+  class BookRepo
+
+    def self.index(db)
+      # Other code should not have to deal with the PG:Result.
+      # Therefore, convert the results into a plain array.
+      db.exec("SELECT * FROM books").to_a
+    end
+
+    def self.show(db, book_id)
+      # TODO: Insert SQL statement
+      res =  db.exec_params("SELECT * FROM books WHERE id = $1",[book_id])
+      return res.first
+    end
+
+    def self.show_checkouts(db, book_id)
+      # TODO: Insert SQL statement
+      res =  db.exec_params("SELECT * FROM checkouts WHERE book_id = $1",[book_id])
+      return res.first
+    end
+
+    def self.checkout(db, book_id, user_id)
+      exist =  db.exec_params("SELECT * FROM checkouts WHERE book_id = $1",[book_id])
+      if (exist.first == nil)
+        db.exec_params("INSERT INTO checkouts (book_id, user_id) VALUES ($1, $2)", [book_id, user_id])
+        return 'added'
+      else
+        db.exec_params("UPDATE checkouts SET status = 'checked_out' WHERE book_id = $1", [book_id])
+        return 'added'
+      end
+    end
+
+    def self.create(db, book_data)
+      # TODO: Insert SQL statement
+      db.exec_params("INSERT INTO books (title, author) VALUES ($1, $2)",[ book_data['title'], book_data['author'] ])
+      res =  db.exec_params("SELECT * FROM books WHERE title = $1",[book_data['title']])
+      return res.first
+    end
+
+    def self.destroy(db, book_id)
+      # TODO: Delete SQL statement
+      db.exec_params("DELETE FROM books WHERE id= $1 ",[book_id])
+    end
+
+  end
+end
--- server.rb
+++ server.rb
@@ -6,3 +6,48 @@ require './lib/library_plus'
 get '/' do
   erb :index
 end
+
+get '/users' do
+  db = Library.create_db_connection('library_dev')
+  @users = Library::UserRepo.all(db)
+  erb :"users/index"
+end
+
+post '/users' do
+  db = Library.create_db_connection('library_dev')
+  Library::UserRepo.save(db,{ 'name' => params[:title] })
+  @users = Library::UserRepo.all(db)
+  erb :"users/index"
+end
+
+get '/books' do
+  db = Library.create_db_connection('library_dev')
+  @books = Library::BookRepo.index(db)
+  erb :"books/index"
+end
+
+post '/books' do
+  db = Library.create_db_connection('library_dev')
+  Library::BookRepo.create(db,{ 'title' => params[:title], 'author' => params[:author] })
+  @books = Library::BookRepo.index(db)
+  erb :"books/index"
+end
+
+get '/books/:show' do
+  db = Library.create_db_connection('library_dev')
+  @users = Library::UserRepo.all(db)
+  @book = Library::BookRepo.show(db, params[:show])
+  @status= Library::BookRepo.show_checkouts(db, params[:show])
+  if (@status == nil)
+    @status = {'status' => 'available'}
+  end
+
+  erb :"books/show"
+end
+
+post '/books/:id/checkout' do
+  db = Library.create_db_connection('library_dev')
+  Library::BookRepo.checkout(db,params[:id], params[:user_id])
+  @books = Library::BookRepo.index(db)
+  erb :"books/index"
+end
