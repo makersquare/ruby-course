5b0d7d11ee734fcfa332ac8c7a3c4cca869954e8
charles milam <charles.milam@gmail.com>
2014-12-07 21:16:04 -0600

    Add endpoints get and push for /checkouts.

--- server.rb
+++ server.rb
@@ -22,3 +22,38 @@ post "/users" do
 
   redirect back
 end
+
+get "/books" do
+  db = Library.create_db_connection('library_dev')
+  @books = Library::BookRepo.all(db)
+
+  erb :"books/index"
+end
+
+post "/books" do
+  db = Library.create_db_connection('library_dev')
+  @title = params[:title]
+  @author = params[:author]
+
+  Library::BookRepo.save(db, :title => @title, :author => @author)
+
+  redirect back
+end
+
+get "/checkouts" do
+  db = Library.create_db_connection('library_dev')
+  @books = Library::BookRepo.all(db)
+  @users = Library::UserRepo.all(db)
+
+  erb :"checkouts/index"
+end
+
+post "/checkouts" do
+  db = Library.create_db_connection('library_dev')
+  @book_id = params[:book_id]
+  @user_id = params[:user_id]
+
+  Library::BookRepo.check_out(db, :book_id => @book_id, :user_id => @user_id)
+
+  redirect back
+end
