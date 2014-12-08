49f7ab514e2ca5d9c0f8ce55362b313d9f38b751
Gilbert <gilbertbgarza@gmail.com>
2014-12-08 08:49:01 -0600

    Starter code for songify exercise

---
+++ lib/songify/genre_repo.rb
@@ -0,0 +1,33 @@
+module Songify
+  class GenreRepo
+
+    def self.all(db)
+      # Other code should not have to deal with the PG:Result.
+      # Therefore, convert the results into a plain array.
+      db.exec("SELECT * FROM genres").to_a
+    end
+
+    def self.find(db, genre_id)
+      db.exec("SELECT * FROM genres WHERE id=$1", [genre_id]).first
+    end
+
+    def self.save(db, genre_data)
+      if genre_data['id']
+        result = db.exec("UPDATE genres SET name = $2 WHERE id = $1", [genre_data['id'], genre_data['name']])
+        self.find(db, genre_data['id'])
+      else
+        raise "name is required." if genre_data['name'].nil? || genre_data['name'] == ''
+
+        result = db.exec("INSERT INTO genres (name) VALUES ($1) RETURNING id", [genre_data['name']])
+        genre_data['id'] = result.entries.first['id']
+        genre_data
+      end
+    end
+
+    def self.destroy(db, genre_id)
+      # TODO: Delete SQL statement
+      #  ALSO DELETE JOIN TABLE ENTRIES BETWEEN THIS GENRE AND ITS ALBUMS
+    end
+
+  end
+end
---
+++ server.rb
@@ -0,0 +1,42 @@
+require 'sinatra'
+require './lib/songify.rb'
+
+# set :bind, '0.0.0.0' # This is needed for Vagrant
+
+get '/' do
+  erb :index
+end
+
+get '/albums' do
+  db = Songify.create_db_connection('songify_dev')
+  @albums = Songify::AlbumRepo.all(db)
+  erb :"albums/index"
+end
+
+post '/albums' do
+  db = Songify.create_db_connection('songify_dev')
+  album = Songify::AlbumRepo.save(db, {
+    'title' => params[:title]
+  })
+  redirect to '/albums'
+end
+
+
+get '/songs' do
+  erb :"songs/index"
+end
+
+
+get '/genres' do
+  db = Songify.create_db_connection('songify_dev')
+  @genres = Songify::GenreRepo.all(db)
+  erb :"genres/index"
+end
+
+post '/genres' do
+  db = Songify.create_db_connection('songify_dev')
+  album = Songify::GenreRepo.save(db, {
+    'name' => params[:name]
+  })
+  redirect to '/genres'
+end
