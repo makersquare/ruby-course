df89d0f59065cec403aa3f4dc63b815451a65dd0
Daniel Olasky <daniel.olasky@gmail.com>
2014-12-09 17:35:47 -0800

    final scaffold for songify

--- server.rb
+++ server.rb
@@ -6,6 +6,8 @@ set :bind, '0.0.0.0' # This is needed for Vagrant
 
 
 get '/' do
-  
+  @message = "Hello World"
+  @remarks = ["I am", "A Group Of", "Messages", "Make Sure", "You Check", "The ERB tags"]
+  erb :index
 end
 
