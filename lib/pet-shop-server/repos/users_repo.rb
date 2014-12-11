0ba0e6687409312064bf6015191db3979901b0fe
charles milam <charles.milam@gmail.com>
2014-12-11 17:55:30 -0600

    Modify names

--- lib/pet-shop-server/repos/users_repo.rb
+++ lib/pet-shop-server/repos/users_repo.rb
@@ -1,19 +1,19 @@
 module Blogtastic
   class UsersRepo
     def self.all db 
-      sql = %q[select * from posts]
+      sql = %q[select * from users]
       result = db.exec(sql)
       result.entries
     end
 
     def self.find db, id
-      sql = %[select * from pets where id = $1]
+      sql = %[select * from users where id = $1]
       result = db.exec(sql, [id])
       result.first
     end
 
     def self.find_by_name db, name
-      sql = %[select * from pets]
+      sql = %[select * from users]
     end
   end
 end
