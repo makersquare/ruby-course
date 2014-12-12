8d43b78c84650b323063eb1309d3d2407d93412a
Cristina Caputo <cristinacaputo@gmail.com>
2014-12-12 10:39:50 -0600

    add server.rb and lib etc

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ ruby '2.0.0'
 gem 'rspec', '~> 2.14.1'
 gem 'sinatra', '~> 1.4.5'
 gem 'sinatra-contrib', '~> 1.4.2'
-
+gem 'rest-client'
+gem 'rack-flash3'
 # Testing
 gem 'pry-byebug'
+gem 'pg'
--- server.rb
+++ server.rb
@@ -1,15 +1,46 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'rest-client'
+require 'rack-flash'
+require 'pry-byebug'
+require 'json' #why?
 
-# #
-# This is our only html view...
-#
-get '/' do
-  if session[:user_id]
-    # TODO: Grab user from database
-    @current_user = $sample_user
+#bundle exec ruby server.rb
+
+
+require_relative 'lib/petshopserver.rb'
+
+# class Petshopserver::Server < Sinatra::Application
+  configure do
+    set :bind, '0.0.0.0'
+    enable :sessions
+    use Rack::Flash
   end
+
+  helpers do
+    def mydb
+      # establish connection to db here
+    end
+  end
+
+  # before do
+  #   if session['user_id']
+  #     user_id = session['user_id']
+  #     db = Petshopserver.create_db_connection 'petshopserver'
+  #     @current_user = Petshopserver::UsersRepo.find db, user_id
+  #   else
+  #     @current_user = {'username' => 'anonymous', 'id' => 1}
+  #   end
+  # end
+
+get '/' do
+  if session['user_id']
+    user_id = session['user_id']
+    db = Petshopserver.create_db_connection 'petshopserver'
+    @current_user = Petshopserver::UsersRepo.find db, user_id
+  else
+      @current_user = {'username' => 'anonymous', 'id' => 1}
+    end
   erb :index
 end
 
@@ -28,13 +59,21 @@ post '/signin' do
   password = params['password']
 
   # TODO: Grab user by username from database and check password
-  user = { 'username' => 'alice', 'password' => '123' }
+  #user = { 'username' => 'alice', 'password' => '123' }
+  db = Petshopserver.create_db_connection 'petshopserver'
+  user = Petshopserver::UsersRepo.find_by_username(db, username)
 
   if password == user['password']
     headers['Content-Type'] = 'application/json'
+    session['user_id'] = user['id']
+    # 
+    user[:cats] =[]
+    user[:dogs] =[]
+
+    user.to_json
     # TODO: Return all pets adopted by this user
     # TODO: Set session[:user_id] so the server will remember this user has logged in
-    $sample_user.to_json
+    #$sample_user.to_json
   else
     status 401
   end
@@ -82,14 +121,16 @@ put '/shops/:shop_id/dogs/:id/adopt' do
 end
 
 
-$sample_user = {
-  id: 999,
-  username: 'alice',
-  cats: [
-    { shopId: 1, name: "NaN Cat", imageUrl: "http://i.imgur.com/TOEskNX.jpg", adopted: true, id: 44 },
-    { shopId: 8, name: "Meowzer", imageUrl: "http://www.randomkittengenerator.com/images/cats/rotator.php", id: 8, adopted: "true" }
-  ],
-  dogs: [
-    { shopId: 1, name: "Leaf Pup", imageUrl: "http://i.imgur.com/kuSHji2.jpg", happiness: 2, id: 2, adopted: "true" }
-  ]
-}
+# $sample_user = {
+#   id: 999,
+#   username: 'alice',
+#   cats: [
+#     { shop_id: 1, name: "NaN Cat",  adopted: true, id: 44 },
+#     { shop_id: 8, name: "Meowzer",  id: 8, adopted: "true" }
+#   ],
+#   dogs: [
+#     { shop_id: 1, name: "Leaf Pup", id: 2, adopted: "true" }
+#   ]
+# }
+
+# end 
