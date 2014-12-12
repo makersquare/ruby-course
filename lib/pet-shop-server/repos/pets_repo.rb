0207c3a514c8c850bcbe5fdf9528ae0b4ea59ccd
Michael Bashour <michael.s.bashour@gmail.com>
2014-12-12 14:04:38 -0600

    Correct build_user method

--- lib/pet-shop-server/repos/pets_repo.rb
+++ lib/pet-shop-server/repos/pets_repo.rb
@@ -54,14 +54,14 @@ module PetShopServer
         catResult = db.exec(catSql, 999)
         dogResult = db.exec(dogSql, 999)
       $real_user = {}
-        $real_user['id'] = result.first['id']
-        $real_user['username'] = result.first['username']
+        $real_user['id'] = catResult.first['id']
+        $real_user['username'] = catResult.first['username']
         # push cats
-        result.each do |cat|
+        catResult.each do |cat|
           push_object = {
-                        'shopId' => cat['shopId'],
+                        'shopId' => cat['shop_id'],
                         'name' => cat['name'],
-                        'imageUrl' => cat['imageUrl'],
+                        'imageUrl' => cat['image_url'],
                         'adopted' => cat['adopted'],
                         'happiness' => cat['happiness']
                         }
@@ -69,15 +69,16 @@ module PetShopServer
           end
 
         #push dogs
-        result.each do |dog|
+        dogResult.each do |dog|
           push_object = {
-                        'shopId' => dog['shopId'],
+                        'shopId' => dog['shop_id'],
                         'name' => dog['name'],
-                        'imageUrl' => dog['imageUrl'],
+                        'imageUrl' => dog['image_url'],
                         'adopted' => dog['adopted'],
                         'happiness' => dog['happiness']
                         }
           $real_user['dogs'].push(push_object)
+          $real_user
           end
         
 
