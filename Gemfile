d714426277a9cadb1f91ddcaec204e0cc7adf39f
Nora Neyland <noraleeneyland@gmail.com>
2014-12-12 17:39:17 +0000

    fix merge issues

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem 'rspec', '~> 2.14.1'
 gem 'sinatra', '~> 1.4.5'
 gem 'sinatra-contrib', '~> 1.4.2'
 gem 'rest-client'
+gem 'rack-flash3'
+gem 'pg'
 
 # Testing
 gem 'pry-byebug'
--- server.rb
+++ server.rb
@@ -1,26 +1,86 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'rest-client'
-require 'json'
+require 'rack-flash'
+require 'pry-byebug'
+require 'json' #why?
 
+#bundle exec ruby server.rb
+
+
+require_relative 'lib/petshopserver.rb'
+
+# class Petshopserver::Server < Sinatra::Application
+  configure do
+    set :bind, '0.0.0.0'
+    enable :sessions
+    use Rack::Flash
+  end
+
+  helpers do
+    def mydb
+      db = Petshopserver.create_db_connection 'petshopserver'
+    end
+  end
+
+  # before do
+  #   if session['user_id']
+  #     user_id = session['user_id']
+  #     db = Petshopserver.create_db_connection 'petshopserver'
+  #     @current_user = Petshopserver::UsersRepo.find db, user_id
+  #   else
+  #     @current_user = {'username' => 'anonymous', 'id' => 1}
+  #   end
+  # end
+
+
+# class Petshopserver::Server < Sinatra::Application
+  
+  configure do
+    set :bind, '0.0.0.0'
+    enable :sessions
+    use Rack::Flash
+  end
+
+#   before do
+#     if session['user_id'] # if it exists, the user is logged in
+#       user_id = session['user_id']
+#       db = Petshopserver.create_db_connection 'petshopserver'
+#       @current_user = Petshopserver::UsersRepo.find db, user_id
+#     else
+#       @current_user = {'username' => 'anonymous', 'id' => 1}
+#     end
+#   end
 # #
+# for session you use a symbol 
+
 # This is our only html view...
 #
 get '/' do
-  if session[:user_id]
+  if session['user_id']
     # TODO: Grab user from database
-    @current_user = $sample_user
+    # @current_user = $sample_user
+    user_id = session['user_id']
+    db = Petshopserver.create_db_connection 'petshopserver'
+    @current_user = Petshopserver::UsersRepo.find db, user_id
   end
   erb :index
+end
+
+get '/logout' do
+  session.delete 'user_id'
+  redirect to '/'
 end
 
 # #
 # ...the rest are JSON endpoints
 #
+
 get '/shops' do
   headers['Content-Type'] = 'application/json'
   RestClient.get("http://pet-shop.api.mks.io/shops")
 end
+
 
 post '/signin' do
   params = JSON.parse request.body.read
@@ -29,17 +89,29 @@ post '/signin' do
   password = params['password']
 
   # TODO: Grab user by username from database and check password
-  user = { 'username' => 'alice', 'password' => '123' }
+  #user = { 'username' => 'alice', 'password' => '123' }
+  user = Petshopserver::UsersRepo.find_by_username(mydb, username)
 
   if password == user['password']
     headers['Content-Type'] = 'application/json'
+    session['user_id'] = user['id']
+    # 
+    user[:cats] =[]
+    user[:dogs] =[]
+
+
+    user.to_json
     # TODO: Return all pets adopted by this user
     # TODO: Set session[:user_id] so the server will remember this user has logged in
-    $sample_user.to_json
+    #$sample_user.to_json
   else
     status 401
   end
 end
+
+# user_id = session['user_id']
+# db = Petshopserver.create_db_connection 'petshopserver'
+# @current_user = Petshopserver::UsersRepo.find db, user_id
 
  # # # #
 # Cats #
@@ -83,14 +155,16 @@ put '/shops/:shop_id/dogs/:id/adopt' do
 end
 
 
-$sample_user = {
-  id: 999,
-  username: 'alice',
-  cats: [
-    { shopId: 1, name: "NaN Cat", imageUrl: "http://i.imgur.com/TOEskNX.jpg", adopted: true, id: 44 },
-    { shopId: 8, name: "Meowzer", imageUrl: "http://www.randomkittengenerator.com/images/cats/rotator.php", id: 8, adopted: "true" }
-  ],
-  dogs: [
-    { shopId: 1, name: "Leaf Pup", imageUrl: "http://i.imgur.com/kuSHji2.jpg", happiness: 2, id: 2, adopted: "true" }
-  ]
-}
+# $sample_user = {
+#   id: 999,
+#   username: 'alice',
+#   cats: [
+#     { shopId: 1, name: "NaN Cat", imageUrl: "http://i.imgur.com/TOEskNX.jpg", adopted: true, id: 44 },
+#     { shopId: 8, name: "Meowzer", imageUrl: "http://www.randomkittengenerator.com/images/cats/rotator.php", id: 8, adopted: "true" }
+#   ],
+#   dogs: [
+#     { shopId: 1, name: "Leaf Pup", imageUrl: "http://i.imgur.com/kuSHji2.jpg", happiness: 2, id: 2, adopted: "true" }
+#   ]
+# }
+
+# end
