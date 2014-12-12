cb7ee8989ad13d0aa878b03371ce41ffa639a85b
Glenn Meyer <glennmeyer@gmail.com>
2014-12-12 16:53:08 +0000

    add petshop.rb

---
+++ lib/petshop.rb
@@ -0,0 +1,120 @@
+require 'pg'
+require 'rest-client'
+require 'json'
+
+module PetShopServer
+  def self.create_db_connection dbname
+    PG.connect(host: 'localhost', dbname: dbname)
+  end
+
+  def get(url)
+    response = RestClient.get(url)
+    JSON.parse(response)
+  end
+
+  def self.clear_tables db
+    db.exec <<-SQL
+      DELETE FROM cats;
+      DELETE FROM dogs;
+      DELETE FROM shops;
+      DELETE FROM users;
+    SQL
+  end
+
+  def self.create_tables db
+    db.exec <<-SQL
+      CREATE TABLE IF NOT EXISTS shops(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR
+      );
+      CREATE TABLE IF NOT EXISTS cats(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR,
+        adopted VARCHAR,
+        imageUrl VARCHAR,
+        shopId INTEGER REFERENCES shops(id)
+          ON DELETE CASCADE
+          ON UPDATE CASCADE
+      );
+      CREATE TABLE IF NOT EXISTS dogs(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR,
+        adopted VARCHAR,
+        imageUrl VARCHAR,
+        shopId INTEGER REFERENCES shops(id)
+          ON DELETE CASCADE
+          ON UPDATE CASCADE
+      );
+      CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR,
+        password VARCHAR
+      );
+    SQL
+  end
+
+  def self.drop_tables db
+
+    db.exec <<-SQL
+      DROP TABLE cats;
+      DROP TABLE dogs;
+      DROP TABLE shops;
+      DROP TABLE users;
+    SQL
+  end
+
+  def self.seed_tables
+    #### Testing ####
+    db = create_db_connection 'pet_shop_server'
+    clear_tables db
+    drop_tables db
+    create_tables db
+    #### Testing ####
+
+    shops_list = get("http://pet-shop.api.mks.io/shops")
+    cats_by_shop = []
+    dogs_by_shop = []
+    shops_query = []
+    cats_query = []
+    dogs_query = []
+
+    shops_list.each do |shop|
+      shops_query << "(\'" + db.escape_string(shop['name']) + "\')"
+      cats_by_shop << get("pet-shop.api.mks.io/shops/" + shop['id'].to_s + "/cats")
+      dogs_by_shop << get("pet-shop.api.mks.io/shops/" + shop['id'].to_s + "/dogs")
+    end
+
+    cat_shop = 0
+    cats_by_shop.each do |shop|
+      cat_shop += 1
+      shop.each do |cat|
+        cats_query << "(\'" + db.escape_string(cat["name"]) + "\', \'" + (cat["adopted"].to_s != "true" ? "false" : "#{cat["adopted"].to_s}") + "\', \'" + db.escape_string(cat["imageUrl"]) + "\', \'#{cat_shop.to_s}\')"
+      end
+    end
+
+    dog_shop = 0
+    dogs_by_shop.each do |shop|
+      dog_shop += 1
+      shop.each do |dog|
+        dog["name"].gsub!(/[^a-zA-Z]/, "")
+        dogs_query << "(\'" + db.escape_string(dog["name"]) + "\', \'" + (dog["adopted"].to_s != "true" ? "false" : "#{dog["adopted"].to_s}") + "\', \'" + db.escape_string(dog["imageUrl"]) + "\', \'#{dog_shop.to_s}\')"
+      end
+    end
+
+    shops_query = shops_query.join(",")
+    cats_query = cats_query.join(",")
+    dogs_query = dogs_query.join(",")
+    users_query = "('Glenn', 'password'), ('Bonnie', 'ladybug'), ('Steve', 'avanti'), ('Stephen', 'broncos')"
+
+    db.exec("INSERT INTO shops (name) VALUES " + shops_query)
+    db.exec("INSERT INTO cats (name, adopted, imageUrl, shopId) VALUES " + cats_query)
+    db.exec("INSERT INTO dogs (name, adopted, imageUrl, shopId) VALUES " + dogs_query)
+
+    db.exec("INSERT INTO users (username, password) VALUES " + users_query)
+  end  
+end
+
+require_relative 'pet-shop-server/repos/cat_repo.rb'
+require_relative 'pet-shop-server/repos/dog_repo.rb'
+require_relative 'pet-shop-server/repos/shop_repo.rb'
+require_relative 'pet-shop-server/repos/user_repo.rb'
