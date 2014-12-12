f3de408e29e6141f17b5234904122beee6f4f379
tmctigrit <mctigrit.tricia@gmail.com>
2014-12-12 12:30:44 -0600

    Add ability to seed from JSON data

--- lib/petshop.rb
+++ lib/petshop.rb
@@ -1,4 +1,6 @@
 require 'pg'
+require 'json'
+require 'rest-client'
 
 # require_relative 'blogtastic/repos/posts_repo.rb'
 # require_relative 'blogtastic/repos/comments_repo.rb'
@@ -50,13 +52,91 @@ module Petshop
     SQL
   end
 
-  def self.seed_db(db)
-    db.exec <<-SQL
-      INSERT INTO users (username, password) values ('anonymous', 'anonymous');
-      INSERT INTO shops (name) values ('Rufinos Ruffers');
-      INSERT INTO dogs (name, imageUrl, adopted, happiness) values ('Barky', 'http://www.google.com/barky.jpg', 'false', 4);
-      INSERT INTO cats (name, imageUrl, adopted) values ('Catzy', 'http://www.cats.com/catzy.jpg', 'false')
-    SQL
+  def self.seed_shops(db)
+    #parse JSON data for shops
+    shops = JSON.parse RestClient.get("http://pet-shop.api.mks.io/shops")
+
+    sql = %q[
+     INSERT INTO shops(id, name)
+     VALUES ($1, $2)
+     ]
+
+   shops.each do |data|
+     id = data["id"]
+     name = data["name"]
+
+     db.exec(sql, [id, name])
+   end
+
+  end
+
+  def self.seed_users(db)
+     sql = %q[
+       INSERT INTO users (username, password)
+       VALUES ('testuser', 'pass123')
+     ]
+     db.exec(sql)
+  end
+
+  def self.seed_cats(db)
+
+    #parse JSON data for shops
+    shops = JSON.parse RestClient.get("http://pet-shop.api.mks.io/shops")
+
+    #get the cats information
+    cats = shops.map do |shop|
+      shop_id = shop['id']
+      JSON.parse RestClient.get("http://pet-shop.api.mks.io/shops/#{shop_id}/cats")
+    end
+
+    flatcats = cats.flatten
+
+   sql = %q[
+     INSERT INTO cats(id, shopId, name, imageUrl, adopted)
+     VALUES ($1, $2, $3, $4, $5)
+   ]
+
+   flatcats.each do |cat|
+     id = cat["id"]
+     shopId = cat["shopId"]
+     name = cat["name"]
+     imageUrl = cat["imageUrl"]
+     adopted = cat["adopted"] ? true : false
+
+     db.exec(sql, [id, shopId, name, imageUrl, adopted])
+  end
+
+end
+
+  def self.seed_dogs(db)
+
+    #parse JSON data for shops
+    shops = JSON.parse RestClient.get("http://pet-shop.api.mks.io/shops")
+
+    #get the dogs information
+    dogs = shops.map do |shop|
+      shop_id = shop['id']
+      JSON.parse RestClient.get("http://pet-shop.api.mks.io/shops/#{shop_id}/dogs")
+    end
+
+    flatdogs = dogs.flatten
+
+    sql = %q[
+     INSERT INTO dogs(id, shopId, name, imageUrl, adopted, happiness)
+     VALUES ($1, $2, $3, $4, $5, $6)
+     ]
+
+   flatdogs.each do |dog|
+     id = dog["id"]
+     shopId = dog["shopId"]
+     name = dog["name"]
+     imageUrl = dog["imageUrl"]
+     adopted = dog["adopted"] ? true : false
+     happiness = dog["happiness"]
+
+     db.exec(sql, [id, shopId, name, imageUrl, adopted, happiness])
+    end
+
   end
 
   def self.drop_tables(db)
@@ -68,4 +148,8 @@ module Petshop
     SQL
   end
 end
+
+
+
+
 
