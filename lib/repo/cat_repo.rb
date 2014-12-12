c10732356cf9e473d591ad3e101adcfaee4ace86
Glenn Meyer <glennmeyer@gmail.com>
2014-12-12 18:49:08 +0000

    json parse, cat repo update

--- lib/repo/cat_repo.rb
+++ lib/repo/cat_repo.rb
@@ -5,32 +5,33 @@ module Petshops
     end
 
     def self.find_by_id(db, id)
-      sql = %q[SELECT * FROM cats where id = $1]
+      sql = %q[SELECT * FROM cats WHERE id = $1]
       db.exec(sql, [id]).entries.first
     end
 
     def self.save(db, cat_data)
       if cat_data['id']
-        sql = %q[UPDATE cats SET ownerId = $2 where id = $1]
+        sql = %q[UPDATE cats SET owner_id = $2 WHERE id = $1]
         result = db.exec(sql, cat_data['id'], cat_data['ownerId'])
         result.entries.first
       else
-        sql = %q[INSERT INTO cats (name, imageurl, shopid) values ($1, $2, $3) returning *]
+        sql = %q[INSERT INTO cats (name, image_url, shopid) VALUES ($1, $2, $3) RETURNING *]
         result = db.exec(sql, [cat_data['name'], cat_data['imageurl'], cat_data['shopid']])
         result.entries.first
       end
     end
 
     def self.find_by_name(db, cat_name)
-      sql = %q[SELECT * FROM cats where name = $1]
+      sql = %q[SELECT * FROM cats WHERE name = $1]
       result = db.exec(sql, [cat_name])
       result.entries.first
     end
 
     def self.find_by_shop_id(db, shop_id)
-      sql = %q[SELECT * FROM cats WHERE shopId = $1]
+      sql = %q[SELECT shop_id AS "shopId", name, image_url AS "imageUrl", adopted, id FROM cats WHERE shop_id = $1]
       result = db.exec(sql, [shop_id])
       result.entries
     end
+
   end
 end
